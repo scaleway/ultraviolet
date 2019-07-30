d4a9a68577072ab863188b5911dd9f8cc68f7ed9
Vivien Ripoche <vripoche@online.net>
2019-07-30 17:15:12 +0200

    feat: checkbox new errors display

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -3,15 +3,36 @@ import PropTypes from 'prop-types'
 import { css } from '@emotion/core'
 import { SwitchState } from '@smooth-ui/core-em'
 import { cx } from 'utils'
+import {
+  borderRadius,
+  gray100,
+} from 'theming'
 import { ActivityIndicator } from './ActivityIndicator'
 import { Icon } from './Icon'
 import { Typography } from './Typography'
+import { Box } from './Box'
+import { Expandable } from './Expandable'
 
 const styles = {
   container: css`
     position: relative;
     display: inline-flex;
     align-items: center;
+  `,
+  hover: p => css`
+    &:hover {
+      svg {
+          border-radius: ${borderRadius(p)};
+          background-color: ${!p.disabled && gray100(p)};
+        }
+      }
+    }
+  `,
+  icon: css`
+    box-sizing: content-box;
+  `,
+  input: css`
+    cursor: pointer;
   `,
 }
 
@@ -22,6 +43,7 @@ export function Checkbox({
   onFocus,
   onBlur,
   valid,
+  error,
   name,
   value,
   size = 24,
@@ -44,32 +66,39 @@ export function Checkbox({
       onBlur={onBlur}
     >
       {({ checked, input }) => (
-        <Typography css={cx(styles.container)} {...props}>
-          {progress ? (
-            <ActivityIndicator mr={hasChildren ? 1 : 0} />
-          ) : (
-            <Icon
-              mr={hasChildren ? 1 : 0}
-              name={
-                checked ? 'checkbox-marked-outline' : 'checkbox-blank-outline'
-              }
-              color={
-                disabled
-                  ? 'gray100'
-                  : valid === false
-                  ? 'warning'
-                  : valid === true
-                  ? 'success'
-                  : checked
-                  ? 'primary'
-                  : 'gray300'
-              }
-              size={size}
-            />
-          )}
-          {hasChildren ? <span>{children}</span> : null}
-          <input type="checkbox" {...input} id={id} disabled={disabled} />
-        </Typography>
+        <>
+          <Typography css={cx([styles.container, !disabled && styles.hover])} {...props}>
+            {progress ? (
+              <ActivityIndicator mr={hasChildren ? 1 : 0} />
+            ) : (
+              <Icon
+                mr={hasChildren ? '10px' : 0}
+                p="2px"
+                css={cx(styles.icon)}
+                name={
+                  checked ? 'checkbox-marked-outline' : 'checkbox-blank-outline'
+                }
+                color={
+                  disabled
+                    ? 'gray100'
+                    : valid === false || Boolean(error)
+                    ? 'warning'
+                    : valid === true
+                    ? 'success'
+                    : checked
+                    ? 'primary'
+                    : 'gray300'
+                }
+                size={size}
+              />
+            )}
+            {hasChildren ? <span>{children}</span> : null}
+            <input css={cx(!disabled && styles.input)} type="checkbox" {...input} id={id} disabled={disabled} />
+          </Typography>
+          <Expandable height={56} overflow="hidden" opened={Boolean(error)}>
+            <Box fontSize={12} color="warning" mt="-6px" px="4px">{error}</Box>
+          </Expandable>
+        </>
       )}
     </SwitchState>
   )
@@ -80,6 +109,7 @@ Checkbox.propTypes = {
   progress: PropTypes.bool,
   size: PropTypes.number,
   children: PropTypes.node,
+  error: PropTypes.string,
 }
 
 export default Checkbox
