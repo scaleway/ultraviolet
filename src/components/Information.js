8200c1716f4aedc04a5b845d7e3bb836185cb155
Yveline Say <yvelinesay@Yvelines-MacBook-Pro.local>
2019-08-28 11:22:38 +0200

    feat: add Information component

---
+++ src/components/Information.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { css } from '@emotion/core'
+import { Box } from '@smooth-ui/core-em'
+import { Icon } from 'components/Icon'
+import { Typography } from 'components/Typography'
+import { cx, thColor } from 'utils'
+
+const style = ({ backgroundColor }) => p => {
+  return css`
+    background-color: ${thColor(backgroundColor)(p) || backgroundColor};
+    border-radius: 4px;
+  `
+}
+
+export const Information = ({
+  backgroundColor,
+  color,
+  icon,
+  iconColor,
+  iconSize,
+  img,
+  imgSize,
+  heading,
+  text,
+  ...props
+}) => (
+  <Box
+    display="flex"
+    py={2}
+    px={3}
+    alignItems="center"
+    css={cx(style({ backgroundColor }))}
+    {...props}
+  >
+    {icon && (
+      <Icon mr={2} name={icon} size={iconSize} color={iconColor || color} />
+    )}
+    {img && <Box as="img" mr={3} src={img} width={imgSize} height={imgSize} />}
+    <Box display="flex" flexDirection="column">
+      {heading && (
+        <Typography variant="bodyA" fontWeight={500} color={color}>
+          {heading}
+        </Typography>
+      )}
+      <Typography variant="bodyA" color={color}>
+        {text}
+      </Typography>
+    </Box>
+  </Box>
+)
+
+Information.defaultProps = {
+  backgroundColor: 'white',
+  color: 'primary',
+  iconSize: 32,
+  imgSize: 57,
+}
+
+Information.propTypes = {
+  backgroundColor: PropTypes.string,
+  color: PropTypes.oneOf(['primary', 'alert']),
+  icon: PropTypes.string,
+  iconSize: PropTypes.number,
+  iconColor: PropTypes.string,
+  imgSize: PropTypes.number,
+}
