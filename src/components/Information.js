b8545b51026222731d11b74751579cd205d036c5
Yveline Say <yvelinesay@Yvelines-MacBook-Pro.local>
2019-09-05 16:59:36 +0200

    fix: update information component with children

--- src/components/Information.js
+++ src/components/Information.js
@@ -14,6 +14,7 @@ const style = ({ backgroundColor }) => p => {
 }
 
 export const Information = ({
+  children,
   backgroundColor,
   color,
   icon,
@@ -47,6 +48,7 @@ export const Information = ({
         {text}
       </Typography>
     </Box>
+    {children}
   </Box>
 )
 
