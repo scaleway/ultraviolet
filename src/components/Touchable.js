4b4c058cd2272568730ade3fd0a0ce8f335f6cb8
Emmanuel Chambon <echambon@scaleway.com>
2020-09-29 10:16:09 +0200

    chore: migrate Touchable and Tag component from shire [CNS-2112]

--- src/components/Touchable.js
+++ src/components/Touchable.js
@@ -1,50 +1,58 @@
-import React from 'react'
-import PropTypes from 'prop-types'
 import { css } from '@emotion/core'
-import { transparentize } from 'polished'
-import { cx } from 'utils'
-import { primary } from 'theming'
+import React from 'react'
 import { Box } from './Box'
 
 const styles = {
-  touchable: p => css`
-    border: 0;
-    transition: opacity 200ms, box-shadow 200ms;
-    user-select: none;
-    background-color: transparent;
-    padding: 0;
-    margin: 0;
-    cursor: pointer;
-    color: currentColor;
-
-    &[type='button'] {
-      appearance: none;
-    }
-
-    &:focus,
-    &:focus-within {
-      outline: none;
-      box-shadow: 0 0 3px ${transparentize(0.7, primary(p))};
-    }
-  `,
-  disabled: css`
-    cursor: default;
-    opacity: 0.5;
-  `,
+  root: css({
+    border: 0,
+    transitionProperty: 'opacity',
+    transitionDuration: '0.15s',
+    userSelect: 'none',
+    backgroundColor: 'transparent',
+    padding: 0,
+    margin: 0,
+  }),
+  actionable: css({
+    cursor: 'pointer',
+    touchAction: 'manipulation',
+  }),
+  disabled: css({
+    opacity: 0.5,
+  }),
 }
 
-export function Touchable({ disabled, as = 'button', type, ...props }) {
-  return (
-    <Box
-      css={cx([styles.touchable, disabled && styles.disabled])}
-      as={as}
-      type={as === 'button' ? 'button' : type}
-      disabled={as === 'button' ? disabled : undefined}
-      {...props}
-    />
-  )
-}
+const Touchable = ({
+  innerRef,
+  disabled,
+  activeOpacity,
+  hasFocus,
+  as = 'button',
+  type,
+  ...props
+}) => (
+  <Box
+    ref={innerRef}
+    css={[
+      styles.root,
+      !disabled && styles.actionable,
+      !disabled &&
+        css({
+          ':active': { opacity: activeOpacity },
+        }),
+      disabled && styles.disabled,
+    ]}
+    {...props}
+    as={as}
+    type={as === 'button' ? 'button' : type}
+    disabled={disabled}
+    tabIndex={hasFocus && 0}
+  />
+)
 
-Touchable.propTypes = {
-  disabled: PropTypes.bool,
+const forwardRef = (props, ref) => <Touchable {...props} innerRef={ref} />
+const ForwardRef = React.forwardRef(forwardRef)
+ForwardRef.displayName = 'fwd(Touchable)'
+
+export {
+  ForwardRef as Touchable,
 }
