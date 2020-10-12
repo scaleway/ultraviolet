9996340077b4239d5b0b77c716d17a7adc0869a9
Emmanuel Chambon <echambon@scaleway.com>
2020-10-12 16:42:43 +0200

    fix: single path svg

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -167,10 +167,7 @@ const ICONS = {
   ),
 
   east: () => (
-    <>
-      <path fill="none" d="M0 0h24v24H0z" />
-      <path d="M15 5l-1.41 1.41L18.17 11H2v2h16.17l-4.59 4.59L15 19l7-7-7-7z" />
-    </>
+    <path d="M15 5l-1.41 1.41L18.17 11H2v2h16.17l-4.59 4.59L15 19l7-7-7-7z" />
   ),
 
   'ray-start-end': () => (
