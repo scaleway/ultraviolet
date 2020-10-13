7c0a113eef878ef4f2642c072316cee698702dad
Vincent Audebert <vaudebert@online.net>
2020-10-13 16:47:48 +0200

    fix: simplifying props

--- src/components/Sphere.js
+++ src/components/Sphere.js
@@ -5,18 +5,21 @@ import React from 'react'
 import { theme } from 'theme'
 
 const styles = {
-  sphere: ({ size, bgColors, isHalved }) => css`
-    align-items: center;
-    border-left: ${size / 2}px solid ${bgColors[0]};
-    border-top: ${size / 2}px solid ${bgColors[0]};
-    border-right: ${size / 2}px solid ${isHalved ? bgColors[1] : bgColors[0]};
-    border-bottom: ${size / 2}px solid ${isHalved ? bgColors[1] : bgColors[0]};
-    border-radius: 50%;
-    display: flex;
-    height: 100%;
-    justify-content: center;
-    width: 100%;
-  `,
+  sphere: ({ size, bgColors }) => {
+    const isHalved = bgColors.length > 1
+    return css`
+      align-items: center;
+      border-left: ${size / 2}px solid ${bgColors[0]};
+      border-top: ${size / 2}px solid ${bgColors[0]};
+      border-right: ${size / 2}px solid ${isHalved ? bgColors[1] : bgColors[0]};
+      border-bottom: ${size / 2}px solid ${isHalved ? bgColors[1] : bgColors[0]};
+      border-radius: 50%;
+      display: flex;
+      height: 100%;
+      justify-content: center;
+      width: 100%;
+    `
+  },
   textSphere: ({ textColor, textSize = 10 }) => css`
     color: ${textColor};
     font-size: ${textSize}px;
@@ -29,11 +32,10 @@ export const Sphere = ({
   text, // Supports only 1 char (star char for instance), that's why we take only first char if long text given
   textColor,
   textSize,
-  isHalved = false,
   ...props
 }) => (
   <Box
-    css={styles.sphere({ size, bgColors, isHalved })}
+    css={styles.sphere({ size, bgColors })}
     width={size}
     height={size}
     position="relative"
@@ -56,7 +58,8 @@ Sphere.defaultProps = {
   size: 32,
   bgColors: [theme.violet],
   text: undefined,
-  isHalved: false,
+  textColor: theme.white,
+  textSize: 16,
 }
 
 Sphere.propTypes = {
@@ -65,7 +68,6 @@ Sphere.propTypes = {
   text: PropTypes.string,
   textColor: PropTypes.string,
   textSize: PropTypes.number,
-  isHalved: PropTypes.bool,
 }
 
 export default Sphere
