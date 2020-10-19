5f5cbff9aff7ac48831ffa195b288c46e92a94e3
Emmanuel Chambon <echambon@scaleway.com>
2020-10-20 01:19:07 +0200

    feat: migrate to storybook

---
+++ src/components/Badge/__tests__/Badge.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import shouldMatchEmotionSnapshot from 'helpers/shouldMatchEmotionSnapshot'
+
+import { Badge } from '..'
+
+test('Badge renders correctly with default values', () => {
+  shouldMatchEmotionSnapshot(<Badge>Sample badge</Badge>)
+})
---
+++ src/components/Dot/__tests__/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import shouldMatchEmotionSnapshot from 'helpers/shouldMatchEmotionSnapshot'
+
+import { Dot } from '..'
+
+test('Button renders correctly', () => {
+  shouldMatchEmotionSnapshot(<Dot>This is hotpink.</Dot>)
+})
