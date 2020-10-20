84d26476f2d966d52324d1d27409253162759c55
Emmanuel Chambon <echambon@scaleway.com>
2020-10-20 03:21:41 +0200

    fix: simpler config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,4 @@
-const babel = require('@babel/core')
-
 module.exports = {
   stories: ['../src/**/__stories__/*.stories.mdx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  babel: config => {
-    const babelConfig = babel.loadPartialConfig({
-      configFile: require.resolve('../babel.config'),
-    })
-    if (!babelConfig) {
-      throw new Error('Failed to load Babel config')
-    }
-    return {
-      ...config,
-      ...babelConfig.options,
-    }
-  },
 }
