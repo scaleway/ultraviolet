cab7ce53f40e47d5ca09f93a77533be29a5c7917
Emmanuel Chambon <echambon@scaleway.com>
2020-10-27 12:46:33 +0100

    fix: fix

--- src/components/Reminder/index.js
+++ src/components/Reminder/index.js
@@ -2,6 +2,7 @@ import styled from '@emotion/styled'
 import PropTypes from 'prop-types'
 import React from 'react'
 import { theme } from '../../theme'
+import { Box } from '../Box'
 import { Icon } from '../Icon'
 import { UniversalLink } from '../UniversalLink'
 
