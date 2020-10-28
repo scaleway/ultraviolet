c6c96bb37b941d05d35509dba8f9ac8a7483eeb5
Emmanuel Chambon <echambon@scaleway.com>
2020-10-28 17:01:58 +0100

    chore: use @scaleway/random-name

--- src/utils/randomize.js
+++
@@ -1,9 +0,0 @@
-import dockerNames from 'docker-names'
-
-export function randomName(prefix = '', separator = '-') {
-  let random = dockerNames.getRandomName().replace(/_/g, separator)
-  while (random.includes('cocks')) {
-    random = dockerNames.getRandomName().replace(/_/g, separator)
-  }
-  return prefix.length > 0 ? `${prefix}${separator}${random}` : random
-}
