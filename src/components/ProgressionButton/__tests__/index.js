9c37799b68de794de10312a1c3b0a684188995d7
Emmanuel Chambon <echambon@scaleway.com>
2020-11-02 10:48:54 +0100

    feat: migrate ProgressionButton from shire

---
+++ src/components/ProgressionButton/__tests__/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { ProgressionButton } from '..'
+import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
+
+describe('ProgressionButton', () => {
+  it('renders correctly with default values', () => {
+    shouldMatchEmotionSnapshot(
+      <ProgressionButton>Progression</ProgressionButton>,
+    )
+  })
+
+  it('renders correctly with a string as creation date', () => {
+    shouldMatchEmotionSnapshot(
+      <ProgressionButton creation={new Date().toString()}>
+        Progression
+      </ProgressionButton>,
+    )
+  })
+
+  it('renders correctly with a duration subceeding its creation date', () => {
+    shouldMatchEmotionSnapshot(
+      <ProgressionButton
+        creation={new Date(new Date().setSeconds(new Date().getSeconds() - 90))}
+        duration={10}
+      >
+        Progression
+      </ProgressionButton>,
+    )
+  })
+
+  it('renders correctly with a duration exceeding its creation date', () => {
+    shouldMatchEmotionSnapshot(
+      <ProgressionButton
+        creation={new Date(new Date().setSeconds(new Date().getSeconds() + 90))}
+        duration={10}
+      >
+        Progression
+      </ProgressionButton>,
+    )
+  })
+})
