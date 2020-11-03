ae2f4072743247543040c8c07e4d2737a9f5c9d5
Alexandre Philibeaux <aphilibeaux@online.net>
2020-11-03 10:35:57 +0100

    feat(component): migration Command component

---
+++ src/components/Command/index.js
@@ -0,0 +1,43 @@
+import { css } from '@emotion/core'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { theme } from '../../theme'
+import { Box } from '../Box'
+
+const style = css`
+  font-family: 'Lucida Console', 'Monaco', 'Courier New', 'Courier', monospace;
+  font-size: 13px;
+  font-weight: 500;
+  border-radius: 5px;
+`
+
+const Command = ({
+  children,
+  color = theme.lightBlack,
+  backgroundColor = theme.light,
+  ...props
+}) => (
+  <Box
+    color={color}
+    backgroundColor={backgroundColor}
+    css={style}
+    as="span"
+    p={1}
+    {...props}
+  >
+    {children}
+  </Box>
+)
+
+Command.propTypes = {
+  children: PropTypes.node.isRequired,
+  color: PropTypes.string,
+  backgroundColor: PropTypes.string,
+}
+
+Command.defaultProps = {
+  color: theme.lightBlack,
+  backgroundColor: theme.light,
+}
+
+export { Command }
