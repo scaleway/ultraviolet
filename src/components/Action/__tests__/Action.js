6d68af94ac55ab7064110f0fe647e8f442147c46
Emmanuel Chambon <echambon@scaleway.com>
2020-11-04 17:22:32 +0100

    test: fix side effects

--- src/components/Action/__tests__/Action.js
+++ src/components/Action/__tests__/Action.js
@@ -3,34 +3,39 @@ import { create } from 'react-test-renderer'
 import { Action } from '..'
 import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
 
-test('Action renders correctly with  name lock and primary variant', () => {
-  shouldMatchEmotionSnapshot(<Action name="lock" variant="primary" />)
-})
+describe('Action', () => {
+  test('Action renders correctly with  name lock and primary variant', () => {
+    shouldMatchEmotionSnapshot(<Action name="lock" variant="primary" />)
+  })
 
-test('Action renders with children ', () => {
-  shouldMatchEmotionSnapshot(
-    <Action name="lock" variant="primary">
-      Test
-    </Action>,
-  )
-})
-
-test('Action renders with rounded and tooltip', () => {
-  shouldMatchEmotionSnapshot(
-    <Action name="lock" variant="primary" rounded>
-      Test
-    </Action>,
-  )
-})
-
-test('Throw an error on Action without name of children props', () => {
-  try {
-    create(<Action />)
-  } catch (e) {
-    expect(e).toEqual(
-      new Error(
-        'Action component need to have either children (as string) or a name prop',
-      ),
+  test('Action renders with children ', () => {
+    shouldMatchEmotionSnapshot(
+      <Action name="lock" variant="primary">
+        Test
+      </Action>,
     )
-  }
+  })
+
+  test('Action renders with rounded and tooltip', () => {
+    shouldMatchEmotionSnapshot(
+      <Action name="lock" variant="primary" rounded>
+        Test
+      </Action>,
+    )
+  })
+
+  test('Throw an error on Action without name of children props', () => {
+    const spy = jest.spyOn(console, 'error').mockImplementation()
+    try {
+      create(<Action />)
+    } catch (e) {
+      expect(e).toEqual(
+        new Error(
+          'Action component need to have either children (as string) or a name prop',
+        ),
+      )
+    }
+    expect(console.error).toHaveBeenCalledTimes(1)
+    spy.mockRestore()
+  })
 })
