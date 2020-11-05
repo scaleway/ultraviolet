fe94ce84fc8f459107a4fff59af6a52228d1f246
Emmanuel Chambon <echambon@scaleway.com>
2020-11-05 17:11:55 +0100

    test: some eslint and unit tests

---
+++ src/components/Touchable/__tests__/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Touchable } from '..'
+import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
+
+describe('Touchable', () => {
+  test('renders correctly with defaults', () => {
+    shouldMatchEmotionSnapshot(<Touchable>Basic Touchable</Touchable>)
+  })
+
+  test('renders correctly disabled', () => {
+    shouldMatchEmotionSnapshot(<Touchable disabled>Basic Touchable</Touchable>)
+  })
+
+  test('renders correctly on focus', () => {
+    shouldMatchEmotionSnapshot(
+      <Touchable hasFocus activeOpacity={0.7}>
+        Basic Touchable
+      </Touchable>,
+    )
+  })
+
+  test('renders correctly with a non default as and type', () => {
+    shouldMatchEmotionSnapshot(
+      <Touchable as="div" type="button">
+        Button Touchable
+      </Touchable>,
+    )
+  })
+})
