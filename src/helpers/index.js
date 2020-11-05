2c19a071be47e8660a0b6eeb861413b5120c871e
Vincent Audebert <vaudebert@online.net>
2020-11-05 14:11:57 +0100

    feat(range): adds range component

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,1 +1,14 @@
 export * from './clipboard'
+
+export const parseIntOr = (str, fallback) => {
+  const value = Number.parseInt(str, 10)
+  return !Number.isNaN(value) ? value : fallback
+}
+
+export const onKeyOnlyNumbers = e => {
+  // `e.which` and `e.keyCode` are deprecated (https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent)
+  const keyCode = e.key.charCodeAt(0)
+  if (keyCode < 48 || keyCode > 57) {
+    e.preventDefault()
+  }
+}
