935d3eec1c6e5c6cdaf92c077965f038d73cecfd
Emmanuel Chambon <echambon@scaleway.com>
2020-11-07 22:43:27 +0100

    fix: coherent style

--- src/components/ExtendedReminder/index.js
+++ src/components/ExtendedReminder/index.js
@@ -25,6 +25,8 @@ const variants = {
 
 const styles = {
   container: variant => css`
+    display: flex;
+    flex-direction: column;
     border-radius: 4px;
     position: relative;
     padding: 16px;
@@ -38,11 +40,12 @@ const styles = {
     text-transform: uppercase;
   `,
   title: variant => css`
-    font-weight: 500;
+    font-weight: 600;
     text-transform: uppercase;
     color: ${variants[variant].main};
   `,
   link: css`
+    margin-top: auto;
     color: ${theme.blue};
     background-color: ${theme.transparent};
     padding: 0;
@@ -62,6 +65,7 @@ const ExtendedReminder = ({
   text,
   linkText,
   to,
+  onClick,
   variant,
   ...props
 }) => {
@@ -79,13 +83,20 @@ const ExtendedReminder = ({
           {badgeText}
         </Badge>
       </Box>
-      <Typography variant="bodyC" css={styles.title(variant)}>
+      <Typography mb={1} variant="bodyC" css={styles.title(variant)}>
         {title}
       </Typography>
-      <Typography mb={3} variant="bodyD">
+      <Typography mb={1} variant="bodyD">
         {text}
       </Typography>
-      <Button variant="link" to={to} icon="east" css={styles.link}>
+      <Button
+        variant="link"
+        to={to}
+        onClick={onClick}
+        size="xs"
+        icon="east"
+        css={styles.link}
+      >
         {linkText}
       </Button>
     </Box>
@@ -99,11 +110,14 @@ ExtendedReminder.propTypes = {
   title: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
   linkText: PropTypes.string.isRequired,
-  to: PropTypes.string.isRequired,
+  to: PropTypes.string,
+  onClick: PropTypes.func,
 }
 
 ExtendedReminder.defaultProps = {
   variant: 'info',
+  to: null,
+  onClick: null,
 }
 
 export { ExtendedReminder }
