f46e1de71f0990a0917b3aa1f2b0cc072110370c
Emmanuel Chambon <echambon@scaleway.com>
2020-11-09 11:10:00 +0100

    feat: port MarkDown component from shire [CNS-3438]

---
+++ src/components/MarkDown/__stories__/markdown_example.js
@@ -0,0 +1,24 @@
+export const simple = `
+  # I am a Markdown H1
+
+  ## I am a Markdown H2
+
+  I am a simple markdown with **strong** _italic_ content
+
+  You can find the complete reference [here](https://commonmark.org/help/)
+
+  I can also render code
+
+  \`const example = 1 // Inline code\`
+
+  \`\`\`
+    // Multiple lines
+    const example = 2
+  \`\`\`
+`
+
+export const withMultipleParagraphs = `
+  I am a paragraph.
+
+  And a second one
+`
