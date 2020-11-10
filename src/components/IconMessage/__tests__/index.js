d00dbdc35c0e351ee207d7296f6afdf92dac7c52
Alexandre Philibeaux <aphilibeaux@online.net>
2020-11-10 10:06:52 +0100

    test(component): add test to IconMessage

---
+++ src/components/IconMessage/__tests__/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { IconMessage } from '..'
+import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
+
+describe('IconMessage', () => {
+  test('renders correctly', () => {
+    shouldMatchEmotionSnapshot(<IconMessage>This is default.</IconMessage>)
+  })
+
+  test('renders correctly with variant and icon', () => {
+    shouldMatchEmotionSnapshot(
+      <IconMessage variant="primary" icon="information-outline">
+        This is variant
+      </IconMessage>,
+    )
+  })
+})
