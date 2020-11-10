6d2649aeb74678d9ef89ebb055a540c4d93f9f82
Emmanuel Chambon <echambon@scaleway.com>
2020-11-10 18:39:52 +0100

    feat: add Pentagon component

---
+++ src/components/Pentagon/index.js
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled'
+import PropTypes from 'prop-types'
+import { theme } from '../../theme'
+
+const Pentagon = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  clip-path: polygon(50% 0%, 100% 38%, 82% 100%, 18% 100%, 0% 38%);
+
+  ${({ size, color }) => `
+    width: ${size};
+    height: ${size};
+    background: ${color};
+  `}
+`
+
+Pentagon.propTypes = {
+  size: PropTypes.string,
+  color: PropTypes.string,
+}
+
+Pentagon.defaultProps = {
+  size: '48px',
+  color: theme.pippin,
+}
+
+export { Pentagon }
