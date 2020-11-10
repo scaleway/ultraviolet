5a74d27fee98ff76a307ecfb7cca89c184b058a7
Alexandre Philibeaux <aphilibeaux@online.net>
2020-11-10 10:00:16 +0100

    test(dotssteps): add snapshots

---
+++ src/components/DotSteps/__tests__/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import { DotSteps } from '..'
+import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
+
+const DotStepsComponent = () => {
+  const [step, setStep] = useState(2)
+  return <DotSteps steps={10} step={step} setStep={setStep} />
+}
+describe('DotSteps', () => {
+  test('renders correctly', () => {
+    shouldMatchEmotionSnapshot(<DotSteps setStep={() => {}} />)
+  })
+
+  test('renders correctly wit 10 steps, default step=4', () => {
+    shouldMatchEmotionSnapshot(
+      <DotSteps steps={10} step={4} setStep={() => {}} />,
+    )
+  })
+
+  test('renders correctly useState function', () => {
+    shouldMatchEmotionSnapshot(<DotStepsComponent />)
+  })
+})
