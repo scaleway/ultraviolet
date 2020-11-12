f7acf6a26a395b7cf48e3c643b0ca86c855cfd32
Emmanuel Chambon <echambon@scaleway.com>
2020-11-12 21:35:58 +0100

    feat: add Pentagon component

---
+++ src/components/Pentagon/__tests__/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Pentagon } from '..'
+import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
+
+describe('Pentagon', () => {
+  test(`renders with default props`, () => {
+    shouldMatchEmotionSnapshot(<Pentagon />)
+  })
+  test(`renders with children`, () => {
+    shouldMatchEmotionSnapshot(<Pentagon>X</Pentagon>)
+  })
+  test(`renders with custom color`, () => {
+    shouldMatchEmotionSnapshot(<Pentagon color="yellow">X</Pentagon>)
+  })
+  test(`renders with custom size`, () => {
+    shouldMatchEmotionSnapshot(<Pentagon size="200px">X</Pentagon>)
+  })
+})
