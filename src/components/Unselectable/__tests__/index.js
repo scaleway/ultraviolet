2c52510dccc6a9d4e102e4d0355486109c2069fd
Emmanuel Chambon <echambon@scaleway.com>
2020-11-13 16:39:20 +0100

    feat: migrate Label, ScrollView, StatusIndicator and Unselectable from shire

---
+++ src/components/Unselectable/__tests__/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Unselectable } from '..'
+import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
+
+describe('Unselectable', () => {
+  it(`renders correctly`, () => {
+    shouldMatchEmotionSnapshot(
+      <Unselectable>Can&apos;t touch this</Unselectable>,
+    )
+  })
+})
