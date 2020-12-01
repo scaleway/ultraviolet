0c973d83964934e8e924e55c10befcba3af48e50
Emmanuel Chambon <echambon@scaleway.com>
2020-12-01 20:20:46 +0100

    feat: port CategoryIcon from shire

---
+++ src/components/CategoryIcon/__tests__/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { CategoryIcon, availableIcons } from '..'
+import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
+
+describe('CategoryIcon', () => {
+  availableIcons.forEach(icon => {
+    test(`render ${icon}`, () => {
+      shouldMatchEmotionSnapshot(<CategoryIcon name={icon} />)
+    })
+  })
+
+  test(`render unknow`, () => {
+    const spy = jest.spyOn(console, 'error').mockImplementation()
+
+    shouldMatchEmotionSnapshot(<CategoryIcon name="unknow" />)
+    expect(console.error).toHaveBeenCalledTimes(1)
+    spy.mockRestore()
+  })
+})
