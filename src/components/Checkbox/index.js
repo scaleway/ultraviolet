b900f480cde9fc6c279cc05332b5ea1d28547ece
Alexandre Philibeaux <aphilibeaux@scaleway.com>
2020-12-02 18:41:57 +0100

    feat(checkbox): use reakit

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -1,14 +1,14 @@
 import { css } from '@emotion/core'
-import { SwitchState } from '@smooth-ui/core-em'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useCheckboxState, Checkbox as ReakitCheckbox } from 'reakit/Checkbox'
 import { borderRadius, gray100, gray550, primary } from '../../theming'
 import { cx } from '../../utils'
 import { ActivityIndicator } from '../ActivityIndicator'
 import { Box } from '../Box'
 import { Expandable } from '../Expandable'
 import { Icon } from '../Icon'
-import { Typography } from '../Typography'
+import { Typography, typographyVariants } from '../Typography'
 
 const styles = {
   container: css`
@@ -17,28 +17,25 @@ const styles = {
     align-items: flex-start;
   `,
   hover: p => css`
+    cursor: ${p.disabled ? 'not-allowed' : 'pointer'};
     &:hover {
       svg {
         border-radius: ${borderRadius(p)};
         background-color: ${!p.disabled && gray100(p)};
-        fill: ${primary(p)};
+        fill: ${!p.disabled && primary(p)};
         transition: fill 300ms;
       }
     }
   `,
   input: p => css`
+    opacity: 0;
+    width: 0;
     &:focus + svg {
       outline: 1px ${gray550(p)} dotted;
     }
   `,
   icon: css`
     box-sizing: content-box;
-  `,
-  enabled: css`
-    cursor: pointer;
-  `,
-  disabled: css`
-    cursor: not-allowed;
   `,
   children: size => css`
     padding-top: ${size > 27 ? '4px' : '2px'};
@@ -47,7 +44,6 @@ const styles = {
 
 export function Checkbox({
   checked,
-  defaultChecked,
   onChange,
   onFocus,
   onBlur,
@@ -55,86 +51,99 @@ export function Checkbox({
   error,
   name,
   value,
-  size = 24,
+  size,
   children,
   progress,
-  id,
   disabled,
-  typoVariant,
+  typographyVariant,
   ...props
 }) {
   const hasChildren = Boolean(children)
+  const checkbox = useCheckboxState({ state: checked })
+
+  useEffect(() => {
+    checkbox.setState(checked)
+  }, [checked])
+
   return (
-    <SwitchState
-      name={name}
-      value={value}
-      checked={checked}
-      defaultChecked={defaultChecked}
-      onChange={onChange}
-      onFocus={onFocus}
-      onBlur={onBlur}
-    >
-      {({ checked, input }) => (
-        <Box {...props}>
-          <Typography css={cx([styles.container, !disabled && styles.hover])}>
-            <input
-              css={cx([
-                styles.input,
-                !disabled ? styles.enabled : styles.disabled,
-              ])}
-              type="checkbox"
-              {...input}
-              id={id}
-              disabled={disabled}
-            />
-            {progress ? (
-              <ActivityIndicator size={size} mr={hasChildren ? 1 : 0} />
-            ) : (
-              <Icon
-                mr={hasChildren ? '10px' : 0}
-                p="2px"
-                css={styles.icon}
-                name={
-                  checked ? 'checkbox-marked-outline' : 'checkbox-blank-outline'
-                }
-                color={
-                  disabled
-                    ? 'gray100'
-                    : valid === false || Boolean(error)
-                    ? 'warning'
-                    : valid === true
-                    ? 'success'
-                    : checked
-                    ? 'primary'
-                    : 'gray300'
-                }
-                size={size}
-              />
-            )}
-            {hasChildren && <div css={styles.children(size)}>{children}</div>}
-          </Typography>
-          <Expandable
-            height={56}
-            overflow="hidden"
-            mt="-6px"
-            opened={Boolean(error)}
-          >
-            <Box fontSize={12} color="warning" px="4px">
-              {error}
-            </Box>
-          </Expandable>
+    <Box {...props}>
+      <Typography
+        as="label"
+        variant={typographyVariant}
+        css={cx([styles.container, styles.hover({ disabled })])}
+      >
+        <ReakitCheckbox
+          {...checkbox}
+          css={styles.input}
+          onChange={onChange}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          checked={checked}
+          disabled={disabled}
+          value={value}
+          name={name}
+        />
+        {progress ? (
+          <ActivityIndicator size={size} mr={hasChildren ? 1 : 0} />
+        ) : (
+          <Icon
+            mr={hasChildren ? '10px' : 0}
+            p="2px"
+            css={styles.icon}
+            name={
+              checkbox?.state
+                ? 'checkbox-marked-outline'
+                : 'checkbox-blank-outline'
+            }
+            color={
+              disabled
+                ? 'gray100'
+                : valid === false || Boolean(error)
+                ? 'warning'
+                : valid === true
+                ? 'success'
+                : checked
+                ? 'primary'
+                : 'gray300'
+            }
+            size={size}
+          />
+        )}
+        {hasChildren && <div css={styles.children(size)}>{children}</div>}
+      </Typography>
+      <Expandable height={56} overflow="hidden" opened={Boolean(error)}>
+        <Box fontSize={12} color="warning" px="4px">
+          {error}
         </Box>
-      )}
-    </SwitchState>
+      </Expandable>
+    </Box>
   )
 }
 
-Checkbox.propTypes = {
-  valid: PropTypes.bool,
-  progress: PropTypes.bool,
-  size: PropTypes.number,
-  children: PropTypes.node,
-  error: PropTypes.string,
+Checkbox.defaultProps = {
+  checked: false,
+  disabled: false,
+  onBlur: () => {},
+  progress: false,
+  name: 'checkbox',
+  typographyVariant: 'default',
+  size: 24,
 }
 
-export default Checkbox
+Checkbox.propTypes = {
+  checked: PropTypes.bool,
+  children: PropTypes.node,
+  disabled: PropTypes.bool,
+  onChange: PropTypes.func.isRequired,
+  onFocus: PropTypes.func.isRequired,
+  onBlur: PropTypes.func,
+  error: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  progress: PropTypes.bool,
+  name: PropTypes.string,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+  valid: PropTypes.bool,
+  size: PropTypes.number,
+  typographyVariant: PropTypes.oneOf(typographyVariants),
+}
+
+export default { Checkbox }
