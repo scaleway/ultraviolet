ba9a626104a43a18e63ad32e37e8e482d1ec66e5
Alexandre Philibeaux <aphilibeaux@scaleway.com>
2020-12-10 17:33:14 +0100

    feat(modal): migration reakit

---
+++ src/components/Modal/__tests__/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Modal } from '..'
+import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
+
+describe('Modal', () => {
+  test(`renders with default Props`, () => {
+    shouldMatchEmotionSnapshot(
+      <Modal>
+        <div>test</div>
+      </Modal>,
+    )
+  })
+
+  test(`renders with opened={true}`, () => {
+    shouldMatchEmotionSnapshot(
+      <Modal opened>
+        <div>test</div>
+      </Modal>,
+    )
+  })
+
+  test(`renders with disclosure`, () => {
+    shouldMatchEmotionSnapshot(
+      <Modal
+        ariaLabel="modal-test"
+        baseId="modal-test"
+        disclosure={() => <button type="button">Test</button>}
+      >
+        <div>modal</div>
+      </Modal>,
+    )
+  })
+  test(`renders with portal node (modal=false)`, () => {
+    shouldMatchEmotionSnapshot(
+      <Modal ariaLabel="modal-test" baseId="modal-test" modal={false}>
+        <div> test </div>
+      </Modal>,
+    )
+  })
+})
