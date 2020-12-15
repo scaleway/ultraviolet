55d41a3cd5b8198181a02f56715eeffa0ef1f87d
Alexandre Philibeaux <aphilibeaux@scaleway.com>
2020-12-15 11:24:16 +0000

    fix(modal): add animated props

--- src/components/Modal/__tests__/index.js
+++ src/components/Modal/__tests__/index.js
@@ -1,6 +1,14 @@
+import { css } from '@emotion/core'
 import React from 'react'
 import { Modal } from '..'
 import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
+
+const customDialogBackdropStyles = css`
+  background-color: aliceblue;
+`
+const customDialogStyles = css`
+  background: radial-gradient(circle, #8b2fe6 0%, #4f0599 50%, #30015a 100%);
+`
 
 describe('Modal', () => {
   test(`renders with default Props`, () => {
@@ -14,6 +22,18 @@ describe('Modal', () => {
   test(`renders with opened={true}`, () => {
     shouldMatchEmotionSnapshot(
       <Modal opened>
+        <div>test</div>
+      </Modal>,
+    )
+  })
+
+  test(`renders with customStyle`, () => {
+    shouldMatchEmotionSnapshot(
+      <Modal
+        opened
+        customDialogBackdropStyles={customDialogBackdropStyles}
+        customDialogStyles={customDialogStyles}
+      >
         <div>test</div>
       </Modal>,
     )
