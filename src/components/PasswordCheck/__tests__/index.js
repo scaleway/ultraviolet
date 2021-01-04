99094298b033c9b5563260f2eea6e2041c923603
Matthias <mprost@scaleway.com>
2021-01-04 16:40:15 +0100

    feat(passwordcheck): added tests

---
+++ src/components/PasswordCheck/__tests__/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PasswordCheck from '..'
+import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
+
+describe('PasswordCheck', () => {
+  test('render with default values', () => {
+    shouldMatchEmotionSnapshot(<PasswordCheck />)
+  })
+  test('render with custom values', () => {
+    shouldMatchEmotionSnapshot(
+      <PasswordCheck
+        rules={[
+          {
+            name: 'custom1',
+            valid: false,
+            text:
+              "That's a beautiful custom password check we have right there",
+          },
+          {
+            name: 'custom2',
+            valid: true,
+            text:
+              "That's a second beautiful custom password check we have right there",
+          },
+        ]}
+      />,
+    )
+  })
+})
