40c1690d19e0e03e89d30ab71333d0a5a41da0cc
Matthias Prost <mprost@online.net>
2021-01-08 16:06:36 +0100

    feat(PasswordCheck): migration of PasswordCheck CNS-2093

---
+++ src/components/PasswordStrengthMeter/__tests__/index.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import { PasswordStrengthMeter } from '..'
+import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
+
+describe('PasswordStrengthMeter', () => {
+  test('render with empty password', () => {
+    shouldMatchEmotionSnapshot(
+      <PasswordStrengthMeter
+        password=""
+        strength={[
+          { color: 'red', t: 'veryWeak' },
+          { color: 'orange', t: 'weak' },
+          { color: 'yellow', t: 'medium' },
+          { color: 'green', t: 'strong' },
+          { color: 'green', t: 'veryStrong' },
+        ]}
+        title="MyTitle"
+      />,
+    )
+  })
+
+  test('render with weak password', () => {
+    shouldMatchEmotionSnapshot(
+      <PasswordStrengthMeter
+        password="weak"
+        strength={[
+          { color: 'red', t: 'veryWeak' },
+          { color: 'orange', t: 'weak' },
+          { color: 'yellow', t: 'medium' },
+          { color: 'green', t: 'strong' },
+          { color: 'green', t: 'veryStrong' },
+        ]}
+        title="MyTitle"
+      />,
+    )
+  })
+
+  test('render with medium password', () => {
+    shouldMatchEmotionSnapshot(
+      <PasswordStrengthMeter
+        password="mediumpas"
+        strength={[
+          { color: 'red', t: 'veryWeak' },
+          { color: 'orange', t: 'weak' },
+          { color: 'yellow', t: 'medium' },
+          { color: 'green', t: 'strong' },
+          { color: 'green', t: 'veryStrong' },
+        ]}
+        title="MyTitle"
+      />,
+    )
+  })
+
+  test('render with strong password', () => {
+    shouldMatchEmotionSnapshot(
+      <PasswordStrengthMeter
+        password="strongpass{"
+        strength={[
+          { color: 'red', t: 'veryWeak' },
+          { color: 'orange', t: 'weak' },
+          { color: 'yellow', t: 'medium' },
+          { color: 'green', t: 'strong' },
+          { color: 'green', t: 'veryStrong' },
+        ]}
+        title="MyTitle"
+      />,
+    )
+  })
+
+  test('render with very strong password', () => {
+    shouldMatchEmotionSnapshot(
+      <PasswordStrengthMeter
+        password="verystrongpass{"
+        strength={[
+          { color: 'red', t: 'veryWeak' },
+          { color: 'orange', t: 'weak' },
+          { color: 'yellow', t: 'medium' },
+          { color: 'green', t: 'strong' },
+          { color: 'green', t: 'veryStrong' },
+        ]}
+        title="MyTitle"
+      />,
+    )
+  })
+})
