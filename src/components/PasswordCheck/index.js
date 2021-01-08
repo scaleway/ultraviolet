3db291b4a4c3d9989cf3347790ca3f132e746144
Matthias Prost <mprost@online.net>
2021-01-08 16:06:36 +0100

    feat(PasswordCheck): migration of PasswordCheck CNS-2093

---
+++ src/components/PasswordCheck/index.js
@@ -0,0 +1,54 @@
+import { css } from '@emotion/core'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { Col, Grid, Row } from '../Grid'
+import { Icon } from '../Icon'
+
+const styles = {
+  condition: css`
+    display: flex;
+    align-items: center;
+    margin: 0 0 8px 0;
+    font-size: 14px;
+  `,
+  icon: css`
+    margin-right: 8px;
+    align-self: center;
+  `,
+}
+
+function PasswordCheck({ rules }) {
+  return (
+    <Grid fluid gutter={0}>
+      <Row>
+        {rules.map(rule => (
+          <Col lg={6} key={rule.name}>
+            <p css={styles.condition}>
+              <Icon
+                name={
+                  rule.valid ? 'check-circle-outline' : 'close-circle-outline'
+                }
+                color={rule.valid ? 'success' : 'darkGrey'}
+                css={styles.icon}
+                size={20}
+              />
+              {rule.text}
+            </p>
+          </Col>
+        ))}
+      </Row>
+    </Grid>
+  )
+}
+
+PasswordCheck.propTypes = {
+  rules: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      valid: PropTypes.bool.isRequired,
+      text: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+}
+
+export { PasswordCheck }
