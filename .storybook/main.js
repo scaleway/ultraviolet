73eb38828811e9112a13c24baeaa235e8d9cfac4
Emmanuel Chambon <echambon@scaleway.com>
2021-01-13 11:16:43 +0100

    chore(deploy): correct first page not loading correctly

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
   stories: ['../src/**/__stories__/*.stories.mdx'],
   addons: [
+    '@storybook/addon-docs',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-docs',
   ],
 }
