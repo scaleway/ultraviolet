83480b2dfc8f6a8a074a9c490e648f2d8b5a94c5
Emmanuel Chambon <echambon@scaleway.com>
2021-01-19 13:37:08 +0100

    feat: migrate SwitchButton to reakit

---
+++ src/components/SwitchButton/__tests__/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { SwitchButton } from '..'
+import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
+
+describe('SwitchButton', () => {
+  test('renders correctly', () => {
+    shouldMatchEmotionSnapshot(
+      <SwitchButton onChange={() => {}} name="radio" value="choice">
+        Choice
+      </SwitchButton>,
+    )
+  })
+
+  test('renders correctly with variant segment', () => {
+    shouldMatchEmotionSnapshot(
+      <SwitchButton
+        onChange={() => {}}
+        name="radio"
+        value="choice"
+        variant="segment"
+      >
+        Choice
+      </SwitchButton>,
+    )
+  })
+
+  test('renders correctly when disabled', () => {
+    shouldMatchEmotionSnapshot(
+      <SwitchButton onChange={() => {}} name="radio" value="choice" disabled>
+        Choice
+      </SwitchButton>,
+    )
+  })
+
+  test('renders correctly with complex child', () => {
+    shouldMatchEmotionSnapshot(
+      <SwitchButton
+        onChange={() => {}}
+        name="radio"
+        value="choice"
+        disabled
+        checked
+      >
+        {({ checked, disabled }) => `${checked} ${disabled}`}
+      </SwitchButton>,
+    )
+  })
+})
