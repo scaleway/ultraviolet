320bb0d9fc16d09af5ab683e7ef3533956eed09d
Emmanuel Chambon <echambon@scaleway.com>
2021-01-19 17:05:18 +0100

    feat: migrate Radio to reakit

---
+++ src/components/Radio/__tests__/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Radio } from '..'
+import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
+
+describe('Radio', () => {
+  test('renders correctly', () => {
+    shouldMatchEmotionSnapshot(
+      <Radio onChange={() => {}} name="radio" value="choice">
+        Choice
+      </Radio>,
+    )
+  })
+
+  test('renders correctly when disabled', () => {
+    shouldMatchEmotionSnapshot(
+      <Radio onChange={() => {}} name="radio" value="choice" disabled>
+        Choice
+      </Radio>,
+    )
+  })
+
+  test('renders correctly when checked', () => {
+    shouldMatchEmotionSnapshot(
+      <Radio onChange={() => {}} name="radio" value="choice" checked>
+        Choice
+      </Radio>,
+    )
+  })
+})
