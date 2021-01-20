215a1eb8390ba4fbf75d10d20846aadceef67a37
Dorian MALISZEWSKI <dmaliszewski@scaleway.com>
2021-01-20 13:03:33 +0100

    feat: migration of volumesize - CNS-2118

---
+++ src/components/VolumeSize/__tests__/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { VolumeSize } from '..'
+import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
+
+describe('VolumeSize', () => {
+  test('renders correctly with minimal props', () => {
+    shouldMatchEmotionSnapshot(<VolumeSize minSize={0} value={5} unit="GB" />)
+  })
+
+  test('renders correctly with all props', () => {
+    shouldMatchEmotionSnapshot(
+      <VolumeSize
+        maxLabel="max"
+        maxSize={20}
+        minLabel="min"
+        minSize={0}
+        requiredLabel="req"
+        title="Test"
+        unit="GB"
+        tooBigMessage="Test too big"
+        tooSmallMessage="Test too small"
+        value={15}
+      />,
+    )
+  })
+
+  test('renders correctly without maxSize', () => {
+    shouldMatchEmotionSnapshot(
+      <VolumeSize
+        minSize={0}
+        title="Test"
+        unit="GB"
+        tooBigMessage="Test too big"
+        tooSmallMessage="Test too small"
+        value={15}
+      />,
+    )
+  })
+
+  test('should display too small error', () => {
+    shouldMatchEmotionSnapshot(
+      <VolumeSize
+        minSize={20}
+        title="Test"
+        unit="GB"
+        tooBigMessage="Test too big"
+        tooSmallMessage="Test too small"
+        value={15}
+      />,
+    )
+  })
+
+  test('should display too big error', () => {
+    shouldMatchEmotionSnapshot(
+      <VolumeSize
+        minSize={0}
+        maxSize={20}
+        title="Test"
+        unit="GB"
+        tooBigMessage="Test too big"
+        tooSmallMessage="Test too small"
+        value={25}
+      />,
+    )
+  })
+})
