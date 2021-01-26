87a22b319718d752d3acdacec7effb32ca18a25c
Emmanuel Chambon <echambon@scaleway.com>
2021-01-26 15:15:02 +0100

    fix: correct overflowing ProgressBar

--- src/components/ProgressBar/__tests__/index.js
+++ src/components/ProgressBar/__tests__/index.js
@@ -18,4 +18,12 @@ describe('ProgressBar', () => {
   it(`renders progression`, () => {
     shouldMatchEmotionSnapshot(<ProgressBar progress />)
   })
+
+  it(`renders correctly when value > 100`, () => {
+    shouldMatchEmotionSnapshot(<ProgressBar value={250} />)
+  })
+
+  it(`renders correctly when value < 0`, () => {
+    shouldMatchEmotionSnapshot(<ProgressBar value={-250} />)
+  })
 })
