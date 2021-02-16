8e8b8fe7dc17eb8734705dd26a83a8a7e1f99474
Dorian Maliszewski <dmaliszewski@scaleway.com>
2021-02-16 16:55:07 +0100

    refactor: drop unused file

--- src/__stories__/helpers.js
+++
@@ -1,9 +0,0 @@
-export const showAlert = (text = 'fired!') => {
-  // eslint-disable-next-line no-alert
-  alert(text)
-}
-
-export const log = (text = 'fired!') => {
-  // eslint-disable-next-line no-console
-  console.log(text)
-}
