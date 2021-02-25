9b93e932ed9a99b71f1125ca0258ef5a7760e141
Emmanuel Chambon <echambon@scaleway.com>
2021-02-25 16:19:27 +0100

    test: deterministic random

--- src/components/Placeholder/__tests__/index.js
+++ src/components/Placeholder/__tests__/index.js
@@ -3,6 +3,14 @@ import { Placeholder, placeholderTypes } from '..'
 import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
 
 describe('Placeholder', () => {
+  beforeAll(() => {
+    jest.spyOn(global.Math, 'random').mockReturnValue(0.4155913669444804)
+  })
+
+  afterAll(() => {
+    jest.spyOn(global.Math, 'random').mockRestore()
+  })
+
   test.each(placeholderTypes)('renders correctly with type="%s"', type => {
     shouldMatchEmotionSnapshot(<Placeholder type={type} />)
   })
