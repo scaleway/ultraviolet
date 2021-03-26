a0dcb9eeacfaf6457a6adc4a94b773ca17f7f3b0
Dorian Maliszewski <dmaliszewski@scaleway.com>
2021-03-26 11:50:04 +0100

    refactor: use colors from theme instead of import 2 (#152)
    * feat: use colors from theme instead of import
    
    * fix: rebase with new typography

--- src/components/Description/__tests__/index.js
+++ src/components/Description/__tests__/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Description } from '..'
+import Description from '..'
 import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
 
 describe('Description', () => {
