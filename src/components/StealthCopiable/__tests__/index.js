b631fd13cb73922f2f7c2df06405ede183d9a820
Manu Chambon <manu@ehma.nu>
2021-03-31 09:48:50 +0200

    feat: port StealthCopiable (#195)
    * feat: port StealthCopiable
    
    * fix: correct lint
    
    * Update src/components/StealthCopiable/__stories__/index.stories.mdx
    
    Co-authored-by: Matthias Prost <mprost@scaleway.com>
    
    * test: ensure text is copied
    
    Co-authored-by: Matthias Prost <mprost@scaleway.com>

---
+++ src/components/StealthCopiable/__tests__/index.js
@@ -0,0 +1,37 @@
+import fireEvent from '@testing-library/user-event'
+import React from 'react'
+import StealthCopiable from '..'
+import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
+
+describe('StealthCopiable', () => {
+  beforeAll(() => {
+    let data = ''
+
+    window.clipboardData = {
+      setData: jest.fn((_, val) => { console.log(val); data = val }),
+      getData: jest.fn(() => data)
+    }
+  });
+
+  it('should renders correctly', () => {
+    shouldMatchEmotionSnapshot(
+      <StealthCopiable>Hello</StealthCopiable>,
+    )
+  })
+
+  it('should renders correctly with custom side', () => {
+    shouldMatchEmotionSnapshot(
+      <StealthCopiable side="left">Hello</StealthCopiable>,
+    )
+  })
+
+  it('should renders correctly with custom side', () => {
+    shouldMatchEmotionSnapshot(
+      <StealthCopiable side="left">Hello</StealthCopiable>,
+      { transform: ({ getByText }) => {
+        fireEvent.click(getByText('Copy'))
+        expect(window.clipboardData.getData()).toBe('Hello')
+      }}
+    )
+  })
+})
