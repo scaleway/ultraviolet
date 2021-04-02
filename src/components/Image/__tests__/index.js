7bc518d0939edd70e8cb6473b0b0e04c8e9579a9
Manu Chambon <manu@ehma.nu>
2021-04-02 15:15:52 +0200

    feat: port Image component (#203)

---
+++ src/components/Image/__tests__/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Image from '..'
+import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
+
+describe('Image', () => {
+  test(`render correctly`, () => {
+    shouldMatchEmotionSnapshot(<Image src="//toto.png" />)
+  })
+})
