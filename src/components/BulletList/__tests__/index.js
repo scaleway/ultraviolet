17862631153c230fe891b18910ec02a2c1cda54f
Emmanuel Chambon <echambon@scaleway.com>
2021-04-06 17:07:44 +0200

    feat: port ActionBar, BulletList and Container

---
+++ src/components/BulletList/__tests__/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import BulletList from '..'
+import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
+
+describe('BulletList', () => {
+  test('renders correctly ', () => {
+    shouldMatchEmotionSnapshot(
+      <BulletList>
+        <div>a</div>
+        <div>b</div>
+        <div>c</div>
+      </BulletList>,
+    )
+  })
+
+  test('renders correctly with custom keyPrefix', () => {
+    shouldMatchEmotionSnapshot(
+      <BulletList keyPrefix="test">
+        <div>a</div>
+        <div>b</div>y<div>c</div>
+      </BulletList>,
+    )
+  })
+})
