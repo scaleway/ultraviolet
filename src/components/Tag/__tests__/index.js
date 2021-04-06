7bf27407562dc42126ee9e366efdf7464693fe5f
Dorian Maliszewski <dmaliszewski@scaleway.com>
2021-04-06 10:05:39 +0200

    refactor: use colors from theme t components part one

--- src/components/Tag/__tests__/index.js
+++ src/components/Tag/__tests__/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Tag } from '..'
+import Tag from '..'
 import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
 
 describe('Tag', () => {
