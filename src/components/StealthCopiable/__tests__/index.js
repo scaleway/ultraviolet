0100bfe43e8b04074b9a9b8477d534aefd89cd67
Emmanuel Chambon <echambon@scaleway.com>
2021-04-07 21:54:57 +0200

    fix(StealthCopiable): allow component as children instead of only string

--- src/components/StealthCopiable/__tests__/index.js
+++ src/components/StealthCopiable/__tests__/index.js
@@ -3,20 +3,22 @@ import React from 'react'
 import StealthCopiable from '..'
 import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
 
+const ComplexChild = ({ children }) => <div>{children}</div>
+
 describe('StealthCopiable', () => {
   beforeAll(() => {
     let data = ''
 
     window.clipboardData = {
-      setData: jest.fn((_, val) => { console.log(val); data = val }),
-      getData: jest.fn(() => data)
+      setData: jest.fn((_, val) => {
+        data = val
+      }),
+      getData: jest.fn(() => data),
     }
-  });
+  })
 
   it('should renders correctly', () => {
-    shouldMatchEmotionSnapshot(
-      <StealthCopiable>Hello</StealthCopiable>,
-    )
+    shouldMatchEmotionSnapshot(<StealthCopiable>Hello</StealthCopiable>)
   })
 
   it('should renders correctly with custom side', () => {
@@ -28,10 +30,28 @@ describe('StealthCopiable', () => {
   it('should renders correctly with custom side', () => {
     shouldMatchEmotionSnapshot(
       <StealthCopiable side="left">Hello</StealthCopiable>,
-      { transform: ({ getByText }) => {
-        fireEvent.click(getByText('Copy'))
-        expect(window.clipboardData.getData()).toBe('Hello')
-      }}
+      {
+        transform: ({ getByText }) => {
+          fireEvent.click(getByText('Copy'))
+          expect(window.clipboardData.getData()).toBe('Hello')
+        },
+      },
+    )
+  })
+
+  it('should renders correctly with a complex children', () => {
+    shouldMatchEmotionSnapshot(
+      <StealthCopiable side="left">
+        <ComplexChild>
+          <span>Hello</span>
+        </ComplexChild>
+      </StealthCopiable>,
+      {
+        transform: ({ getByText }) => {
+          fireEvent.click(getByText('Copy'))
+          expect(window.clipboardData.getData()).toBe('Hello')
+        },
+      },
     )
   })
 })
