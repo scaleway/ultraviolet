f4f4b91df0735504cdbf0745d35abec05149a30e
Manu Chambon <manu@ehma.nu>
2021-05-17 10:19:20 +0200

    feat(UniversalLink): add mailto native support (#331)

--- src/components/Link/__tests__/index.js
+++ src/components/Link/__tests__/index.js
@@ -3,16 +3,21 @@ import Link, { linkVariants } from '..'
 import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
 
 describe('Link', () => {
+  test(`render correctly with no variant`, () => {
+    shouldMatchEmotionSnapshot(<Link href="/">Hello</Link>)
+  })
+
   describe('variant', () => {
-    linkVariants.forEach(variant => {
-      test(`render ${variant}`, () => {
+    test.each(linkVariants.map(variant => [`render ${variant}`, variant]))(
+      '%s',
+      (_, variant) => {
         shouldMatchEmotionSnapshot(
           <Link to="/" variant={variant}>
             Hello
           </Link>,
         )
-      })
-    })
+      },
+    )
   })
 
   test(`render correctly with target blank`, () => {
