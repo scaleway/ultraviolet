4ce02504f3fb0b5f16c1ec17cd5aae78da1a1f10
Alexandre Philibeaux <aphilibeaux@scaleway.com>
2021-05-28 12:16:35 +0200

    fix(action): add forwardRef to button

--- src/components/Action/index.js
+++ src/components/Action/index.js
@@ -1,6 +1,6 @@
 import { css } from '@emotion/react'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { forwardRef } from 'react'
 import Button, { buttonVariants } from '../Button'
 import Icon, { icons } from '../Icon'
 import Tooltip from '../Tooltip'
@@ -17,22 +17,24 @@ const action = rounded => css`
   ${rounded && `border-radius: 16px;`}
 `
 
-const Action = ({ name, size, children, tooltip, rounded, ...props }) => {
-  if (!name && !children) {
-    throw new Error(
-      'Action component need to have either children (as string) or a name prop',
-    )
-  }
+const Action = forwardRef(
+  ({ name, size, children, tooltip, rounded, ...props }, ref) => {
+    if (!name && !children) {
+      throw new Error(
+        'Action component need to have either children (as string) or a name prop',
+      )
+    }
 
-  return (
-    <Tooltip text={tooltip}>
-      <Button css={action(rounded)} {...props}>
-        {name && <Icon size={size} name={name} />}
-        {children}
-      </Button>
-    </Tooltip>
-  )
-}
+    return (
+      <Tooltip text={tooltip}>
+        <Button ref={ref} css={action(rounded)} {...props}>
+          {name && <Icon size={size} name={name} />}
+          {children}
+        </Button>
+      </Tooltip>
+    )
+  },
+)
 
 Action.propTypes = {
   children: PropTypes.node,
