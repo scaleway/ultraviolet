51ab7f83f075532d0dd3df87ebe629834053270b
Vincent Audebert <vaudebert@scaleway.com>
2021-06-14 12:13:53 +0200

    refactor: migrate command into typography CNS-4040

--- src/components/Command/__tests__/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Command from '..'
-import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
-
-describe('Commands', () => {
-  test('renders correctly', () =>
-    shouldMatchEmotionSnapshot(<Command>halt</Command>))
-})
