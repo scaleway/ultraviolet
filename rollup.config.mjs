616cb0e8fd487f298c61a04019dc0043f2a1a036
Emmanuel Chambon <echambon@scaleway.com>
2021-07-07 18:17:02 +0200

    build: rollup + babel + @babel/typescript + rollup-plugin-dts

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,67 +5,73 @@ import svgr from '@svgr/rollup'
 import { readPackageAsync } from 'read-pkg'
 import postcss from 'rollup-plugin-postcss'
 import { visualizer } from 'rollup-plugin-visualizer'
+import dts from "rollup-plugin-dts"
 
 const PROFILE = !!process.env.PROFILE
 
-export default async () => {
-  const pkg = await readPackageAsync()
+const pkg = await readPackageAsync()
 
-  const targets = `
-    > 1%,
-    last 2 versions,
-    not IE > 0,
-    not IE_Mob > 0
-  `
+const targets = `
+  > 1%,
+  last 2 versions,
+  not IE > 0,
+  not IE_Mob > 0
+`
 
-  const external = id =>
-    !id.endsWith('.css') &&
-    !id.endsWith('.png') &&
-    [
-      '@emotion',
-      ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.peerDependencies || {}),
-    ].find(dep => new RegExp(dep).test(id))
+const external = id =>
+  !id.endsWith('.css') &&
+  !id.endsWith('.png') &&
+  [
+    '@emotion',
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ].find(dep => new RegExp(dep).test(id))
 
-  return {
-    external,
-    input: './src/index.js',
-    output: {
-      file: 'dist/module.js',
-      format: 'es',
-    },
-    plugins: [
-      babel({
-        babelHelpers: 'runtime',
-        babelrc: false,
-        exclude: 'node_modules/**',
-        plugins: [
-          'babel-plugin-annotate-pure-calls',
-          '@babel/plugin-transform-runtime',
-        ],
-        presets: [
-          ['@babel/env', { loose: true, modules: false, targets }],
-          '@babel/preset-react',
-          ['@emotion/babel-preset-css-prop', { sourceMap: false }],
-        ],
+export default [{
+  external,
+  input: './src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'es',
+  },
+  plugins: [
+    babel({
+      babelHelpers: 'runtime',
+      babelrc: false,
+      exclude: 'node_modules/**',
+      plugins: [
+        'babel-plugin-annotate-pure-calls',
+        '@babel/plugin-transform-runtime',
+      ],
+      presets: [
+        '@babel/preset-typescript',
+        ['@babel/env', { loose: true, modules: false, targets }],
+        '@babel/preset-react',
+        ['@emotion/babel-preset-css-prop', { sourceMap: false }],
+      ],
+    }),
+    postcss({
+      inject: false,
+    }),
+    nodeResolve({
+      preferBuiltins: true,
+    }),
+    url({
+      limit: 63488,
+    }),
+    svgr.default({ memo: true }),
+    PROFILE &&
+      visualizer({
+        brotliSize: true,
+        filename: '.reports/index.html',
+        gzipSize: true,
+        open: true,
       }),
-      postcss({
-        inject: false,
-      }),
-      nodeResolve({
-        preferBuiltins: true,
-      }),
-      url({
-        limit: 63488,
-      }),
-      svgr.default({ memo: true }),
-      PROFILE &&
-        visualizer({
-          brotliSize: true,
-          filename: '.reports/index.html',
-          gzipSize: true,
-          open: true,
-        }),
     ].filter(Boolean),
-  }
-}
+  },
+  {
+    input: "./src/index.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+]
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from './components'
+export { default as theme } from './theme'
+export * from './utils'
