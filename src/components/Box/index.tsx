c1ba7c0ea8a351da639250d8245b329e09df2fef
Adrien Gibrat <agibrat@eleven-labs.com>
2021-08-09 10:38:04 +0200

    fix: use simpler ts types hacks for Box component
    ensure consistents emotion css classNames
    & reset tests snapshots

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,54 +1,50 @@
-import { Theme, css } from '@emotion/react'
-import styled from '@emotion/styled'
+import { css } from '@emotion/react'
+import styled, { StyledComponent } from '@emotion/styled'
 import { x } from '@xstyled/emotion'
 import PropTypes from 'prop-types'
 import React, {
   ElementType,
-  FunctionComponent,
-  ReactNode,
+  HTMLAttributes,
+  RefAttributes,
   forwardRef,
 } from 'react'
-
-const borderedStyles = ({ theme }: { theme: Theme }) => css`
-  padding: ${theme.space['3']};
-  border-radius: ${theme.radii.default};
-  border: 1px solid ${theme.colors.gray350};
-`
-
-const StyledBox = styled(x.div, {
-  shouldForwardProp: prop => !['bordered'].includes(prop.toString()),
-})<{ bordered?: boolean }>`
-  ${({ bordered }) => (bordered ? borderedStyles : null)}
-`
 
 type BoxProps = {
   bordered?: boolean
   width?: number | string
   height?: number | string
-  children?: ReactNode
-} & XStyledProps &
-  React.HTMLAttributes<HTMLElement>
-
-const forwardType = forwardRef<Element, BoxProps>(() => null)
-
-type BoxType = typeof forwardType & {
-  withComponent: (
-    element: string | ElementType<unknown>,
-  ) => FunctionComponent<BoxProps>
 }
 
-// @ts-expect-error We add withComponent just below
-const Box: BoxType = forwardRef<Element, BoxProps>(
-  ({ width, height, bordered = false, ...props }, ref) => (
-    // @ts-expect-error As we won't know the Element kind we can't assume that Ref will be a Element
-    <StyledBox ref={ref} w={width} h={height} bordered={bordered} {...props} />
-  ),
-)
+const StyledBox = styled(x.div, {
+  shouldForwardProp: prop => prop !== 'bordered',
+})<BoxProps>`
+  ${({ bordered, theme }) =>
+    bordered
+      ? css`
+          padding: ${theme.space['3']};
+          border-radius: ${theme.radii.default};
+          border: 1px solid ${theme.colors.gray350};
+        `
+      : null}
+`
 
-Box.withComponent =
-  (element: string | ElementType<unknown>): FunctionComponent<BoxProps> =>
-  props =>
+type AllBoxProps = BoxProps &
+  XStyledProps &
+  HTMLAttributes<Element> &
+  RefAttributes<Element>
+
+const Box = forwardRef(
+  // map props `width` to w & `height` to h, see https://xstyled.dev/docs/upgrade-guide/#width-becomes-w-height-becomes-h
+  ({ width, height, ...props }: BoxProps, ref) => (
+    // @ts-expect-error As we won't know the Element kind we can't assume what Ref will be
+    <StyledBox ref={ref} w={width} h={height} {...props} />
+  ),
+) as unknown as StyledComponent<AllBoxProps>
+
+Box.withComponent = (element: string | ElementType<unknown>) =>
+  ((props: AllBoxProps) => (
     <Box as={element} {...props} />
+  )) as unknown as StyledComponent<AllBoxProps>
 
 Box.propTypes = {
   bordered: PropTypes.bool,
