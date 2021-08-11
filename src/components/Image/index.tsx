3debfc6a6c0fc7342b7227c9a41256e1dd1c4533
Emmanuel Chambon <echambon@scaleway.com>
2021-08-11 16:37:56 +0200

    fix: correct annotation

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -2,10 +2,10 @@ import PropTypes from 'prop-types'
 import React, { FunctionComponent } from 'react'
 import Box from '../Box'
 
-// @ts-expect-error Box expose Element, not ImgElement
 const Image: FunctionComponent<HTMLImageElement & XStyledProps> = ({
   src,
   ...props
+  // @ts-expect-error Box expose Element, not ImgElement
 }) => <Box as="img" src={src} {...props} />
 
 Image.propTypes = {
