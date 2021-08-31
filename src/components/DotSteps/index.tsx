4fe56c9ecec16a45a58b8cc098bdc96ed35d8772
Adrien Gibrat <agibrat@eleven-labs.com>
2021-08-31 17:27:55 +0200

    feat(DotSteps): update component dots style

--- src/components/DotSteps/index.tsx
+++ src/components/DotSteps/index.tsx
@@ -1,8 +1,40 @@
-import { css } from '@emotion/react'
+import styled from '@emotion/styled'
 import PropTypes from 'prop-types'
 import React, { FunctionComponent } from 'react'
 import Dot from '../Dot'
 import FlexBox from '../FlexBox'
+
+const DotStep = styled(Dot)`
+  margin: ${({ theme }) => theme.space[1]};
+  color: ${({ theme }) => theme.colors.primary};
+  cursor: pointer;
+  position: relative;
+  &::before,
+  &::after {
+    position: absolute;
+    border-radius: inherit;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  &::after {
+    content: '';
+    width: 18px;
+    height: 18px;
+  }
+  &[aria-selected='true'] {
+    background-color: transparent;
+    &::before {
+      content: '';
+      width: 5px;
+      height: 5px;
+      background-color: currentColor;
+    }
+    &::after {
+      border: 1px solid currentColor;
+    }
+  }
+`
 
 type Props = {
   setStep(index: number): void
@@ -17,14 +49,10 @@ const DotSteps: FunctionComponent<Props> = ({
 }) => (
   <FlexBox justifyContent="center">
     {Array.from({ length: steps }, (_, i) => (
-      <Dot
+      <DotStep
         key={`dot-step-${i + 1}`}
-        color={step === i + 1 ? 'primary' : 'gray'}
-        mr={i + 1 < steps ? 1 : 0}
+        aria-selected={step === i + 1}
         onClick={() => setStep(i + 1)}
-        css={css`
-          cursor: pointer;
-        `}
       />
     ))}
   </FlexBox>
