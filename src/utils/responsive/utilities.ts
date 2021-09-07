3a2e83eab8775a1f0f7b71fb3ce8ee561c626d3b
Manu Chambon <manu@ehma.nu>
2021-09-07 09:21:26 +0200

    fix: correct css 1px gap when using responsive utilities (#700)

--- src/utils/responsive/utilities.ts
+++ src/utils/responsive/utilities.ts
@@ -9,5 +9,5 @@ export const up = (size: ScreenSize, rules: string): string => {
 export const down = (size: ScreenSize, rules: string): string => {
   if (!(size in screens)) return ''
 
-  return `@media (max-width: ${screens[size]}px) { ${rules} }`
+  return `@media (max-width: ${screens[size] - 1}px) { ${rules} }`
 }
