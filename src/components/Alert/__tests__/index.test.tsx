ac097fcb4ef303748c3895afa1df05e294b53748
Tom Lienard <tlienard@scaleway.com>
2021-09-15 11:37:46 +0200

    feat(alert): fix codecov errors

--- src/components/Alert/__tests__/index.test.tsx
+++ src/components/Alert/__tests__/index.test.tsx
@@ -1,8 +1,13 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import React from 'react'
 import Alert, { alertTypes, alertVariants } from '..'
 import shouldMatchEmotionSnapshot from '../../../helpers/shouldMatchEmotionSnapshot'
 
 describe('Alert', () => {
+  beforeAll(() => {
+    jest.spyOn(console, 'error').mockImplementation(() => null)
+  })
+
   test('renders correctly with default values', () =>
     shouldMatchEmotionSnapshot(<Alert>Sample Alert</Alert>))
 
@@ -17,8 +22,16 @@ describe('Alert', () => {
       shouldMatchEmotionSnapshot(<Alert variant={variant}>Sample Alert</Alert>))
   })
 
+  test(`renders correctly unknow variant and fallback to standard`, () =>
+    // @ts-ignore
+    shouldMatchEmotionSnapshot(<Alert variant="unknow">Sample Alert</Alert>))
+
   alertTypes.forEach(type => {
     test(`renders correctly variant ${type}`, () =>
       shouldMatchEmotionSnapshot(<Alert type={type}>Sample Alert</Alert>))
   })
+
+  test(`renders correctly unknow theme type and fallback to defaults`, () =>
+    // @ts-ignore
+    shouldMatchEmotionSnapshot(<Alert type="unknow">Sample Alert</Alert>))
 })
