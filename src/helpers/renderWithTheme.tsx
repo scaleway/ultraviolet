0f2f48856daaa974d868cdf65171609fed7f9ecb
Tom Lienard <tlienard@scaleway.com>
2021-10-12 17:10:52 +0200

    feat(tests): use jest-helpers

--- src/helpers/renderWithTheme.tsx
+++ src/helpers/renderWithTheme.tsx
@@ -1,24 +1,27 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import createCache from '@emotion/cache'
-import { CacheProvider, ThemeProvider } from '@emotion/react'
-import { RenderOptions, render } from '@testing-library/react'
-import React, { ReactNode } from 'react'
-import defaultTheme from '../theme'
+import { renderWithTheme } from './jestHelpers'
 
-const emotionCache = createCache({
-  key: 'cache',
-})
+export default renderWithTheme
+// /* eslint-disable import/no-extraneous-dependencies */
+// import createCache from '@emotion/cache'
+// import { CacheProvider, ThemeProvider } from '@emotion/react'
+// import { RenderOptions, render } from '@testing-library/react'
+// import React, { ReactNode } from 'react'
+// import defaultTheme from '../theme'
 
-emotionCache.compat = true
+// const emotionCache = createCache({
+//   key: 'cache',
+// })
 
-export default (
-  component: ReactNode,
-  options?: RenderOptions,
-  theme: typeof defaultTheme = defaultTheme,
-): ReturnType<typeof render> =>
-  render(
-    <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={theme}>{component}</ThemeProvider>
-    </CacheProvider>,
-    options,
-  )
+// emotionCache.compat = true
+
+// export default (
+//   component: ReactNode,
+//   options?: RenderOptions,
+//   theme: typeof defaultTheme = defaultTheme,
+// ): ReturnType<typeof render> =>
+//   render(
+//     <CacheProvider value={emotionCache}>
+//       <ThemeProvider theme={theme}>{component}</ThemeProvider>
+//     </CacheProvider>,
+//     options,
+//   )
--- src/helpers/shouldMatchEmotionSnapshotWithPortal.ts
+++ src/helpers/shouldMatchEmotionSnapshotWithPortal.ts
@@ -1,41 +1,45 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { createSerializer } from '@emotion/jest'
-import { RenderOptions, render } from '@testing-library/react'
-import { ReactNode } from 'react'
-import defaultTheme from '../theme'
-import renderWithTheme from './renderWithTheme'
+import { shouldMatchEmotionSnapshotWithPortal } from './jestHelpers'
 
-// use only class hash (generated from css style content)
-expect.addSnapshotSerializer(
-  createSerializer({ classNameReplacer: className => className }),
-)
+export default shouldMatchEmotionSnapshotWithPortal
 
-interface Options {
-  options?: RenderOptions
-  theme?: typeof defaultTheme
-  transform?: (node: ReturnType<typeof render>) => Promise<void> | void
-}
+// /* eslint-disable import/no-extraneous-dependencies */
+// import { createSerializer } from '@emotion/jest'
+// import { RenderOptions, render } from '@testing-library/react'
+// import { ReactNode } from 'react'
+// import defaultTheme from '../theme'
+// import renderWithTheme from './renderWithTheme'
 
-export default async (
-  component: ReactNode,
-  { transform, options, theme }: Options = {},
-): Promise<void> => {
-  // Save the instance of console (disable warning about adding element directly to document.body which is necessary when testing portal components)
-  const { console } = global
-  global.console = { ...console, error: jest.fn() }
+// // use only class hash (generated from css style content)
+// expect.addSnapshotSerializer(
+//   createSerializer({ classNameReplacer: className => className }),
+// )
 
-  const node = renderWithTheme(
-    component,
-    {
-      container: document.body,
-      ...options,
-    },
-    theme,
-  )
-  if (transform) await transform(node)
-  expect(node.asFragment()).toMatchSnapshot()
+// interface Options {
+//   options?: RenderOptions
+//   theme?: typeof defaultTheme
+//   transform?: (node: ReturnType<typeof render>) => Promise<void> | void
+// }
 
-  // Unmounting to don't see the warning message described above
-  node.unmount()
-  global.console = console
-}
+// export default async (
+//   component: ReactNode,
+//   { transform, options, theme }: Options = {},
+// ): Promise<void> => {
+//   // Save the instance of console (disable warning about adding element directly to document.body which is necessary when testing portal components)
+//   const { console } = global
+//   global.console = { ...console, error: jest.fn() }
+
+//   const node = renderWithTheme(
+//     component,
+//     {
+//       container: document.body,
+//       ...options,
+//     },
+//     theme,
+//   )
+//   if (transform) await transform(node)
+//   expect(node.asFragment()).toMatchSnapshot()
+
+//   // Unmounting to don't see the warning message described above
+//   node.unmount()
+//   global.console = console
+// }
