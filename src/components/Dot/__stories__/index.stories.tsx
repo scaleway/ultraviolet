2192c4bf429102c703cdc0d4a967c213a22b0803
Tom Lienard <tom.lienrd@gmail.com>
2021-10-25 13:50:57 +0200

    feat(dot): use tsx for story

---
+++ src/components/Dot/__stories__/index.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+import Dot, { DotProps } from '..'
+
+export default {
+  component: Dot,
+  title: 'Components/Data Display/dot',
+} as Meta
+
+const Template: Story<DotProps> = args => <Dot {...args} />
+
+export const Default = Template.bind({})
+
+export const Color = Template.bind({})
+Color.parameters = {
+  docs: {
+    storyDescription: 'Customize the style of the dot with `color` prop.',
+  },
+}
+Color.decorators = [
+  () => (
+    <>
+      <Dot color="primary" mr={1} />
+      <Dot color="success" mr={1} />
+      <Dot color="warning" mr={1} />
+      <Dot color="info" mr={1} />
+    </>
+  ),
+]
