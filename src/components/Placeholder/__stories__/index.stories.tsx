5e1c13ffed8ef3287c0470748a41e9ec039d1c45
Tom Lienard <tom.lienrd@gmail.com>
2021-10-26 15:03:08 +0200

    feat(placeholder): use tsx for story

---
+++ src/components/Placeholder/__stories__/index.stories.tsx
@@ -0,0 +1,50 @@
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+import Placeholder, { PlaceholderProps, variants } from '..'
+import Typography from '../../Typography'
+
+export default {
+  component: Placeholder,
+  title: 'Components/Data Display/Placeholder',
+} as Meta
+
+const Template: Story<PlaceholderProps> = args => <Placeholder {...args} />
+
+export const Default = Template.bind({})
+Default.parameters = {
+  docs: {
+    storyDescription:
+      'Placeholder will display empty blocks with animation. It can be used for loading screen or loading components.',
+  },
+}
+
+export const Box = Template.bind({})
+Box.decorators = [
+  () => (
+    <Placeholder
+      display="flex"
+      height={130}
+      width={180}
+      length={4}
+      variant="box"
+    />
+  ),
+]
+
+export const Variants = Template.bind({})
+Variants.decorators = [
+  () => (
+    <>
+      {Object.keys(variants)
+        .filter(variant => variant !== 'box')
+        .map(variant => (
+          <>
+            <Typography mb={2}>{variant}</Typography>
+            <Placeholder key={variant} variant={variant} mr={2} mb={2}>
+              {variant}
+            </Placeholder>
+          </>
+        ))}
+    </>
+  ),
+]
