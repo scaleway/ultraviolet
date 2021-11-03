0b80a91300de23d4af67743ec34f51679d9a4263
Tom Lienard <tom.lienrd@gmail.com>
2021-11-03 17:28:05 +0100

    refactor(range): migrate story to tsx

---
+++ src/components/Range/__stories__/index.stories.tsx
@@ -0,0 +1,55 @@
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+import Range, { RangeProps } from '..'
+import ControlValue from '../../../__stories__/components/ControlValue'
+
+export default {
+  component: Range,
+  decorators: [
+    Children => (
+      <div style={{ marginTop: 24 }}>
+        <Children />
+      </div>
+    ),
+  ],
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'A range bar. Range only work as [controlled component](https://reactjs.org/docs/forms.html).',
+      },
+    },
+  },
+  title: 'Components/Data Entry/Range',
+} as Meta
+
+const Template: Story<RangeProps> = args => <Range {...args} />
+
+export const Default = Template.bind({})
+
+export const SingleValue = Template.bind({})
+SingleValue.decorators = [
+  () => (
+    <ControlValue value={[3]}>
+      {({ value, onChange }) => (
+        <Range value={value} min={0} max={10} onChange={onChange} />
+      )}
+    </ControlValue>
+  ),
+]
+
+export const TwoValues = Template.bind({})
+TwoValues.parameters = {
+  docs: {
+    storyDescription: 'For now, Range component will work with max two values.',
+  },
+}
+TwoValues.decorators = [
+  () => (
+    <ControlValue value={[3, 8]}>
+      {({ value, onChange }) => (
+        <Range value={value} min={0} max={15} onChange={onChange} />
+      )}
+    </ControlValue>
+  ),
+]
