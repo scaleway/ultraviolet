1572b41011e6f354feaef60f65f654ca6cf831e6
Tom Lienard <tom.lienrd@gmail.com>
2021-11-08 14:30:45 +0100

    fix: add back global style to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import I18n from '@scaleway/use-i18n'
-import { css, ThemeProvider } from '@emotion/react'
+import { css, ThemeProvider, Global } from '@emotion/react'
+import { normalize } from 'polished'
 
 import theme from '../src/theme'
-import { GlobalStyle } from '../src'
 
 const STORY_SORT = {
   order: ['Home', 'Testing', 'Theme', 'Components'],
@@ -55,6 +55,99 @@ const adjustedTheme = ancestorTheme => ({
   ),
 })
 
+const globalStyles = theme => css`
+  ${normalize()}
+  /* Fallback system fonts */
+  @font-face {
+    font-family: 'System';
+    font-style: normal;
+    font-weight: 400;
+    src: local('.SFNSText-Regular'),
+      local('.HelveticaNeueDeskInterface-Regular'), local('.LucidaGrandeUI'),
+      local('Segoe UI'), local('Ubuntu'), local('Roboto-Regular'),
+      local('DroidSans'), local('Tahoma');
+  }
+  @font-face {
+    font-family: 'System';
+    font-style: normal;
+    font-weight: 500;
+    src: local('.SFNSText-Medium'),
+      local('.HelveticaNeueDeskInterface-MediumP4'), local('.LucidaGrandeUI'),
+      local('Segoe UI Semibold'), local('Ubuntu Medium'), local('Roboto-Medium'),
+      local('DroidSans-Bold'), local('Tahoma Bold');
+  }
+  @font-face {
+    font-family: 'System';
+    font-style: normal;
+    font-weight: 700;
+    src: local('.SFNSText-Bold'), local('.HelveticaNeueDeskInterface-Bold'),
+      local('.LucidaGrandeUI'), local('Segoe UI Bold'), local('Ubuntu Bold'),
+      local('Roboto-Bold'), local('DroidSans-Bold'), local('Tahoma Bold');
+  }
+  * {
+    box-sizing: border-box;
+  }
+  html {
+    box-sizing: border-box;
+    line-height: 1.5;
+    font-size: 16px;
+    height: 100%;
+    -webkit-tap-highlight-color: transparent;
+    margin: 0;
+  }
+  body {
+    background-color: ${theme.colors.white};
+    color: ${theme.colors.gray700};
+    font-family: ${theme.fonts.sansSerif};
+    overflow: hidden;
+    height: 100%;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    margin: 0;
+  }
+  input,
+  textarea,
+  select,
+  button {
+    font-family: ${theme.fonts.sansSerif};
+  }
+  button,
+  html [type='button'],
+  [type='reset'],
+  [type='submit'] {
+    appearance: none;
+  }
+  img,
+  canvas,
+  iframe,
+  video,
+  svg,
+  select,
+  textarea {
+    max-width: 100%;
+  }
+  a {
+    color: ${theme.colors.blue};
+    text-decoration: none;
+  }
+  a:hover,
+  a:focus,
+  a:active {
+    color: ${theme.colors.darkBlue};
+    text-decoration: underline;
+  }
+  p {
+    margin: 0;
+  }
+`
+
+const overflowStyle = css`
+  body {
+    overflow: initial !important;
+  }
+`
+
 export const decorators = [
   Story => (
     <I18n
@@ -68,15 +161,7 @@ export const decorators = [
       supportedLocales={['en', 'fr', 'es']}
     >
       <ThemeProvider theme={adjustedTheme}>
-        <GlobalStyle
-          additionalStyles={[
-            css`
-              body {
-                overflow: initial !important;
-              }
-            `,
-          ]}
-        />
+        <Global styles={[globalStyles, overflowStyle]} />
         <Story />
       </ThemeProvider>
     </I18n>
