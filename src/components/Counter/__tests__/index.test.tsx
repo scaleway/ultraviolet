b59a7e5d7f6cf3c0da0ea64a7a3dff1436a9d01d
Vincent Audebert <vaudebert@scaleway.com>
2021-12-01 17:11:34 +0100

    fix: save tests

--- src/components/Counter/__tests__/index.test.tsx
+++ src/components/Counter/__tests__/index.test.tsx
@@ -1,3 +1,4 @@
+import { waitFor } from '@testing-library/react'
 import React from 'react'
 import Counter from '..'
 import { shouldMatchEmotionSnapshot } from '../../../helpers/jestHelpers'
@@ -6,4 +7,14 @@ describe('Counter', () => {
   test.each([0, 100, 10000])('renders correctly end=%p', end =>
     shouldMatchEmotionSnapshot(<Counter end={end} />),
   )
+
+  test('renders and execute onEnd', async () => {
+    const end = 10
+    const mockOnEnd = jest.fn()
+    await shouldMatchEmotionSnapshot(<Counter end={end} onEnd={mockOnEnd} />, {
+      transform: async () => {
+        await waitFor(() => expect(mockOnEnd).toHaveBeenCalledTimes(1))
+      },
+    })
+  })
 })
