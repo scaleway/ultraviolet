9cb794516bf95cbcc694727db034e1d8cd400cf1
Matthias <mprost@scaleway.com>
2021-12-02 17:30:50 +0100

    ci(box): fixed box tests

--- src/components/Box/__tests__/index.tsx
+++ src/components/Box/__tests__/index.tsx
@@ -5,10 +5,9 @@ import { shouldMatchEmotionSnapshot } from '../../../helpers/jestHelpers'
 describe('Box', () => {
   test('Box renders correctly ', () => shouldMatchEmotionSnapshot(<Box />))
 
+  // We don't support colors any more with new theme
   test('Box renders with system', () =>
-    shouldMatchEmotionSnapshot(
-      <Box backgroundColor="primary" height={100} mx="auto" p={2} />,
-    ))
+    shouldMatchEmotionSnapshot(<Box height={100} mx="auto" p={2} />))
 
   test('Box renders with bordered', () =>
     shouldMatchEmotionSnapshot(<Box bordered />))
