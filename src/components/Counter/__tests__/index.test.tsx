68cfaf2bdd71fe827e94c470c3f4977b6623090c
Vincent <vincent.audebert@yahoo.fr>
2021-12-02 11:48:30 +0100

    fix: a11y for counter (#957)
    * fix: a11y for counter
    
    * fix: save tests
    
    * fix: counter tests
    
    * fix: tests
    
    * fix: manu feedbacks
    
    * fix: empty commit to trigger pipeline

--- src/components/Counter/__tests__/index.test.tsx
+++ src/components/Counter/__tests__/index.test.tsx
@@ -1,3 +1,4 @@
+import { act } from '@testing-library/react'
 import React from 'react'
 import Counter from '..'
 import { shouldMatchEmotionSnapshot } from '../../../helpers/jestHelpers'
@@ -6,4 +7,22 @@ describe('Counter', () => {
   test.each([0, 100, 10000])('renders correctly end=%p', end =>
     shouldMatchEmotionSnapshot(<Counter end={end} />),
   )
+
+  test('renders and execute onEnd', async () => {
+    const end = 10
+    const mockOnEnd = jest.fn()
+    await act(async () => {
+      await shouldMatchEmotionSnapshot(
+        <Counter end={end} onEnd={mockOnEnd} />,
+        {
+          transform: async () => {
+            await new Promise(resolve => {
+              setTimeout(() => resolve([]), 2000)
+            })
+            expect(mockOnEnd).toHaveBeenCalledTimes(1)
+          },
+        },
+      )
+    })
+  })
 })
