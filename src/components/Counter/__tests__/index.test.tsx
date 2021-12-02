2695d8f0d22cbf35ad20eecee01cbad9d338cd41
Vincent Audebert <vaudebert@scaleway.com>
2021-12-02 10:40:09 +0100

    fix: manu feedbacks

--- src/components/Counter/__tests__/index.test.tsx
+++ src/components/Counter/__tests__/index.test.tsx
@@ -1,3 +1,4 @@
+import { act } from '@testing-library/react'
 import React from 'react'
 import Counter from '..'
 import { shouldMatchEmotionSnapshot } from '../../../helpers/jestHelpers'
@@ -10,13 +11,18 @@ describe('Counter', () => {
   test('renders and execute onEnd', async () => {
     const end = 10
     const mockOnEnd = jest.fn()
-    await shouldMatchEmotionSnapshot(<Counter end={end} onEnd={mockOnEnd} />, {
-      transform: async () => {
-        await new Promise(resolve => {
-          setTimeout(() => resolve([]), 2000)
-        })
-        expect(mockOnEnd).toHaveBeenCalledTimes(1)
-      },
+    await act(async () => {
+      await shouldMatchEmotionSnapshot(
+        <Counter end={end} onEnd={mockOnEnd} />,
+        {
+          transform: async () => {
+            await new Promise(resolve => {
+              setTimeout(() => resolve([]), 2000)
+            })
+            expect(mockOnEnd).toHaveBeenCalledTimes(1)
+          },
+        },
+      )
     })
   })
 })
