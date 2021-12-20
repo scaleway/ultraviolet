26b8be6e29438b358a3fe0cea54d9892ec7350a9
Tom Lienard <tom.lienrd@gmail.com>
2021-12-20 15:40:31 +0100

    feat: add toaster to a11y tests

--- src/__tests__/a11y.test.tsx
+++ src/__tests__/a11y.test.tsx
@@ -8,7 +8,13 @@ import * as process from 'process'
 import React, { VoidFunctionComponent } from 'react'
 import { renderWithTheme } from '../helpers/jestHelpers'
 
-const testedComponents = ['Alert', 'ActivityIndicator', 'Badge', 'Counter']
+const testedComponents = [
+  'Alert',
+  'ActivityIndicator',
+  'Badge',
+  'Counter',
+  'Toaster',
+]
 
 const foundFiles: string[] = []
 
