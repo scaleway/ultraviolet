9d55833c6d02f68828e4343e3ff1de08bd7d2848
Emmanuel Chambon <echambon@scaleway.com>
2021-12-23 11:18:27 +0100

    fix: use new colors

--- src/components/BorderedBox/index.tsx
+++ src/components/BorderedBox/index.tsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled'
 const BorderedBox = styled.div`
   padding: ${({ theme }) => theme.space['3']};
   border-radius: ${({ theme }) => theme.radii.default};
-  border: 1px solid ${({ theme }) => theme.colorsDeprecated.gray350};
+  border: 1px solid ${({ theme }) => theme.colors.neutral.borderWeak};
 `
 
 export default BorderedBox
