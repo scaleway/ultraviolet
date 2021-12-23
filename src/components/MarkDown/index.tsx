42df0389198f883214abe62e5c7dad83cf4c6c26
Vincent <vincent.audebert@yahoo.fr>
2021-12-23 14:51:12 +0100

    fix: a11y for markdown but mostly due to reminder (#1037)
    * fix: a11y for markdown but mostly due to reminder
    
    * fix: feedback
    
    * fix: tests

--- src/components/MarkDown/index.tsx
+++ src/components/MarkDown/index.tsx
@@ -61,11 +61,7 @@ const RootRendererComponent: FunctionComponent<{
   inline?: boolean
   parentProps?: XStyledProps
 }> = ({ inline = false, parentProps = {}, children }) => (
-  <StyledContainer
-    as={inline ? 'span' : 'div'}
-    inline={inline}
-    {...parentProps}
-  >
+  <StyledContainer inline={inline} {...parentProps}>
     {children}
   </StyledContainer>
 )
