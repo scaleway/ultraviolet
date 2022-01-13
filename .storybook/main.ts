45a446bbeb420f64f6ab2f96984d204c2b6ee926
Emmanuel Chambon <echambon@scaleway.com>
2022-01-13 19:22:11 +0100

    chore: migrate storybook config to ts

---
+++ .storybook/main.ts
@@ -0,0 +1,43 @@
+import { join } from 'path'
+import { Configuration } from 'webpack'
+
+export default {
+  core: {
+    builder: 'webpack5',
+  },
+  typescript: {
+    // also valid 'react-docgen-typescript' | false
+    reactDocgen: 'react-docgen-typescript',
+  },
+  stories: [
+    '../src/**/__stories__/*.stories.mdx',
+    '../src/**/__stories__/*.stories.tsx',
+  ],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-links',
+    '@storybook/addon-a11y',
+  ],
+  webpackFinal: (config: Configuration) => {
+    const cwd = process.cwd()
+    // TODO: remove when storybook supports emotion 11
+    const newConfig = config
+    newConfig.resolve = {
+      ...(newConfig?.resolve ? newConfig.resolve : {}),
+      alias: {
+        ...(newConfig?.resolve?.alias ? newConfig.resolve.alias : {}),
+        '@emotion/core': join(cwd, 'node_modules', '@emotion', 'react'),
+        '@emotion/styled': join(cwd, 'node_modules', '@emotion', 'styled'),
+        '@emotion/styled-base': join(cwd, 'node_modules', '@emotion', 'styled'),
+        'emotion-theming': join(cwd, 'node_modules', '@emotion', 'react'),
+      },
+      fallback: {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        path: require.resolve('path-browserify'),
+      },
+    }
+
+    return config
+  },
+}
--- .storybook/scaleway.ts
+++ .storybook/scaleway.ts
@@ -1,4 +1,4 @@
-import { create } from '@storybook/theming/create'
+import { create } from '@storybook/theming'
 import brandImage from './assets/scaleway-text.png'
 
 export default create({
