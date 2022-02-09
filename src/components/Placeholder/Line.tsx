a95fb8af5883bd311f9e2f51de4eb68669d690c6
Vincent Audebert <vaudebert@scaleway.com>
2022-02-09 13:45:14 +0100

    fix: tests CI failing

--- src/components/Placeholder/Line.tsx
+++ src/components/Placeholder/Line.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled'
 
 const sizes = [80, 120, 160, 200]
 
-const randomSize = () => sizes[Math.floor(Math.random() * sizes.length)]
+const randomSize = () => sizes[Math.floor(Math.random() * sizes.length) + 1]
 
 const Line = styled.div`
   height: 12px;
