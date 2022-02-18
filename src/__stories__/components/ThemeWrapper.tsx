864e929f0e2a5421c1fc2e721ab745d676d17acb
Matthias <mprost@scaleway.com>
2022-02-18 12:17:53 +0100

    feat: added theme wrapper into storybook documentation

---
+++ src/__stories__/components/ThemeWrapper.tsx
@@ -0,0 +1,19 @@
+import { Global, ThemeProvider } from '@emotion/react'
+import React, { ReactNode } from 'react'
+import { useDarkMode } from 'storybook-dark-mode'
+import { globalStyles } from '../../../.storybook/preview'
+import lightTheme, { darkTheme } from '../../theme'
+
+const ThemeWrapper = ({ children }: { children: ReactNode }) => {
+  const mode = useDarkMode() ? 'dark' : 'light'
+  const currentTheme = mode === 'light' ? lightTheme : darkTheme
+
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <Global styles={[globalStyles]} />
+      {children}
+    </ThemeProvider>
+  )
+}
+
+export default ThemeWrapper
