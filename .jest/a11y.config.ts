88b212c0e115c192ba94b303ec597ceaa4ff5b36
Tom <tlienard@scaleway.com>
2022-03-08 10:48:19 +0000

    fix: a11y & pnpm-lock deps

--- .jest/a11y.config.ts
+++ .jest/a11y.config.ts
@@ -15,7 +15,7 @@ const config: Config.InitialOptions = {
       },
     ],
   ],
-  transformIgnorePatterns: ['node_modules/(?!@scaleway)'],
+  transformIgnorePatterns: ['node_modules/(?!@scaleway)/'],
   testPathIgnorePatterns: ['/nodes_modules/', 'src/components'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
