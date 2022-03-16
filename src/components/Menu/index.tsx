9b9566adfd2ea4261ab6fc75d6a37ecfe6297d51
Levende MOHAMMAD <lmohammad@scaleway.com>
2022-03-16 12:01:50 +0100

    fix(menu): add className to menu component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,4 +1,5 @@
 import { Theme, css } from '@emotion/react'
+import styled from '@emotion/styled'
 import { transparentize } from 'polished'
 import PropTypes from 'prop-types'
 import React, { ComponentProps, FunctionComponent, ReactNode } from 'react'
@@ -89,7 +90,10 @@ const arrowPlacementStyles = {
 } as const
 
 type ArrowPlacement = keyof typeof arrowPlacementStyles
-
+type MenuListProps = {
+  align: AlignStyle
+  hasArrow: boolean
+}
 export const arrowPlacements = Object.keys(
   arrowPlacementStyles,
 ) as ArrowPlacement[]
@@ -99,43 +103,39 @@ type AlignStyle = {
   right?: string | null
 }
 
-const styles = {
-  align: (align: AlignStyle) => css`
-    &:after,
-    &:before {
-      left: ${align.left};
-      right: ${align.right};
-    }
-  `,
-  menu: (theme: Theme) => css`
-    background-color: ${theme.colors.neutral.backgroundWeak};
-    display: flex;
-    flex-direction: column;
-    text-align: center;
-    justify-content: center;
-    color: ${theme.colors.neutral.text};
-    border-radius: 4px;
-    position: relative;
+const MenuList = styled.div<MenuListProps>`
+  &:after,
+  &:before {
+    left: ${({ align }) => align.left};
+    right: ${({ align }) => align.left};
+  }
+  background-color: ${({ theme }) => theme.colors.neutral.backgroundWeak};
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  justify-content: center;
+  color: ${({ theme }) => theme.colors.neutral.text};
+  border-radius: 4px;
+  position: relative;
 
-    &:after,
-    &:before {
-      border: solid transparent;
-      border-width: 9px;
-      content: ' ';
-      height: 0;
-      width: 0;
-      position: absolute;
-      pointer-events: none;
-    }
+  &:after,
+  &:before {
+    border: solid transparent;
+    border-width: 9px;
+    content: ' ';
+    height: 0;
+    width: 0;
+    position: absolute;
+    pointer-events: none;
+  }
 
-    &:after {
-      border-color: transparent;
-    }
-    &:before {
-      border-color: transparent;
-    }
-  `,
-}
+  &:after {
+    border-color: transparent;
+  }
+  &:before {
+    border-color: transparent;
+  }
+`
 
 interface ChildrenProps {
   placement: ComponentProps<typeof Popper>['placement']
@@ -148,6 +148,7 @@ type MenuProps = Omit<ComponentProps<typeof Popper>, 'children'> & {
   ariaLabel?: string
   placement?: ArrowPlacement
   children?: ((props: ChildrenProps) => ReactNode) | ReactNode
+  className?: string
 }
 
 type MenuType = FunctionComponent<MenuProps> & { Item: typeof Item }
@@ -163,6 +164,7 @@ const Menu: MenuType = ({
   name = 'menu',
   placement = 'bottom',
   visible = false,
+  className = '',
 }) => (
   <Popper
     aria-label={ariaLabel}
@@ -173,21 +175,22 @@ const Menu: MenuType = ({
     name={name}
     placement={placement}
     visible={visible}
+    className={className}
   >
     {({ placement: localPlacement, toggle, visible: isOpen }) =>
       isOpen && (
-        <div
+        <MenuList
+          align={align}
+          hasArrow={hasArrow}
           role="menu"
-          css={[
-            styles.menu,
-            hasArrow && arrowPlacementStyles[localPlacement as ArrowPlacement],
-            styles.align(align),
-          ]}
+          css={
+            hasArrow && arrowPlacementStyles[localPlacement as ArrowPlacement]
+          }
         >
           {typeof children === 'function'
             ? children({ placement: localPlacement, toggle, visible })
             : children}
-        </div>
+        </MenuList>
       )
     }
   </Popper>
@@ -201,6 +204,7 @@ Menu.propTypes = {
   ariaLabel: PropTypes.string,
   baseId: PropTypes.string,
   children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]).isRequired,
+  className: PropTypes.string,
   disclosure: PropTypes.func.isRequired,
   hasArrow: PropTypes.bool,
   modal: PropTypes.bool,
