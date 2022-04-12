a4b2b3baf4e496a7213261dbc9c931031f16fa45
Fabien Hébert <fhebert@scaleway.com>
2022-04-12 11:56:15 +0200

    feat: 🎸 new Bullet component

---
+++ src/components/Bullet/__tests__/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Bullet, { bulletSizes, bulletVariants } from '..'
+import { shouldMatchEmotionSnapshot } from '../../../helpers/jestHelpers'
+
+describe('Bullet', () => {
+  test('renders correctly with a text', () =>
+    shouldMatchEmotionSnapshot(<Bullet text="1" />))
+
+  test('renders correctly with an icon', () =>
+    shouldMatchEmotionSnapshot(<Bullet icon="ai" />))
+
+  describe('variant', () => {
+    bulletVariants.forEach(variant => {
+      test(`render ${variant}`, () =>
+        shouldMatchEmotionSnapshot(<Bullet variant={variant} text="1" />))
+    })
+  })
+
+  describe('size', () => {
+    bulletSizes.forEach(size => {
+      test(`render ${size}`, () =>
+        shouldMatchEmotionSnapshot(<Bullet size={size} text="1" />))
+    })
+  })
+})
---
+++ src/components/Bullet/index.tsx
@@ -0,0 +1,133 @@
+import { Theme, css } from '@emotion/react'
+import styled from '@emotion/styled'
+import PropTypes from 'prop-types'
+import React, { ComponentProps, FunctionComponent } from 'react'
+import Icon from '../Icon'
+import Tooltip from '../Tooltip'
+
+const variants = {
+  danger: ({ theme }: { theme: Theme }) => `
+    background-color: ${theme.colors.danger.background};
+    color: ${theme.colors.danger.text};
+  `,
+  default: ({ theme }: { theme: Theme }) => `
+    border: 1px solid ${theme.colors.neutral.borderWeak};
+    background-color: ${theme.colors.neutral.background};
+    color: ${theme.colors.neutral.text};
+  `,
+  disabled: ({ theme }: { theme: Theme }) => `
+  border: 1px solid ${theme.colors.neutral.borderWeak};
+    background-color: ${theme.colors.neutral.background};
+    color: ${theme.colors.neutral.textWeak};
+  `,
+  on: ({ theme }: { theme: Theme }) => `
+    background-color: ${theme.colors.primary.background};
+    color: ${theme.colors.primary.text};
+  `,
+  success: ({ theme }: { theme: Theme }) => `
+    background-color: ${theme.colors.success.background};
+    color: ${theme.colors.success.text};
+  `,
+  wait: ({ theme }: { theme: Theme }) => `
+    background-color: ${theme.colors.info.background};
+    color: ${theme.colors.info.text};
+  `,
+  warning: ({ theme }: { theme: Theme }) => `
+    background-color: ${theme.colors.warning.background};
+    color: ${theme.colors.warning.text};
+  `,
+} as const
+type BulletVariant = keyof typeof variants
+export const bulletVariants = Object.keys(variants) as BulletVariant[]
+
+const sizes = {
+  medium: ({ theme }: { theme: Theme }) => css`
+    width: ${theme.space['4']};
+    height: ${theme.space['4']};
+    font-size: 16px;
+  `,
+  small: ({ theme }: { theme: Theme }) => css`
+    width: ${theme.space['3']};
+    height: ${theme.space['3']};
+    font-size: 14px;
+  `,
+} as const
+type BulletSize = keyof typeof sizes
+export const bulletSizes = Object.keys(sizes) as BulletSize[]
+
+const iconSizes: Record<BulletSize, number> = {
+  medium: 14,
+  small: 10,
+}
+
+const variantStyles = ({ variant }: { variant: BulletVariant }) =>
+  variants[variant]
+const sizeStyles = ({ size }: { size: BulletSize }) => sizes[size]
+
+type StyledContainerType = { variant: BulletVariant; size: BulletSize }
+const StyledContainer = styled('div')<StyledContainerType>`
+  display: inline-flex;
+  border-radius: 50%;
+  justify-content: center;
+  align-items: center;
+  font-weight: 500;
+  ${variantStyles}
+  ${sizeStyles}
+`
+
+type ContentProps =
+  | { icon: ComponentProps<typeof Icon>['name']; text?: never }
+  | { icon?: never; text: string }
+type BulletProps = {
+  className?: string
+  size?: BulletSize
+  tooltip?: string
+  tooltipBaseId?: string
+  variant?: BulletVariant
+} & ContentProps
+
+const Bullet: FunctionComponent<BulletProps> = ({
+  className,
+  variant = 'default',
+  size = 'medium',
+  icon,
+  text,
+  tooltip,
+  tooltipBaseId,
+}) => (
+  <Tooltip baseId={tooltipBaseId} text={tooltip}>
+    <StyledContainer variant={variant} size={size} className={className}>
+      {icon ? <Icon name={icon} size={iconSizes[size]} /> : text}
+    </StyledContainer>
+  </Tooltip>
+)
+
+const customXorPropType = (
+  props: Record<string, unknown>,
+  propName: string,
+  comp: string,
+) => {
+  if ((props.text && props.icon) || (!props.text && !props.icon)) {
+    return new Error(
+      `Invalid props supplied to ${comp}, you must supply an icon prop OR a text prop, not both`,
+    )
+  }
+  if (props[propName] && typeof props[propName] !== 'string') {
+    return new Error(
+      `Invalid prop supplied to ${comp}, text should be a string`,
+    )
+  }
+
+  return null
+}
+Bullet.propTypes = {
+  className: PropTypes.string,
+  icon: customXorPropType,
+  size: PropTypes.oneOf<BulletSize>(bulletSizes),
+  text: customXorPropType,
+  tooltip: PropTypes.string,
+  tooltipBaseId: PropTypes.string,
+  variant: PropTypes.oneOf<BulletVariant>(bulletVariants),
+}
+
+export default Bullet
