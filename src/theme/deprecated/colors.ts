fc9528a63ce0a8c2144368da48548f637d3932c3
Manu Chambon <echambon@scaleway.com>
2022-05-16 15:25:27 +0200

    fix: remove unused deprecated colors (#1456)

--- src/theme/deprecated/colors.ts
+++ src/theme/deprecated/colors.ts
@@ -9,24 +9,14 @@ const gray700 = '#4a4f62'
 const gray950 = '#151a2d'
 const black = '#000'
 
-const ngray300 = '#dbdbdf'
-
 const violet = '#4f0599'
-const lightViolet = '#a365f6'
 
 const green = '#45d6b5'
 const red = '#ef5774'
 const orange = '#ff8c69'
 const blue = '#3f6ed8'
-const darkBlue = '#2f52a1'
 
-const zumthor = '#e4edff'
-const foam = '#cdfcf1'
-const serenade = '#ffefe6'
-const pippin = '#ffe1e7'
-const shadow = '#a5a5cd'
 const gold = '#ffd536'
-const crimson = '#cf0f34'
 
 // System colors
 const primary = violet
@@ -44,9 +34,6 @@ const baseColors = {
   chartGreen: '#33BBB3',
   chartNegativeRed: '#ef5774',
   chartPurple: '#9B83F9',
-  crimson,
-  darkBlue,
-  foam,
   gold,
   gray50,
   gray100,
@@ -58,20 +45,14 @@ const baseColors = {
   gray950,
   green,
   info,
-  lightViolet,
-  ngray300,
   orange,
-  pippin,
   primary,
   red,
-  serenade,
-  shadow,
   success,
   transparent,
   violet,
   warning,
   white,
-  zumthor,
 }
 
 export type ColorDeprecated = keyof typeof baseColors
