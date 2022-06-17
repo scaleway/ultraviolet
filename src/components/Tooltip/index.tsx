95f94271809845e7579574c2f5ed4162115dd5cf
Emmanuel Chambon <echambon@scaleway.com>
2022-06-17 17:50:43 +0200

    fix: correct Tooltip errors

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -41,10 +41,12 @@ type PositionsType = {
   tooltipInitialPosition: string
 }
 
-const StyledTooltip = styled.div<{
+type StyledTooltipProps = {
   maxWidth: number
   positions: PositionsType
-}>`
+}
+
+const StyledTooltip = styled.div<StyledTooltipProps>`
   background: ${({ theme }) => theme.colors.neutral.backgroundStronger};
   color: ${({ theme }) => theme.colors.neutral.textStronger};
   border-radius: ${({ theme }) => theme.radii.default};
@@ -146,7 +148,16 @@ const computePositions = ({
 
 type TooltipProps = {
   id?: string
-  children: ReactNode
+  children:
+    | ReactNode
+    | ((renderProps: {
+        className?: string
+        onBlur: () => void
+        onFocus: () => void
+        onMouseEnter: () => void
+        onMouseLeave: () => void
+        ref: RefObject<HTMLDivElement>
+      }) => ReactNode)
   maxWidth?: number
   placement?: TooltipPlacement
   text?: ReactNode
