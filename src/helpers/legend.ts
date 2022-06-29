33481345c5d1301b9e5b3e09a401f27d1052b85e
Emmanuel Chambon <echambon@scaleway.com>
2022-06-29 15:47:30 +0200

    fix(Charts): correct color generation

--- src/helpers/legend.ts
+++ src/helpers/legend.ts
@@ -3,11 +3,14 @@ import { Theme } from '@emotion/react'
 export const legendColors = (theme: Theme): string[] => [
   theme.colors.success.backgroundStrong,
   theme.colors.secondary.backgroundStrong,
-  theme.colors.neutral.backgroundStrong,
+  theme.colors.info.backgroundStrong,
+  theme.colors.danger.backgroundStrong,
+  theme.colors.primary.backgroundStrong,
+  theme.colors.warning.backgroundStrong,
 ]
 
 export const getLegendColor = (index: number, theme: Theme): string => {
   const colors = legendColors(theme)
 
-  return colors[index] || colors[colors.length - 1]
+  return colors[index] || colors[colors.length % index]
 }
