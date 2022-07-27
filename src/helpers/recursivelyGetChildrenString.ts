e51bb7c80e78233339781b8d48c75f9bb903d7b5
Matthias <mprost@scaleway.com>
2022-07-27 15:11:12 +0200

    ci: fix

--- src/helpers/recursivelyGetChildrenString.ts
+++ src/helpers/recursivelyGetChildrenString.ts
@@ -1,11 +1,12 @@
-import { ReactNode, isValidElement } from 'react'
+import { ReactNode } from 'react'
 
 const recursivelyGetChildrenString = (children: ReactNode): string => {
   if (typeof children === 'string') return children
   if (Array.isArray(children)) return '' // We can't determine which string to display in tooltip
-  if (typeof children === 'object' && isValidElement(children)) {
-    const childProps = (children?.props as Record<string, unknown>)
-      ?.children as ReactNode
+  if (typeof children === 'object') {
+    const childProps = (
+      (children as Record<string, unknown>)?.props as Record<string, unknown>
+    )?.children as ReactNode
     if (childProps) return recursivelyGetChildrenString(childProps)
   }
 
