325968a3ef7e029fc8f12af426c8baebc6bfc13f
Matthias Prost <mprost@scaleway.com>
2022-07-28 10:15:58 +0200

    fix: remove all errors and enable typecheck (#1697)
    * fix: remove all errors and enable typecheck
    
    * ci: fix

--- src/helpers/recursivelyGetChildrenString.ts
+++ src/helpers/recursivelyGetChildrenString.ts
@@ -4,8 +4,9 @@ const recursivelyGetChildrenString = (children: ReactNode): string => {
   if (typeof children === 'string') return children
   if (Array.isArray(children)) return '' // We can't determine which string to display in tooltip
   if (typeof children === 'object') {
-    const childProps = (children?.props as Record<string, unknown>)
-      ?.children as ReactNode
+    const childProps = (
+      (children as Record<string, unknown>)?.props as Record<string, unknown>
+    )?.children as ReactNode
     if (childProps) return recursivelyGetChildrenString(childProps)
   }
 
