c9488b0d151ed91da76f2e681d06c02b00bade4b
Manu Chambon <echambon@scaleway.com>
2022-08-11 19:15:59 +0200

    feat(Notice): migrate from Box to div (#1753)
    * feat(Notice): migrate from Box to div
    
    * Apply suggestions from code review
    
    Co-authored-by: Matthias Prost <mprost@scaleway.com>
    
    Co-authored-by: Matthias Prost <mprost@scaleway.com>

--- src/components/Notice/index.tsx
+++ src/components/Notice/index.tsx
@@ -1,24 +1,24 @@
 import styled from '@emotion/styled'
 import { ReactNode } from 'react'
-import Box, { BoxProps } from '../Box'
 import Icon from '../Icon'
 import MarkDown from '../MarkDown'
 
-const Container = styled(Box)`
+const Container = styled.div`
   color: ${({ theme }) => theme.colors.neutral.textWeak};
   font-size: 12px;
   display: flex;
   align-items: center;
+  gap: ${({ theme }) => theme.space['1']};
 `
 
 type NoticeProps = {
   children: ReactNode
   className?: string
-} & BoxProps
+}
 
-const Notice = ({ children, className, ...props }: NoticeProps) => (
-  <Container className={className} {...props}>
-    <Icon name="information-outline" verticalAlign="top" mr={1} size={20} />
+const Notice = ({ children, className }: NoticeProps) => (
+  <Container className={className}>
+    <Icon name="information-outline" verticalAlign="top" size={20} />
     {typeof children === 'string' ? (
       <MarkDown source={children} linkTarget="_blank" />
     ) : null}
