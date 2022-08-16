712600f21d3843e6fba7e722b6692e7d862ffe61
Emmanuel Chambon <echambon@scaleway.com>
2022-08-16 18:30:46 +0200

    fix: correct stories

--- src/components/ProgressionButton/__stories__/index.stories.tsx
+++ src/components/ProgressionButton/__stories__/index.stories.tsx
@@ -15,9 +15,7 @@ export default {
 } as Meta
 
 const Template: Story<ComponentProps<typeof ProgressionButton>> = args => (
-  <ProgressionButton height={40} {...args}>
-    Progressing...
-  </ProgressionButton>
+  <ProgressionButton {...args}>Progressing...</ProgressionButton>
 )
 
 export const Default = Template.bind({})
@@ -40,16 +38,12 @@ Custom.decorators = [
       <ProgressionButton
         creation={new Date(new Date().setSeconds(new Date().getSeconds() + 10))}
         duration={20}
-        color="beta"
-        height={40}
       >
         I&apos;m going to start in 10 seconds and take 20 seconds to complete
       </ProgressionButton>
       <ProgressionButton
         creation={new Date(new Date().setSeconds(new Date().getSeconds() - 60))}
         duration={90}
-        color="primary"
-        height={40}
       >
         I was supposed to start one minute ago and take 90 seconds (meaning I
         only have 30s left and start at 66%)
@@ -57,8 +51,6 @@ Custom.decorators = [
       <ProgressionButton
         creation={new Date(new Date().setSeconds(new Date().getSeconds() - 90))}
         duration={90}
-        color="info"
-        height={40}
       >
         I was supposed to start 90 seconds ago and take 90 seconds (meaning
         I&apos;m already done)
