73df0f61d668462ab89587f24cf770bf5e3a6a0a
Matthias Prost <mprost@scaleway.com>
2022-08-30 13:57:50 +0200

    feat(Toggle): refactor of component (#1792)
    * feat(Toggle): refactor of component
    
    * fix: feedback dodo
    
    * fix: feedback tom and baptiste
    
    * fix: width of toggle

---
+++ src/components/Toggle/__stories__/index.stories.tsx
@@ -0,0 +1,100 @@
+import { Meta, Story } from '@storybook/react'
+import { ComponentProps } from 'react'
+import Toggle from '..'
+import Icon from '../../Icon'
+import Text from '../../Text'
+
+export default {
+  component: Toggle,
+  parameters: {
+    docs: {
+      description: {
+        component: 'Toggle button used to give an on / off state into a form.',
+      },
+    },
+  },
+  title: 'Components/Data Entry/Toggle',
+} as Meta
+
+const Template: Story<ComponentProps<typeof Toggle>> = ({
+  checked,
+  ...args
+}) => <Toggle {...args} name="Toggle" />
+
+export const Default = Template.bind({})
+
+export const Label = Template.bind({})
+Label.args = {
+  label: 'Toggle me on',
+  name: 'label',
+}
+
+export const ComplexLabel = Template.bind({})
+ComplexLabel.parameters = {
+  docs: {
+    storyDescription:
+      'Toggle can accept a more complex label than just a text, it allows your to customize even more the look of the toggle.',
+  },
+}
+ComplexLabel.args = {
+  label: (
+    <div
+      style={{
+        alignItems: 'center',
+        display: 'inline-flex',
+        gap: '8px',
+        marginLeft: '8px',
+      }}
+    >
+      <Icon name="lock" size={18} />
+      <Text as="span" variant="body">
+        Lock functionality
+      </Text>
+    </div>
+  ),
+  name: 'label',
+}
+
+export const LabelPosition = Template.bind({})
+LabelPosition.parameters = {
+  docs: {
+    storyDescription:
+      'Easily change the position of the label by passing `labelPosition` prop.',
+  },
+}
+LabelPosition.decorators = [
+  () => (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+      <Template name="left" label="Left label" labelPosition="left" />
+      <Template name="right" label="Right label" labelPosition="right" />
+    </div>
+  ),
+]
+
+export const Sizes = Template.bind({})
+Sizes.parameters = {
+  docs: {
+    storyDescription: 'Toggle can have different sizes by passing `size` prop.',
+  },
+}
+Sizes.decorators = [
+  () => (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+      <Template name="small" size="small" label="Small size" />
+      <Template name="large" size="large" label="Large size" />
+    </div>
+  ),
+]
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+  disabled: true,
+  label: "You can't toggle me",
+  name: 'disabled',
+}
+
+export const Tooltip = Template.bind({})
+Tooltip.args = {
+  name: 'tooltip',
+  tooltip: 'Hello there!',
+}
---
+++ src/components/Toggle/index.tsx
@@ -0,0 +1,209 @@
+import styled from '@emotion/styled'
+import {
+  ChangeEvent,
+  ChangeEventHandler,
+  ReactNode,
+  useCallback,
+  useState,
+} from 'react'
+import Tooltip from '../Tooltip'
+
+const TOGGLE_RADIUS = '24px'
+const TOGGLE_POINT_RADIUS = '100px'
+
+const SIZES = {
+  large: {
+    ball: 16,
+    height: 24,
+    width: 48,
+  },
+  small: {
+    ball: 12,
+    height: 20,
+    width: 40,
+  },
+} as const
+
+const StyledToggle = styled.div<{
+  'data-checked': boolean
+  'data-disabled': boolean
+  size: 'small' | 'large'
+}>`
+  box-sizing: content-box;
+  outline: none;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  border: none;
+  border-radius: ${TOGGLE_RADIUS};
+  position: relative;
+  transition: all 300ms;
+  background-color: ${({ theme }) => theme.colors.neutral.textWeak};
+  width: ${({ size }) => SIZES[size].width}px;
+  height: ${({ size }) => SIZES[size].height}px;
+
+  &:after {
+    content: '';
+    position: absolute;
+    top: ${({ size }) => SIZES[size].height / 2 - SIZES[size].ball / 2}px;
+    left: 5px;
+    width: ${({ size }) => SIZES[size].ball}px;
+    height: ${({ size }) => SIZES[size].ball}px;
+    border-radius: ${TOGGLE_POINT_RADIUS};
+    background-color: ${({ theme }) => theme.colors.neutral.backgroundWeak};
+    transition: all 300ms;
+  }
+
+  &:focus-within,
+  &:focus {
+    box-shadow: ${({ theme }) => theme.shadows.focusPrimary};
+  }
+
+  &[data-disabled='false']:active:after {
+    width: ${({ size }) => SIZES[size].ball * 1.3775}px;
+  }
+
+  &[data-checked='true'] {
+    color: ${({ theme }) => theme.colors.neutral.textStrong};
+    background-color: ${({ theme }) => theme.colors.primary.backgroundStrong};
+
+    &:after {
+      left: calc(100% - 5px);
+      transform: translateX(-100%);
+    }
+
+    &:focus-within,
+    &:focus {
+      box-shadow: ${({ theme }) => theme.shadows.focusPrimary};
+    }
+  }
+
+  &[data-disabled='true'] {
+    background: ${({ theme }) => theme.colors.neutral.borderWeakDisabled};
+    &[data-checked='true'] {
+      background: ${({ theme }) =>
+        theme.colors.primary.backgroundStrongDisabled};
+    }
+  }
+`
+
+const StyledCheckbox = styled.input`
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+
+  &[disabled] {
+    cursor: not-allowed;
+  }
+`
+
+const StyledLabel = styled.label<{
+  size: 'small' | 'large'
+}>`
+  display: flex;
+  align-items: center;
+  width: fit-content;
+  cursor: pointer;
+
+  &:active ${StyledToggle}[data-disabled='false']:after {
+    width: ${({ size }) => SIZES[size].ball * 1.3775}px;
+  }
+
+  &[aria-disabled='true'] {
+    cursor: not-allowed;
+    color: ${({ theme }) => theme.colors.neutral.textDisabled};
+  }
+`
+
+const StyledLabelContent = styled.span<{
+  labelPosition: 'left' | 'right'
+}>`
+  ${({ theme, labelPosition }) =>
+    labelPosition === 'left'
+      ? `margin-right: ${theme.space['1']}`
+      : `margin-left: ${theme.space['1']}`}
+`
+
+const LabelContent = ({
+  label,
+  labelPosition,
+}: Required<Pick<ToggleProps, 'label' | 'labelPosition'>>) => {
+  if (typeof label === 'string') {
+    return (
+      <StyledLabelContent labelPosition={labelPosition}>
+        {label}
+      </StyledLabelContent>
+    )
+  }
+
+  return <>{label}</>
+}
+
+type ToggleProps = {
+  id?: string
+  checked?: boolean
+  name: string
+  tooltip?: string
+  onChange?: ChangeEventHandler<HTMLInputElement>
+  size?: 'large' | 'small'
+  labelPosition?: 'left' | 'right'
+  label?: ReactNode
+  disabled?: boolean
+}
+
+const Toggle = ({
+  checked = false,
+  disabled = false,
+  id,
+  name,
+  onChange,
+  size = 'large',
+  tooltip,
+  labelPosition = 'right',
+  label,
+}: ToggleProps) => {
+  const [state, setState] = useState(checked)
+
+  const onLocalChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      onChange?.(event)
+      setState(event.target.checked)
+    },
+    [onChange, setState],
+  )
+
+  return (
+    <Tooltip text={tooltip}>
+      <StyledLabel
+        aria-disabled={disabled}
+        size={size}
+        onClick={evt => evt.stopPropagation()}
+      >
+        {label && labelPosition === 'left' ? (
+          <LabelContent label={label} labelPosition={labelPosition} />
+        ) : null}
+        <StyledToggle size={size} data-checked={state} data-disabled={disabled}>
+          <StyledCheckbox
+            id={id || name}
+            aria-label={name}
+            checked={state}
+            aria-checked={state}
+            disabled={disabled}
+            name={name}
+            onChange={onLocalChange}
+            type="checkbox"
+          />
+        </StyledToggle>
+        {label && labelPosition === 'right' ? (
+          <LabelContent label={label} labelPosition={labelPosition} />
+        ) : null}
+      </StyledLabel>
+    </Tooltip>
+  )
+}
+
+export default Toggle
