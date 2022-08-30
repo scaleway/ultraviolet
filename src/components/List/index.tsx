8aece5191c4f4809e2bf8d92507b0d028020e8c6
Levende MOHAMMAD <lmohammad@scaleway.com>
2022-08-30 17:43:04 +0200

    feat: change loader by placeholder in List

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -15,7 +15,6 @@ import {
 } from 'react'
 import orderBy from '../../utils/orderBy'
 import Box from '../Box'
-import Loader from '../Loader'
 import Pagination from '../Pagination'
 import type { PaginationProps } from '../Pagination'
 import usePagination, { UsePaginationReturn } from '../Pagination/usePagination'
@@ -64,12 +63,6 @@ type BodyProps<DataType> = {
   children: (props: ListBodyRenderProps<DataType>) => JSX.Element
 }
 
-const StyledActivityContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  margin: ${({ theme }) => `${theme.space['2']} 0`};
-`
-
 const CenteredText = styled(Text)`
   text-align: center;
   margin: ${({ theme }) => theme.space['2']};
@@ -90,17 +83,10 @@ function Body<DataType extends Record<string, unknown>>({
     perPage,
   } = useListContext<DataType>()
 
-  const defaultLoader = useMemo(() => {
-    if (perPage) {
-      return <Placeholder length={perPage} variant="list" />
-    }
-
-    return (
-      <StyledActivityContainer>
-        <Loader active size={50} />
-      </StyledActivityContainer>
-    )
-  }, [perPage])
+  const defaultLoader = useMemo(
+    () => <Placeholder length={perPage} variant="list" />,
+    [perPage],
+  )
 
   if (isLoading) {
     return (customLoader as JSX.Element) ?? defaultLoader
