6ca3e61782d83215cc7b76ce7ee1be7d0eea0467
Matthias <mprost@scaleway.com>
2022-10-06 15:38:37 +0200

    docs: remove decorators from code snippets - global configuration

--- src/components/Menu/__stories__/index.stories.tsx
+++ src/components/Menu/__stories__/index.stories.tsx
@@ -16,7 +16,6 @@ export default {
         component:
           'A menu is a widget that offers a list of choices to the user, such as a set of actions or functions. A menu is usually opened, or made visible, by activating a menu button, choosing an item in a menu that opens a sub menu, or by invoking a command, such as Shift + F10 on Windows, that opens a context specific menu. When a user activates a choice in a menu, the menu usually closes unless the choice opened a submenu.',
       },
-      source: { excludeDecorators: true },
     },
   },
   title: 'Components/Navigation/Menu',
