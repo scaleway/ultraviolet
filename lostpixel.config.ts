6e97ae2510835a2ceb1864c1b74dbdb757fbf657
Matthias <mprost@scaleway.com>
2022-10-07 14:07:00 +0200

    ci: replace loki by lost-pixel

---
+++ lostpixel.config.ts
@@ -0,0 +1,9 @@
+import { CustomProjectConfig } from 'lost-pixel'
+
+export const config: CustomProjectConfig = {
+  storybookShots: {
+    storybookUrl: './storybook-static',
+  },
+  generateOnly: true,
+  failOnDifference: true,
+}
