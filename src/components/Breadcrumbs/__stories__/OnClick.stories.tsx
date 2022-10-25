045abf0832043a3ac32b82a235657b05c05edee6
QuiiBz <tlienard@scaleway.com>
2022-10-25 11:35:23 +0200

    fix: show proper code by spreading props

--- src/components/Breadcrumbs/__stories__/OnClick.stories.tsx
+++ src/components/Breadcrumbs/__stories__/OnClick.stories.tsx
@@ -2,11 +2,11 @@ import { Story } from '@storybook/react'
 import { ComponentProps, useState } from 'react'
 import Breadcrumbs from '..'
 
-export const OnClick: Story<ComponentProps<typeof Breadcrumbs>> = () => {
+export const OnClick: Story<ComponentProps<typeof Breadcrumbs>> = props => {
   const [value, setValue] = useState(1)
 
   return (
-    <Breadcrumbs variant="bubble" selected={value}>
+    <Breadcrumbs variant="bubble" selected={value} {...props}>
       <Breadcrumbs.Item onClick={(_, step) => setValue(step)}>
         Step 1
       </Breadcrumbs.Item>
