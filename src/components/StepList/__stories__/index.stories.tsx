a2486f6499cfd46d08b40e864f2cefe03d9cff01
Emmanuel Chambon <echambon@scaleway.com>
2022-11-23 16:52:50 +0100

    feat(StepList): allow complex children

--- src/components/StepList/__stories__/index.stories.tsx
+++ src/components/StepList/__stories__/index.stories.tsx
@@ -1,6 +1,8 @@
 import { Meta, Story } from '@storybook/react'
 import { ComponentProps } from 'react'
 import StepList, { Sizes as SizesType } from '..'
+import Stack from '../../Stack'
+import Text from '../../Text'
 
 export default {
   component: StepList,
@@ -100,6 +102,51 @@ BulletIcon.decorators = [
         check success
       </StepList.Item>
       <StepList.Item bulletIcon="check">check default</StepList.Item>
+    </StepList>
+  ),
+]
+
+export const ComplexChild = Template.bind({})
+BulletIcon.parameters = {
+  docs: {
+    story: {
+      description: 'With complex child bullet will stay aligned',
+    },
+  },
+}
+ComplexChild.decorators = [
+  () => (
+    <StepList>
+      <StepList.Item bulletIcon="check" variant="success">
+        <Stack gap={1}>
+          <Text as="h4" variant="headingSmall">
+            Lorem ipsum
+          </Text>
+          <Text as="p" variant="body">
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum
+          </Text>
+        </Stack>
+      </StepList.Item>
+      <StepList.Item bulletIcon="check">
+        <Stack gap={1}>
+          <Text as="h4" variant="headingSmall">
+            Sed ut perspiciatis
+          </Text>
+          <Text as="p" variant="body">
+            unde omnis iste natus error sit voluptatem accusantium doloremque
+            laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+            veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+            fugit, sed quia consequuntur
+          </Text>
+        </Stack>
+      </StepList.Item>
     </StepList>
   ),
 ]
