2b410200a8a290bc4218b807a0355484b5f33e34
Matthias <mprost@scaleway.com>
2022-11-25 11:25:36 +0100

    feat(toaster): add more options and update stories

--- src/components/Toaster/__stories__/index.stories.tsx
+++ src/components/Toaster/__stories__/index.stories.tsx
@@ -1,6 +1,5 @@
-import { Meta, Story } from '@storybook/react'
-import ToastContainer, { toast } from '..'
-import { Button } from '../..'
+import { Meta } from '@storybook/react'
+import ToastContainer from '..'
 
 export default {
   component: ToastContainer,
@@ -15,38 +14,6 @@ export default {
   title: 'Components/Feedback/Toaster',
 } as Meta
 
-const Template: Story = () => (
-  <>
-    <ToastContainer />
-    <div style={{ display: 'flex', gap: 8 }}>
-      <Button
-        variant="success"
-        onClick={() => toast.success('This is success')}
-      >
-        Success
-      </Button>
-    </div>
-  </>
-)
-
-export const Default = Template.bind({})
-
-export const Variants: Story = () => (
-  <>
-    <ToastContainer />
-    <div style={{ display: 'flex', gap: 8 }}>
-      <Button
-        variant="success"
-        onClick={() => toast.success('This is success')}
-      >
-        Success
-      </Button>
-      <Button variant="info" onClick={() => toast.info('This is info')}>
-        Info
-      </Button>
-      <Button variant="warning" onClick={() => toast.error('This is error')}>
-        Error
-      </Button>
-    </div>
-  </>
-)
+export { Playground } from './Playground.stories'
+export { Variants } from './Variants.stories'
+export { Positions } from './Positions.stories'
