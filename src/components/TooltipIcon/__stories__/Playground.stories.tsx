95a99e2fe4047df0f777d19d32f175bda1eed81b
Vincent Audebert <vaudebert@scaleway.com>
2022-12-06 11:14:36 +0100

    fix: issue with actions

--- src/components/TooltipIcon/__stories__/Playground.stories.tsx
+++ src/components/TooltipIcon/__stories__/Playground.stories.tsx
@@ -1,5 +1,3 @@
 import { Template } from './Template.stories'
 
 export const Playground = Template.bind({})
-
-Playground.args = { ...Template.args }
--- src/components/UnitInput/__stories__/Playground.stories.tsx
+++ src/components/UnitInput/__stories__/Playground.stories.tsx
@@ -2,8 +2,6 @@ import { Template } from './Template.stories'
 
 export const Playground = Template.bind({})
 
-Playground.args = { ...Template.args }
-
 Playground.parameters = {
   docs: {
     storyDescription: `UnitInput provide data as follows:
