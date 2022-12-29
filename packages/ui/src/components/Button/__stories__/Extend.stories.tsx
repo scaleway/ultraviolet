675458b5629576f4222d6719907ee5b2ad48b97d
Emmanuel Chambon <echambon@scaleway.com>
2022-12-29 19:58:31 +0100

    fix(Button): proper layout on extend buttons

--- packages/ui/src/components/Button/__stories__/Extend.stories.tsx
+++ packages/ui/src/components/Button/__stories__/Extend.stories.tsx
@@ -1,12 +1,75 @@
-import { Template } from './Template.stories'
+import type { ComponentStory } from '@storybook/react'
+import { Button } from '..'
+import { Stack } from '../../Stack'
 
-export const Extend = Template.bind({})
+const Buttons: ComponentStory<typeof Button> = ({ ...props }) => (
+  <>
+    <div>
+      <Button icon="plus" iconSize={24} extend size="large" {...props}>
+        Normal Button
+      </Button>
+    </div>
+    <div>
+      <Button icon="plus" iconSize={24} extend size="medium" {...props}>
+        Normal Button
+      </Button>
+    </div>
+    <div>
+      <Button icon="plus" iconSize={24} extend size="small" {...props}>
+        Normal Button
+      </Button>
+    </div>
+    <div>
+      <Button icon="plus" iconSize={24} extend size="xsmall" {...props}>
+        Normal Button
+      </Button>
+    </div>
+    <div>
+      <Button icon="plus" iconSize={24} extend size="xxsmall" {...props}>
+        Normal Button
+      </Button>
+    </div>
+    <div>
+      <Button
+        icon="plus"
+        iconSize={24}
+        extend
+        as="a"
+        href="https://scaleway.com"
+        {...props}
+      >
+        Link Button
+      </Button>
+    </div>
+    <div>
+      <Button
+        icon="plus"
+        iconSize={24}
+        extend
+        as="a"
+        href="https://scaleway.com"
+        size="small"
+        {...props}
+      >
+        Link Button
+      </Button>
+    </div>
+  </>
+)
 
-Extend.args = {
-  icon: 'plus',
-  extend: true,
-  children: 'Extend button !',
-}
+export const Extend: ComponentStory<typeof Button> = ({ ...props }) => (
+  <Stack gap={1} style={{ width: '100%' }}>
+    <Stack gap={1}>
+      <Buttons {...props} />
+    </Stack>
+    <Stack gap={1} alignItems="center">
+      <Buttons {...props} />
+    </Stack>
+    <Stack gap={1} alignItems="end">
+      <Buttons {...props} />
+    </Stack>
+  </Stack>
+)
 
 Extend.parameters = {
   docs: {
