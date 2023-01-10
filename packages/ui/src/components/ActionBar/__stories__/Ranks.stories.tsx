9ca6fbb8a808afe8e242b18678c49e5cc67b7a07
Matthias Prost <mprost@scaleway.com>
2023-01-10 14:42:53 +0100

    refactor(ActionBar): remove z-index by using portal and add animation on display (#2179)

--- packages/ui/src/components/ActionBar/__stories__/Ranks.stories.tsx
+++ packages/ui/src/components/ActionBar/__stories__/Ranks.stories.tsx
@@ -1,30 +1,14 @@
-import styled from '@emotion/styled'
-import type { ComponentStory } from '@storybook/react'
-import ActionBar from '..'
+import { Template } from './Template.stories'
 
-const FullHeightDiv = styled.div`
-  display: flex;
-  align-items: center;
-  height: 100%;
-  padding: 0 ${({ theme }) => theme.space['2']};
-`
+export const Ranks = Template.bind({})
 
-export const Ranks: ComponentStory<typeof ActionBar> = props => (
-  <>
-    <ActionBar {...props} rank={1}>
-      <FullHeightDiv>I am an Action Bar with rank 1</FullHeightDiv>
-    </ActionBar>
-    <ActionBar {...props} rank={2}>
-      <FullHeightDiv>I am an Action Bar with rank 2</FullHeightDiv>
-    </ActionBar>
-  </>
-)
+Ranks.args = {
+  rank: 1,
+}
 
 Ranks.parameters = {
   docs: {
-    story: {
-      description:
-        'You can choose the order of multiple `ActionBar` by using the `rank` prop.',
-    },
+    storyDescription:
+      'You can choose the order of multiple `ActionBar` by using the `rank` prop.',
   },
 }
