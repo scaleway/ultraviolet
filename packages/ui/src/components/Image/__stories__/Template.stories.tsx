9ba0a820bbf3cfb55078816af1a5de5962f712e9
Manu Chambon <echambon@scaleway.com>
2023-03-03 12:14:55 +0100

    feat: remove Image component (#2285)
    * feat: remove Image component
    
    * fix: update snaps

--- packages/ui/src/components/Image/__stories__/Template.stories.tsx
+++
@@ -1,7 +0,0 @@
-import type { ComponentStory } from '@storybook/react'
-import { Image } from '..'
-import defaultLogo from './scaleway-text.png'
-
-export const Template: ComponentStory<typeof Image> = ({ src, ...args }) => (
-  <Image src={src ?? defaultLogo} alt="Scaleway logo" {...args} />
-)
