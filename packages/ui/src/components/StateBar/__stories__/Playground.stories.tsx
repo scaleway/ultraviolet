b4e34331cf661d719dc08a4d002d97473b5c6dfc
Nicolas Randrianarivo <nrandrianarivo@scaleway.com>
2023-03-17 18:00:58 +0100

    feat(StateBar): remove StateBar component

--- packages/ui/src/components/StateBar/__stories__/Playground.stories.tsx
+++
@@ -1,9 +0,0 @@
-import type { ComponentStory } from '@storybook/react'
-import { StateBar } from '..'
-
-export const Playground: ComponentStory<typeof StateBar> = ({ ...props }) => (
-  <StateBar {...props}>
-    <StateBar.State label="Label">your value</StateBar.State>
-    <StateBar.Bar value={50} />
-  </StateBar>
-)
