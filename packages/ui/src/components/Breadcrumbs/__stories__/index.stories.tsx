10b7bd73ffc72e2cb41bfb1f3e44029d85b0c758
Matthias <mprost@scaleway.com>
2023-03-23 11:51:23 +0100

    refactor(Breadcrumbs): remove variants

--- packages/ui/src/components/Breadcrumbs/__stories__/index.stories.tsx
+++ packages/ui/src/components/Breadcrumbs/__stories__/index.stories.tsx
@@ -8,7 +8,6 @@ export default {
 } as Meta
 
 export { Playground } from './Playground.stories'
-export { Variants } from './Variants.stories'
 export { Selected } from './Selected.stories'
 export { To } from './To.stories'
 export { OnClick } from './OnClick.stories'
