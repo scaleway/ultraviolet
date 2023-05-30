81452f462fd7c17f591784dc91d53b4532278f05
Clément Lafont <clafont@scaleway.com>
2023-05-30 10:55:35 +0200

    ci: fix tsc storybook (#2573)

--- packages/ui/src/components/Radio/__stories__/Template.stories.tsx
+++ packages/ui/src/components/Radio/__stories__/Template.stories.tsx
@@ -1,9 +1,7 @@
-import type { ComponentStory } from '@storybook/react'
+import type { StoryFn } from '@storybook/react'
 import { Radio } from '..'
 
-export const Template: ComponentStory<typeof Radio> = args => (
-  <Radio {...args} />
-)
+export const Template: StoryFn<typeof Radio> = args => <Radio {...args} />
 
 Template.args = {
   checked: false,
