0719ade2d91b09851eb3a6f94807b3732c878e96
Antoine Le Taxin <aletaxin@scaleway.com>
2023-08-29 14:47:01 +0200

    test(Modal): add story for Modal with Tooltip (#2808)

---
+++ .changeset/twelve-masks-collect.md
@@ -0,0 +1,5 @@
+---
+'@ultraviolet/ui': patch
+---
+
+Add z-index to the Modal backdrop to be above other components DS-526
---
+++ packages/ui/src/components/Modal/__stories__/WithTooltip.stories.tsx
@@ -0,0 +1,12 @@
+import type { StoryFn } from '@storybook/react'
+import { Button } from '../../Button'
+import { Tooltip } from '../../Tooltip'
+import { Modal } from '../index'
+
+export const WithTooltip: StoryFn = props => (
+  <Modal {...props} disclosure={<Button>With a Tooltip</Button>}>
+    <Tooltip placement="top" text="The Tooltip should be on top of the Modal.">
+      Content with a tooltip
+    </Tooltip>
+  </Modal>
+)
