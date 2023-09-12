5c606e69436171b007d24f99e2b3a2c70199c75d
QuiiBz <tlienard@scaleway.com>
2023-09-12 15:44:41 +0200

    fix(modal): dialog should use overflow y

---
+++ .changeset/happy-squids-design.md
@@ -0,0 +1,5 @@
+---
+'@ultraviolet/ui': patch
+---
+
+Dialog should only prevent overflow y, not overflow x
--- packages/ui/src/components/Modal/Dialog.tsx
+++ packages/ui/src/components/Modal/Dialog.tsx
@@ -132,9 +132,9 @@ export const Dialog = ({
   // Handle body scroll
   useEffect(() => {
     if (open && preventBodyScroll) {
-      document.body.style.overflow = 'hidden'
+      document.body.style.overflowY = 'hidden'
     } else {
-      document.body.style.overflow = 'auto'
+      document.body.style.overflowY = 'auto'
     }
   }, [preventBodyScroll, open])
 
