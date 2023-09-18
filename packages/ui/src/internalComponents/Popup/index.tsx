ccd7535458f3327a47b2e748f2900ea18bd4090f
QuiiBz <tlienard@scaleway.com>
2023-09-18 09:58:51 +0200

    fix: tabIndex on popup

--- packages/ui/src/internalComponents/Popup/index.tsx
+++ packages/ui/src/internalComponents/Popup/index.tsx
@@ -352,6 +352,7 @@ export const Popup = forwardRef(
           onPointerEnter={!isControlled ? onPointerEvent(true) : noop}
           onPointerLeave={!isControlled ? onPointerEvent(false) : noop}
           ref={childrenRef}
+          tabIndex={0}
           onKeyDown={onKeyDown}
           data-container-full-width={containerFullWidth}
         >
