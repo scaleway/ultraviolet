88ec8f4ca3692245e49278d66e26fe9b6dcd1030
Matthias Prost <mprost@scaleway.com>
2023-10-25 14:52:09 +0200

    fix(TagInput): placeholder to take full width (#3046)

--- packages/ui/src/components/TagInput/__stories__/Playground.stories.tsx
+++ packages/ui/src/components/TagInput/__stories__/Playground.stories.tsx
@@ -4,4 +4,6 @@ export const Playground = Template.bind({})
 
 Playground.args = {
   tags: ['default'],
+  placeholder:
+    'This is a very long placeholder to see what happen when it is too long',
 }
