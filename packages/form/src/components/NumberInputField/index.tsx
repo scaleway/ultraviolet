f1d248c74c32246014fc600d4b014856ad8d9893
Dorian Maliszewski <dmaliszewski@scaleway.com>
2023-10-27 14:53:46 +0000

    fix: test on NumberInputField and weird NumberInput behaviour

--- packages/form/src/components/NumberInputField/index.tsx
+++ packages/form/src/components/NumberInputField/index.tsx
@@ -2,6 +2,7 @@ import { NumberInput } from '@ultraviolet/ui'
 import type { ComponentProps, FocusEvent, FocusEventHandler } from 'react'
 import type { FieldValues } from 'react-hook-form'
 import { Controller } from 'react-hook-form'
+import { useErrors } from '../../providers'
 import type { BaseFieldProps } from '../../types'
 
 type NumberInputValueFieldProps<TFieldValues extends FieldValues> =
@@ -42,38 +43,49 @@ export const NumberInputField = <TFieldValues extends FieldValues>({
   text,
   rules,
   className,
-}: NumberInputValueFieldProps<TFieldValues>) => (
-  <Controller
-    name={name}
-    rules={{
-      required,
-      max: maxValue,
-      min: minValue,
-      ...rules,
-    }}
-    render={({ field }) => (
-      <NumberInput
-        name={field.name}
-        value={field.value}
-        disabled={disabled}
-        onBlur={(event: FocusEvent<HTMLInputElement>) => {
-          field.onBlur()
-          onBlur?.(event)
-        }}
-        onChange={event => {
-          field.onChange(event)
-          onChange?.(event)
-        }}
-        onFocus={onFocus}
-        maxValue={maxValue}
-        minValue={minValue}
-        onMinCrossed={onMinCrossed}
-        onMaxCrossed={onMaxCrossed}
-        size={size}
-        step={step}
-        text={text}
-        className={className}
-      />
-    )}
-  />
-)
+  label,
+}: NumberInputValueFieldProps<TFieldValues>) => {
+  const { getError } = useErrors()
+
+  return (
+    <Controller
+      name={name}
+      rules={{
+        required,
+        max: maxValue,
+        min: minValue,
+        ...rules,
+      }}
+      render={({ field, fieldState: { error } }) => (
+        <NumberInput
+          name={field.name}
+          value={field.value}
+          disabled={disabled}
+          onBlur={(event: FocusEvent<HTMLInputElement>) => {
+            field.onBlur()
+            onBlur?.(event)
+          }}
+          onChange={event => {
+            // React hook form doesnt allow undefined values after definition https://react-hook-form.com/docs/usecontroller/controller (that make sense)
+            field.onChange(event ?? null)
+            onChange?.(event)
+          }}
+          onFocus={onFocus}
+          maxValue={maxValue}
+          minValue={minValue}
+          onMinCrossed={onMinCrossed}
+          onMaxCrossed={onMaxCrossed}
+          size={size}
+          step={step}
+          text={text}
+          className={className}
+          label={label}
+          error={getError(
+            { label: label ?? '', max: maxValue, min: minValue },
+            error,
+          )}
+        />
+      )}
+    />
+  )
+}
