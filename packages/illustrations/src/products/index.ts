b76b22ddb23b86e4ad2629883b1b719f7031b76b
Matthias <mprost@scaleway.com>
2023-11-03 15:26:38 +0100

    fix: add new illustrations

--- packages/illustrations/src/products/index.ts
+++ packages/illustrations/src/products/index.ts
@@ -1,65 +1,33 @@
-import { appleSiliconOriginal } from './appleSilicon'
-import { blockStorageOriginal } from './blockStorage'
-import { cockpitOriginal } from './cockpit'
-import { containerRegistryOriginal } from './containerRegistry'
-import { containersOriginal } from './containers'
-import { dediboxOriginal } from './dedibox'
-import { documentDBOriginal } from './documentDB'
-import { domainsAndDnsOriginal } from './domainsAndDns'
-import { elasticMetalOriginal } from './elasticMetal'
-import { functionsOriginal } from './functions'
-import { iamOriginal } from './iam'
-import { instanceOriginal } from './instance'
-import { iotHubOriginal } from './iotHub'
-import { ipfsNamingOriginal } from './ipfsNaming'
-import { ipfsPinningOriginal } from './ipfsPinning'
-import { k8sKapsuleOriginal } from './k8sKapsule'
-import { k8sKosmosOriginal } from './k8sKosmos'
-import { loadBalancersOriginal } from './loadBalancers'
-import { managedDatabaseOriginal } from './managedDatabase'
-import { natsOriginal } from './nats'
-import { objectStorageOriginal } from './objectStorage'
-import { publicGatewayOriginal } from './publicGateway'
-import { redisOriginal } from './redis'
-import { secretManagerOriginal } from './secretManager'
-import { serverlessDBOriginal } from './serverlessDB'
-import { serverlessJobOriginal } from './serverlessJob'
-import { snsOriginal } from './sns'
-import { sqsOriginal } from './sqs'
-import { transactionalEmailOriginal } from './transactionalEmail'
-import { vpcOriginal } from './vpc'
-import { webHostingOriginal } from './webHosting'
-
-export default {
-  appleSiliconOriginal,
-  cockpitOriginal,
-  containerRegistryOriginal,
-  containersOriginal,
-  dediboxOriginal,
-  domainsAndDnsOriginal,
-  elasticMetalOriginal,
-  functionsOriginal,
-  instanceOriginal,
-  iotHubOriginal,
-  ipfsPinningOriginal,
-  k8sKapsuleOriginal,
-  k8sKosmosOriginal,
-  loadBalancersOriginal,
-  managedDatabaseOriginal,
-  natsOriginal,
-  objectStorageOriginal,
-  publicGatewayOriginal,
-  redisOriginal,
-  secretManagerOriginal,
-  snsOriginal,
-  sqsOriginal,
-  transactionalEmailOriginal,
-  vpcOriginal,
-  webHostingOriginal,
-  blockStorageOriginal,
-  documentDBOriginal,
-  ipfsNamingOriginal,
-  iamOriginal,
-  serverlessDBOriginal,
-  serverlessJobOriginal,
-}
+/* eslint-disable no-restricted-syntax */
+export * from './appleSilicon'
+export * from './blockStorage'
+export * from './cockpit'
+export * from './containerRegistry'
+export * from './containers'
+export * from './dedibox'
+export * from './documentDB'
+export * from './domainsAndDns'
+export * from './elasticMetal'
+export * from './functions'
+export * from './iam'
+export * from './instance'
+export * from './iotHub'
+export * from './ipfsNaming'
+export * from './ipfsPinning'
+export * from './k8sKapsule'
+export * from './k8sKosmos'
+export * from './loadBalancers'
+export * from './managedDatabase'
+export * from './nats'
+export * from './objectStorage'
+export * from './publicGateway'
+export * from './redis'
+export * from './secretManager'
+export * from './serverlessDB'
+export * from './serverlessJob'
+export * from './sns'
+export * from './sqs'
+export * from './transactionalEmail'
+export * from './vpc'
+export * from './webHosting'
+/* eslint-enable no-restricted-syntax */
