Cookiecrisp34/thugggggg

refactor(form): migrate to react hook form

packages/form/src/hooks/useField.ts
@@ -1,0 +1,48 @@
+import type {
+  FieldPath,
+  FieldPathValue,
+  FieldValues,
+  Path,
+  Validate,
+} from 'react-hook-form'
+import { useController, useFormContext } from 'react-hook-form'
+
+type Options<TFieldValues extends FieldValues> = {
+  subscription?: Record<string, boolean>
+  validate?: Validate<
+    FieldPathValue<TFieldValues, Path<TFieldValues>>,
+    TFieldValues
+  >
+}
+
+/**
+ * @deprecated
+ * @todo rename to useFieldDeprecated
+ */
+export const useField = <
+  T,
+  TFieldValues extends FieldValues = FieldValues,
+  TFieldName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>(
+  name: TFieldName,
+  options?: Options<TFieldValues>,
+) => {
+  const { getValues } = useFormContext<TFieldValues>()
+  const { field, fieldState } = useController<TFieldValues>({
+    name,
+    rules: { validate: options?.validate },
+  })
+
+  return {
+    input: {
+      value: getValues(name) as T,
+      onChange: field.onChange,
+    },
+    meta: {
+      error: fieldState.error?.message,
+      touched: fieldState.isTouched,
+      invalid: fieldState.invalid,
+      dirty: fieldState.isDirty,
+    },
+  }
+}

scaleway/ultraviolet

packages/form/src/hooks/useField.ts
@@ -1,0 +1,47 @@
+import type {
+  FieldPath,
+  FieldPathValue,
+  FieldValues,
+  Path,
+  Validate,
+} from 'react-hook-form'
+import { useController, useFormContext } from 'react-hook-form'
+
+type Options<TFieldValues extends FieldValues> = {
+  subscription?: Record<string, boolean>
+  validate?: Validate<
+    FieldPathValue<TFieldValues, Path<TFieldValues>>,
+    TFieldValues
+  >
+}
+
+/**
+ * @deprecated
+ */
+export const useFieldDeprecated = <
+  T,
+  TFieldValues extends FieldValues = FieldValues,
+  TFieldName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>(
+  name: TFieldName,
+  options?: Options<TFieldValues>,
+) => {
+  const { getValues } = useFormContext<TFieldValues>()
+  const { field, fieldState } = useController<TFieldValues>({
+    name,
+    rules: { validate: options?.validate },
+  })
+
+  return {
+    input: {
+      value: getValues(name) as T,
+      onChange: field.onChange,
+    },
+    meta: {
+      error: fieldState.error?.message,
+      touched: fieldState.isTouched,
+      invalid: fieldState.invalid,
+      dirty: fieldState.isDirty,
+    },
+  }
+}