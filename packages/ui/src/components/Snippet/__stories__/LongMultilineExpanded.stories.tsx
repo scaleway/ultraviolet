b281f2ee16d666ce6c294783b327b70a04a11674
Matthias <mprost@scaleway.com>
2023-11-13 16:52:19 +0100

    feat(Snippet): new prop initially expanded on component

---
+++ packages/ui/src/components/Snippet/__stories__/LongMultilineExpanded.stories.tsx
@@ -0,0 +1,37 @@
+import { Template } from './Template.stories'
+
+export const LongMultilineExpanded = Template.bind({})
+
+LongMultilineExpanded.args = {
+  children: `
+# Install the package and start it
+pnpm add @ultraviolet/ui
+pnpm install
+pnpm start
+
+# Build
+pnpm build
+
+# Test
+pnpm test:unit
+# Install the package and start it
+pnpm add @ultraviolet/ui
+pnpm install
+pnpm start
+
+# Build
+pnpm build
+
+# Test
+pnpm test:unit
+`,
+  initiallyExpanded: true,
+}
+
+LongMultilineExpanded.parameters = {
+  docs: {
+    description: {
+      story: `Using the prop \`initiallyExpanded\` you can set the snippet to be expanded by default.`,
+    },
+  },
+}
