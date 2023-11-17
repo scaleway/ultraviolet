24476f7f9aa1eb122774e364a893942e7da97b9e
Clément Lafont <clafont@scaleway.com>
2023-11-17 18:19:25 +0100

    refactor: index exports

--- packages/form/src/hooks/useFormState.ts
+++ packages/form/src/hooks/useFormState.ts
@@ -1,5 +1,5 @@
 import type { FieldValues } from 'react-hook-form'
-import { useFormContext } from 'react-hook-form'
+import { useFormContext, useWatch } from 'react-hook-form'
 
 type UseFormStateParams = {
   subscription?: Record<string, boolean>
@@ -12,12 +12,12 @@ export const useFormStateDeprecated = <TFieldValues extends FieldValues>(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   _params?: UseFormStateParams,
 ) => {
-  const { formState, watch } = useFormContext<TFieldValues>()
+  const { formState } = useFormContext<TFieldValues>()
 
   return {
     dirtySinceLastSubmit: formState.isDirty,
     submitErrors: formState.errors.root,
-    values: watch(),
+    values: useWatch() as TFieldValues,
     hasValidationErrors: !formState.isValid,
     pristine: !formState.isDirty,
     errors: formState.errors,
