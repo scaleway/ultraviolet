f09997260c7dd58a4d4a6a920ebfe9af5d8589ff
Clément Lafont <clafont@scaleway.com>
2023-11-22 16:55:03 +0100

    fix: tsc

--- packages/form/src/components/Submit/__stories__/Invalid.stories.tsx
+++ packages/form/src/components/Submit/__stories__/Invalid.stories.tsx
@@ -2,13 +2,11 @@ import type { StoryFn } from '@storybook/react'
 import { Submit } from '..'
 import { mockErrors } from '../../../mocks'
 import { Form } from '../../Form'
+import { TextInputField } from '../../TextInputField'
 
 export const Invalid: StoryFn = () => (
-  <Form
-    onRawSubmit={() => {}}
-    errors={mockErrors}
-    validate={() => ({ fake: 'error' })}
-  >
+  <Form onRawSubmit={() => {}} errors={mockErrors}>
+    <TextInputField required disabled name="invalid" />
     <Submit>This form is invalid</Submit>
   </Form>
 )
