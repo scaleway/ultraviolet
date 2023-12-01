7f397e1ca87a895f8b575d9748e632e5dd523e3b
Matthias Prost <mprost@scaleway.com>
2023-12-01 11:53:59 +0100

    fix(Text): one line prop to be working on modals (#3200)

--- packages/ui/src/components/Modal/__stories__/WithTooltip.stories.tsx
+++ packages/ui/src/components/Modal/__stories__/WithTooltip.stories.tsx
@@ -1,12 +1,28 @@
 import type { StoryFn } from '@storybook/react'
 import { Button } from '../../Button'
-import { Tooltip } from '../../Tooltip'
+import { Row } from '../../Row'
+import { Stack } from '../../Stack'
+import { Text } from '../../Text'
 import { Modal } from '../index'
 
 export const WithTooltip: StoryFn = props => (
   <Modal {...props} disclosure={<Button>With a Tooltip</Button>}>
-    <Tooltip text="The Tooltip should be on top of the Modal.">
-      Content with a tooltip
-    </Tooltip>
+    <Row templateColumns="4fr 2fr 2fr 1fr" alignItems="center">
+      <Text as="span" variant="body" oneLine>
+        This-is-a-really-long-file-name-that-will-be-truncated.svg
+      </Text>
+
+      <Stack alignItems="end" justifyContent="center">
+        <Text variant="body" as="span">
+          130.5 MB
+        </Text>
+      </Stack>
+
+      <Stack alignItems="end" justifyContent="center">
+        <Text variant="body" as="span">
+          Uploaded
+        </Text>
+      </Stack>
+    </Row>
   </Modal>
 )
---
+++ packages/ui/src/hooks/useIsOverflowing.ts
@@ -0,0 +1,40 @@
+import type { MutableRefObject } from 'react'
+import { useEffect, useState } from 'react'
+
+/**
+ * This hook checks if the element has overflow based on the offsetWidth and scrollWidth of the element.
+ */
+export const useIsOverflowing = (
+  ref: MutableRefObject<HTMLElement | HTMLDivElement | undefined | null>,
+  callback?: (hasOverflow: boolean) => void,
+) => {
+  const [isOverflowing, setIsOverflowing] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (!ref.current) {
+        return
+      }
+
+      const element = ref.current
+
+      const hasOverflow = element.clientWidth < element.scrollWidth
+      setIsOverflowing(hasOverflow)
+      if (callback) callback(hasOverflow)
+    }
+
+    // This will add the function into the browser event queue after the DOM is painted
+    // which is needed to get the correct offsetWidth and scrollWidth
+    setTimeout(() => handleResize(), 0)
+
+    // Listen for resize events
+    window.addEventListener('resize', handleResize)
+
+    // Cleanup the event listener
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [ref, callback])
+
+  return isOverflowing
+}
