8c4140be4737b40374e41fc6518053a5bb071383
Matthias <mprost@scaleway.com>
2023-12-19 14:24:05 +0100

    fix: run svgo on all svgs

--- svgo.config.js
+++ svgo.config.js
@@ -1,12 +1,23 @@
 module.exports = {
+  multipass: true,
+  js2svg: {
+    pretty: true,
+  },
   plugins: [
     {
       name: 'preset-default',
       params: {
         overrides: {
-          // customize the params of a default plugin
-          inlineStyles: {
-            onlyMatchedOnce: false,
+          // this will ensure we keep className essential for theme switch
+          removeUnknownsAndDefaults: {
+            unknownContent: true,
+            unknownAttrs: false,
+            defaultAttrs: true,
+            defaultMarkupDeclarations: true,
+            uselessOverrides: true,
+            keepDataAttrs: true,
+            keepAriaAttrs: true,
+            keepRoleAttr: false,
           },
         },
       },
