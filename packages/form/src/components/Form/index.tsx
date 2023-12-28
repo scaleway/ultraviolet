Cookiecrisp34/thugggggg

refactor(form): migrate to react hook form

packages/form/src/components/Form/index.tsx
@@ -1,75 +1,167 @@
-import arrayMutators from 'final-form-arrays'
-import type { JSX, ReactNode } from 'react'
+import { FORM_ERROR } from 'final-form'
+import type { ReactNode } from 'react'
+import React, { useMemo, useRef } from 'react'
 import type {
-  FormRenderProps,
-  FormProps as ReactFinalFormProps,
-} from 'react-final-form'
-import { Form as ReactFinalForm } from 'react-final-form'
+  DefaultValues,
+  FieldErrors,
+  FieldPath,
+  FieldValues,
+  UseFormHandleSubmit,
+  UseFormReturn,
+} from 'react-hook-form'
+import { FormProvider, useForm } from 'react-hook-form'
 import { ErrorProvider } from '../../providers'
 import type { FormErrors } from '../../types'
 
-export type FormProps<FormValues = unknown> = {
-  children?:
-    | ((props: FormRenderProps<FormValues, Partial<FormValues>>) => ReactNode)
-    | ReactNode
-  errors: FormErrors
-  /**
-   * onRawSubmit is the base onSubmit from final-form
-   */
-  onRawSubmit: ReactFinalFormProps<FormValues, Partial<FormValues>>['onSubmit']
-  initialValues?: Partial<FormValues>
-  validateOnBlur?: ReactFinalFormProps<
-    FormValues,
-    Partial<FormValues>
-  >['validateOnBlur']
-  validate?: ReactFinalFormProps<FormValues, Partial<FormValues>>['validate']
-  /**
-   * The form name attribute
-   */
-  name?: string
-  render?: ReactFinalFormProps<FormValues, Partial<FormValues>>['render']
-  mutators?: ReactFinalFormProps<FormValues, Partial<FormValues>>['mutators']
-  keepDirtyOnReinitialize?: boolean
-  className?: string
+type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never }
+type SingleXOR<T, U> = T | U extends object
+  ? (Without<T, U> & U) | (Without<U, T> & T)
+  : T | U
+
+export type XOR<T extends unknown[]> = T extends [infer Only]
+  ? Only
+  : T extends [infer A, infer B, ...infer Rest]
+  ? XOR<[SingleXOR<A, B>, ...Rest]>
+  : never
+
+type FormStateReturn<TFormValues extends FieldValues> = {
+  values: TFormValues
+  hasValidationErrors: boolean
+  errors: FieldErrors<TFormValues>
+  submitting: boolean
+  pristine: boolean
+  handleSubmit: () => Promise<void>
+  submitError: boolean
+  form: {
+    change: UseFormReturn<TFormValues>['setValue']
+    reset: UseFormReturn<TFormValues>['reset']
+    submit: () => Promise<void>
+  }
 }
 
-export const Form = <FormValues,>({
+type OnRawSubmitReturn =
+  | { [FORM_ERROR]?: string }
+  | undefined
+  | null
+  | string
+  | void
+  | boolean
+  | number
+
+type FormSubmitContextValue<TFieldValues extends FieldValues = FieldValues> = {
+  handleSubmit: ReturnType<UseFormHandleSubmit<TFieldValues>>
+}
+
+export const FormSubmitContext = React.createContext<FormSubmitContextValue>(
+  {} as FormSubmitContextValue,
+)
+
+export type FormProps<TFormValues extends FieldValues> = {
+  children?: ((props: FormStateReturn<TFormValues>) => ReactNode) | ReactNode
+  errors: FormErrors
+  validate?: (
+    values: TFormValues,
+  ) => Partial<Record<FieldPath<TFormValues>, string>>
+  onRawSubmit: (
+    data: TFormValues,
+    formState: {
+      reset: UseFormReturn<TFormValues>['reset']
+      resetFieldState: UseFormReturn<TFormValues>['resetField']
+      restart: () => void
+    },
+  ) => Promise<OnRawSubmitReturn> | OnRawSubmitReturn
+} & XOR<
+  [
+    { initialValues?: DefaultValues<TFormValues> },
+    { methods: UseFormReturn<TFormValues> },
+  ]
+>
+
+export const Form = <TFormValues extends FieldValues>({
   children,
-  onRawSubmit,
-  errors,
+  methods: methodsProp,
   initialValues,
-  validateOnBlur,
-  validate,
-  name,
-  render,
-  mutators,
-  keepDirtyOnReinitialize,
-  className,
-}: FormProps<FormValues>): JSX.Element => (
-  <ReactFinalForm
-    initialValues={initialValues}
-    validateOnBlur={validateOnBlur}
-    validate={validate}
-    mutators={{
-      ...arrayMutators,
-      ...mutators,
-    }}
-    onSubmit={onRawSubmit}
-    render={
-      render ??
-      (renderProps => (
+  errors,
+  onRawSubmit,
+}: FormProps<TFormValues>) => {
+  const methodsHook = useForm({
+    defaultValues: initialValues,
+    mode: 'onChange',
+    // resolver: !validate
+    //   ? undefined
+    //   : values => {
+    //       const validateErrors = validate(values)
+    //
+    //       if (errors) {
+    //         return {
+    //           values: {},
+    //           errors: Object.entries(validateErrors).reduce(
+    //             (allErrors, [field, error]) => ({
+    //               ...allErrors,
+    //               [field]: {
+    //                 type: error,
+    //               },
+    //             }),
+    //             {},
+    //           ),
+    //         }
+    //       }
+    //
+    //       return {
+    //         values,
+    //         errors: {},
+    //       }
+    //     },
+  })
+
+  const methods = !methodsProp ? methodsHook : methodsProp
+
+  const formRef = useRef<HTMLFormElement>(null)
+
+  const handleSubmit = methods.handleSubmit(async values => {
+    const result = await onRawSubmit(values, {
+      reset: methods.reset,
+      resetFieldState: methods.resetField,
+      restart: () => methods.reset(initialValues),
+    })
+    if (result && typeof result !== 'boolean' && typeof result !== 'number') {
+      methods.setError('root.submit', {
+        type: 'custom',
+        message: typeof result === 'object' ? result[FORM_ERROR] : result,
+      })
+    }
+  })
+
+  const formSubmitContextValue = useMemo(
+    () => ({ handleSubmit }),
+    [handleSubmit],
+  )
+
+  return (
+    <FormProvider {...methods}>
+      <FormSubmitContext.Provider value={formSubmitContextValue}>
         <ErrorProvider errors={errors}>
-          <form
-            noValidate
-            name={name}
-            onSubmit={renderProps.handleSubmit}
-            className={className}
-          >
-            {typeof children === 'function' ? children(renderProps) : children}
+          <form onSubmit={handleSubmit} ref={formRef}>
+            {typeof children === 'function'
+              ? children({
+                  values: methods.watch(),
+                  hasValidationErrors: !methods.formState.isValid,
+                  errors: methods.formState.errors,
+                  submitting: methods.formState.isSubmitting,
+                  pristine: !methods.formState.isDirty,
+                  handleSubmit,
+                  submitError: !!methods.formState.errors?.root?.['submit'],
+                  form: {
+                    change: methods.setValue,
+                    // submit: () => formRef.current?.submit(),
+                    reset: methods.reset,
+                    submit: handleSubmit,
+                  },
+                })
+              : children}
           </form>
         </ErrorProvider>
-      ))
-    }
-    keepDirtyOnReinitialize={keepDirtyOnReinitialize}
-  />
-)
+      </FormSubmitContext.Provider>
+    </FormProvider>
+  )
+}

packages/form/src/components/Submit/index.tsx
@@ -1,7 +1,6 @@
 import { Button } from '@ultraviolet/ui'
-import type { ComponentProps, JSX, ReactNode } from 'react'
-import { useEffect, useState } from 'react'
-import { useFormState } from 'react-final-form'
+import type { ComponentProps, ReactNode } from 'react'
+import { useFormContext } from 'react-hook-form'
 
 type SubmitProps = {
   children?: ReactNode
@@ -29,24 +28,12 @@ export const Submit = ({
   tooltip,
   fullWidth,
   onClick,
-}: SubmitProps): JSX.Element => {
-  const { invalid, submitting, hasValidationErrors, dirtySinceLastSubmit } =
-    useFormState({
-      subscription: {
-        dirtySinceLastSubmit: true,
-        hasValidationErrors: true,
-        invalid: true,
-        submitting: true,
-      },
-    })
-  const [isLoading, setIsLoading] = useState(true)
-  const isDisabled =
-    disabled ||
-    submitting ||
-    isLoading ||
-    (invalid && hasValidationErrors && !dirtySinceLastSubmit)
+}: SubmitProps) => {
+  const { formState } = useFormContext()
 
-  useEffect(() => setIsLoading(false), [])
+  const { isSubmitting, isValid } = formState
+
+  const isDisabled = disabled || isSubmitting || !isValid
 
   return (
     <Button
@@ -54,7 +41,7 @@ export const Submit = ({
       disabled={isDisabled}
       icon={icon}
       iconPosition={iconPosition}
-      isLoading={submitting}
+      isLoading={isSubmitting}
       size={size}
       type="submit"
       variant={variant}

scaleway/ultraviolet

packages/form/src/components/Form/index.tsx
@@ -1,75 +1,146 @@
-import arrayMutators from 'final-form-arrays'
 import type { ReactNode } from 'react'
+import React, { useMemo } from 'react'
 import type {
-  FormRenderProps,
-  FormProps as ReactFinalFormProps,
-} from 'react-final-form'
-import { Form as ReactFinalForm } from 'react-final-form'
+  DefaultValues,
+  FieldErrors,
+  FieldPath,
+  FieldValues,
+  UseFormHandleSubmit,
+  UseFormReturn,
+} from 'react-hook-form'
+import { FormProvider, useForm } from 'react-hook-form'
+import { FORM_ERROR } from '../../constants'
 import { ErrorProvider } from '../../providers'
 import type { FormErrors } from '../../types'
+import { defaultErrors } from './defaultErrors'
 
-export type FormProps<FormValues = unknown> = {
-  children?:
-    | ((props: FormRenderProps<FormValues, Partial<FormValues>>) => ReactNode)
-    | ReactNode
-  errors: FormErrors
-  /**
-   * onRawSubmit is the base onSubmit from final-form
-   */
-  onRawSubmit: ReactFinalFormProps<FormValues, Partial<FormValues>>['onSubmit']
-  initialValues?: Partial<FormValues>
-  validateOnBlur?: ReactFinalFormProps<
-    FormValues,
-    Partial<FormValues>
-  >['validateOnBlur']
-  validate?: ReactFinalFormProps<FormValues, Partial<FormValues>>['validate']
-  /**
-   * The form name attribute
-   */
-  name?: string
-  render?: ReactFinalFormProps<FormValues, Partial<FormValues>>['render']
-  mutators?: ReactFinalFormProps<FormValues, Partial<FormValues>>['mutators']
-  keepDirtyOnReinitialize?: boolean
-  className?: string
+type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never }
+type SingleXOR<T, U> = T | U extends object
+  ? (Without<T, U> & U) | (Without<U, T> & T)
+  : T | U
+
+export type XOR<T extends unknown[]> = T extends [infer Only]
+  ? Only
+  : T extends [infer A, infer B, ...infer Rest]
+  ? XOR<[SingleXOR<A, B>, ...Rest]>
+  : never
+
+type FormStateReturn<TFormValues extends FieldValues> = {
+  values: TFormValues
+  hasValidationErrors: boolean
+  errors: FieldErrors<TFormValues>
+  submitting: boolean
+  pristine: boolean
+  handleSubmit: () => Promise<void>
+  submitError: boolean
+  valid: boolean
+  form: {
+    change: UseFormReturn<TFormValues>['setValue']
+    reset: UseFormReturn<TFormValues>['reset']
+    submit: () => Promise<void>
+  }
 }
 
-export const Form = <FormValues,>({
+type OnRawSubmitReturn =
+  | { [FORM_ERROR]?: string }
+  | undefined
+  | null
+  | string
+  | void
+  | boolean
+  | number
+
+type FormSubmitContextValue<TFieldValues extends FieldValues = FieldValues> = {
+  handleSubmit: ReturnType<UseFormHandleSubmit<TFieldValues>>
+}
+
+export const FormSubmitContext = React.createContext<FormSubmitContextValue>(
+  {} as FormSubmitContextValue,
+)
+
+export type FormProps<TFormValues extends FieldValues = FieldValues> = {
+  children?: ((props: FormStateReturn<TFormValues>) => ReactNode) | ReactNode
+  errors: FormErrors
+  name?: string
+  validate?: (
+    values: TFormValues,
+  ) => Partial<Record<FieldPath<TFormValues>, string>>
+  onRawSubmit: (
+    data: TFormValues,
+    formState: {
+      reset: UseFormReturn<TFormValues>['reset']
+      resetFieldState: UseFormReturn<TFormValues>['resetField']
+      restart: () => void
+      change: UseFormReturn<TFormValues>['setValue']
+    },
+  ) => Promise<OnRawSubmitReturn> | OnRawSubmitReturn
+} & XOR<
+  [
+    { initialValues?: DefaultValues<TFormValues> },
+    { methods: UseFormReturn<TFormValues> },
+  ]
+>
+
+export const Form = <TFormValues extends FieldValues>({
   children,
-  onRawSubmit,
-  errors,
+  methods: methodsProp,
   initialValues,
-  validateOnBlur,
-  validate,
+  errors,
+  onRawSubmit,
   name,
-  render,
-  mutators,
-  keepDirtyOnReinitialize,
-  className,
-}: FormProps<FormValues>) => (
-  <ReactFinalForm
-    initialValues={initialValues}
-    validateOnBlur={validateOnBlur}
-    validate={validate}
-    mutators={{
-      ...arrayMutators,
-      ...mutators,
-    }}
-    onSubmit={onRawSubmit}
-    render={
-      render ??
-      (renderProps => (
-        <ErrorProvider errors={errors}>
-          <form
-            noValidate
-            name={name}
-            onSubmit={renderProps.handleSubmit}
-            className={className}
-          >
-            {typeof children === 'function' ? children(renderProps) : children}
+}: FormProps<TFormValues>) => {
+  const methodsHook = useForm({
+    defaultValues: initialValues,
+    mode: 'onChange',
+  })
+
+  const methods = !methodsProp ? methodsHook : methodsProp
+
+  const handleSubmit = methods.handleSubmit(async values => {
+    const result = await onRawSubmit(values, {
+      reset: methods.reset,
+      resetFieldState: methods.resetField,
+      restart: () => methods.reset(initialValues),
+      change: methods.setValue,
+    })
+    if (result && typeof result !== 'boolean' && typeof result !== 'number') {
+      methods.setError('root.submit', {
+        type: 'custom',
+        message: typeof result === 'object' ? result[FORM_ERROR] : result,
+      })
+    }
+  })
+
+  const formSubmitContextValue = useMemo(
+    () => ({ handleSubmit }),
+    [handleSubmit],
+  )
+
+  return (
+    <FormProvider {...methods}>
+      <FormSubmitContext.Provider value={formSubmitContextValue}>
+        <ErrorProvider errors={{ ...defaultErrors, ...errors }}>
+          <form onSubmit={handleSubmit} name={name}>
+            {typeof children === 'function'
+              ? children({
+                  values: methods.watch(),
+                  hasValidationErrors: !methods.formState.isValid,
+                  errors: methods.formState.errors,
+                  submitting: methods.formState.isSubmitting,
+                  pristine: !methods.formState.isDirty,
+                  handleSubmit,
+                  submitError: !!methods.formState.errors?.root?.['submit'],
+                  valid: methods.formState.isValid,
+                  form: {
+                    change: methods.setValue,
+                    reset: methods.reset,
+                    submit: handleSubmit,
+                  },
+                })
+              : children}
           </form>
         </ErrorProvider>
-      ))
-    }
-    keepDirtyOnReinitialize={keepDirtyOnReinitialize}
-  />
-)
+      </FormSubmitContext.Provider>
+    </FormProvider>
+  )
+}

packages/form/src/components/Submit/index.tsx
@@ -1,7 +1,6 @@
 import { Button } from '@ultraviolet/ui'
 import type { ComponentProps, ReactNode } from 'react'
-import { useEffect, useState } from 'react'
-import { useFormState } from 'react-final-form'
+import { useFormState } from 'react-hook-form'
 
 type SubmitProps = {
   children?: ReactNode
@@ -30,23 +29,9 @@ export const Submit = ({
   fullWidth,
   onClick,
 }: SubmitProps) => {
-  const { invalid, submitting, hasValidationErrors, dirtySinceLastSubmit } =
-    useFormState({
-      subscription: {
-        dirtySinceLastSubmit: true,
-        hasValidationErrors: true,
-        invalid: true,
-        submitting: true,
-      },
-    })
-  const [isLoading, setIsLoading] = useState(true)
-  const isDisabled =
-    disabled ||
-    submitting ||
-    isLoading ||
-    (invalid && hasValidationErrors && !dirtySinceLastSubmit)
+  const { isSubmitting, isValid } = useFormState()
 
-  useEffect(() => setIsLoading(false), [])
+  const isDisabled = disabled || isSubmitting || !isValid
 
   return (
     <Button
@@ -54,7 +39,7 @@ export const Submit = ({
       disabled={isDisabled}
       icon={icon}
       iconPosition={iconPosition}
-      isLoading={submitting}
+      isLoading={isSubmitting}
       size={size}
       type="submit"
       variant={variant}