41a88aaf02cbd2a37146b2e7320f8b2fa6f2a4db
Matthias <mprost@scaleway.com>
2024-01-09 12:05:45 +0100

    fix: svg usages as react component needs to be explicit

--- global.d.ts
+++ global.d.ts
@@ -1,7 +1,10 @@
 /// <reference types="@emotion/react/types/css-prop" />
 
 declare module '*.svg' {
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>
   const content: string
+
+  export { ReactComponent }
   export default content
 }
 
--- packages/icons/global.d.ts
+++ packages/icons/global.d.ts
@@ -1,9 +1,10 @@
 // We need to declare the svg module so that we can import it and style it with emotion
 declare module '*.svg' {
-  import type styled from '@emotion/styled'
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>
+  const content: string
 
-  const svg: Parameters<typeof styled>[0]
-  export default svg
+  export { ReactComponent }
+  export default content
 }
 
 declare module '*.md' {
