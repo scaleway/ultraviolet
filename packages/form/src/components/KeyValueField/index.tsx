382db15e45647967109e1b0474d4ee6f8c802f62
Nicolas Randrianarivo <nrandrianarivo@scaleway.com>
2024-01-12 16:56:07 +0100

    feat: add KeyValueField component

--- packages/form/src/components/KeyValueField/index.tsx
+++ packages/form/src/components/KeyValueField/index.tsx
@@ -1,10 +1,16 @@
 import { Button, Row, Stack } from '@ultraviolet/ui'
 import type { ComponentProps } from 'react'
 import { useFieldArray } from 'react-hook-form'
+<<<<<<< HEAD
 import type { Control, FieldArrayPath, FieldValues } from 'react-hook-form'
 import { TextInputField } from '../TextInputField'
 
 type InputKeyProps = {
+=======
+import { TextInputField } from '../TextInputField'
+
+type InputProps = {
+>>>>>>> 0a3ee302 (feat: add KeyValueField component)
   label: ComponentProps<typeof TextInputField>['label']
   required?: ComponentProps<typeof TextInputField>['required']
   regex?: ComponentProps<typeof TextInputField>['regex']
@@ -13,11 +19,16 @@ type InputKeyProps = {
 type InputValueProps = {
   type?: ComponentProps<typeof TextInputField>['type']
   placeholder?: ComponentProps<typeof TextInputField>['placeholder']
+<<<<<<< HEAD
 } & InputKeyProps
+=======
+} & InputProps
+>>>>>>> 0a3ee302 (feat: add KeyValueField component)
 
 type AddButtonProps = {
   name: ComponentProps<typeof Button>['children']
   fullWidth?: ComponentProps<typeof Button>['fullWidth']
+<<<<<<< HEAD
   tooltip: string
   maxSizeReachedTooltip: string
 }
@@ -30,10 +41,20 @@ type KeyValueFieldProps<
 > = {
   name: TFieldArrayName
   inputKey: InputKeyProps
+=======
+  tooltip?: string
+  tooltipAlert: string
+}
+
+type KeyValueFieldProps = {
+  name: string
+  inputKey: InputProps
+>>>>>>> 0a3ee302 (feat: add KeyValueField component)
   inputValue: InputValueProps
   addButton: AddButtonProps
   maxSize?: number
   readonly?: boolean
+<<<<<<< HEAD
   control?: Control<TFieldValues>
 }
 
@@ -45,17 +66,30 @@ export const KeyValueField = <
   TFieldArrayName extends
     FieldArrayPath<TFieldValues> = FieldArrayPath<TFieldValues>,
 >({
+=======
+}
+
+export type KeyValue = { key: string; value: string }
+
+export const KeyValueField = ({
+>>>>>>> 0a3ee302 (feat: add KeyValueField component)
   name,
   inputKey,
   inputValue,
   addButton,
   maxSize = 100,
   readonly = false,
+<<<<<<< HEAD
   control,
 }: KeyValueFieldProps<TFieldValues, TFieldArrayName>) => {
   const { fields, append, remove } = useFieldArray({
     name,
     control,
+=======
+}: KeyValueFieldProps) => {
+  const { fields, append, remove } = useFieldArray({
+    name,
+>>>>>>> 0a3ee302 (feat: add KeyValueField component)
   })
 
   const canAdd = fields.length !== undefined && fields.length < maxSize
@@ -65,7 +99,16 @@ export const KeyValueField = <
       {fields.length ? (
         <Stack gap={2}>
           {fields.map((field, index) => (
+<<<<<<< HEAD
             <Row key={field.id} templateColumns="1fr 1fr auto" gap={2}>
+=======
+            <Row
+              key={field.id}
+              templateColumns="1fr 1fr auto"
+              alignItems="flex-start"
+              gap={2}
+            >
+>>>>>>> 0a3ee302 (feat: add KeyValueField component)
               <TextInputField
                 readOnly={readonly}
                 required={inputKey.required}
@@ -86,7 +129,11 @@ export const KeyValueField = <
 
               <Button
                 disabled={readonly}
+<<<<<<< HEAD
                 data-testid={`remove-button-${index}`}
+=======
+                data-testid={`remove-button-${index}`} // @note: used for this component unit test
+>>>>>>> 0a3ee302 (feat: add KeyValueField component)
                 icon="delete"
                 variant="outlined"
                 sentiment="danger"
@@ -99,16 +146,24 @@ export const KeyValueField = <
       ) : null}
       <Stack direction="row" justifyContent="flex-start">
         <Button
+<<<<<<< HEAD
           data-testid="add-button"
+=======
+          data-testid="add-button" // @note: used for this component unit test
+>>>>>>> 0a3ee302 (feat: add KeyValueField component)
           icon="plus"
           variant="filled"
           sentiment="neutral"
           fullWidth={addButton.fullWidth}
           disabled={!canAdd || readonly}
+<<<<<<< HEAD
           tooltip={
             !canAdd ? addButton.maxSizeReachedTooltip : addButton.tooltip
           }
           // @ts-expect-error can't infer properly
+=======
+          tooltip={!canAdd ? addButton.tooltipAlert : addButton.tooltip}
+>>>>>>> 0a3ee302 (feat: add KeyValueField component)
           onClick={() => append({ key: '', value: '' })}
         >
           {addButton.name}
