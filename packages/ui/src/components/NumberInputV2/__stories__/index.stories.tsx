b26b2ef27698d9860b52bdabc0975986643b7600
Matthias Prost <mprost@scaleway.com>
2024-01-30 17:05:26 +0100

    docs: fix number input docs (#3384)

--- packages/ui/src/components/NumberInputV2/__stories__/index.stories.tsx
+++ packages/ui/src/components/NumberInputV2/__stories__/index.stories.tsx
@@ -20,12 +20,11 @@ export default {
   title: 'Components/Data Entry/NumberInputV2',
 } as Meta<typeof NumberInputV2>
 
-/* export { Playground } from './Playground.stories'
+export { Playground } from './Playground.stories'
 export { Sizes } from './Sizes.stories'
 export { Placeholder } from './Placeholder.stories'
-export { Helper } from './Helper.stories' */
+export { Helper } from './Helper.stories'
 export { MinMax } from './MinMax.stories'
-/*
 export { Step } from './Step.stories'
 export { Unit } from './Unit.stories'
 export { Disabled } from './Disabled.stories'
@@ -33,4 +32,3 @@ export { ReadOnly } from './ReadOnly.stories'
 export { Error } from './Error.stories'
 export { Success } from './Success.stories'
 export { LabelDescription } from './LabelDescription.stories'
-*/
