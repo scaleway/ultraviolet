5c9df3e3687395909b2183619a2e3314b2dcb13b
Fabien Hébert <fabien.hebert@gmail.com>
2024-02-14 15:49:19 +0100

    feat: update tag input component (#3425)
    * feat: update tag input component
    
    * fix: feedbacks
    
    * fix: label
    
    * fix: value property
    
    ---------
    
    Co-authored-by: Fabien Hébert <fhebert@scaleway.com>

--- packages/form/src/components/TagInputField/__tests__/index.spec.tsx
+++ packages/form/src/components/TagInputField/__tests__/index.spec.tsx
@@ -1,54 +1,39 @@
-import { describe, expect, test } from '@jest/globals'
-import { screen, waitFor } from '@testing-library/react'
+import { describe, expect, jest, test } from '@jest/globals'
+import { renderHook, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { TagInputField } from '../..'
-import { shouldMatchEmotionSnapshotFormWrapper } from '../../../../.jest/helpers'
+import { useForm } from 'react-hook-form'
+import { Form, Submit, TagInputField } from '../..'
+import {
+  renderWithTheme,
+  shouldMatchEmotionSnapshotFormWrapper,
+} from '../../../../.jest/helpers'
+import { mockErrors } from '../../../mocks'
 
-describe('ToggleField', () => {
+describe('TagInputField', () => {
   test('should render correctly', () =>
     shouldMatchEmotionSnapshotFormWrapper(
       <TagInputField name="test" placeholder="placeholder" />,
     ))
 
-  test('should render correctly disabled', () =>
-    shouldMatchEmotionSnapshotFormWrapper(
-      <TagInputField name="test-disabled" placeholder="placeholder" disabled />,
-    ))
+  test('should works with initialValues', async () => {
+    const onSubmit = jest.fn<(values: { test: string[] }) => void>()
+    const { result } = renderHook(() =>
+      useForm<{ test: string[] }>({ defaultValues: { test: ['First'] } }),
+    )
 
-  test('should render correctly with default tags', () =>
-    shouldMatchEmotionSnapshotFormWrapper(
-      <TagInputField
-        name="test-tags"
-        placeholder="placeholder"
-        tags={['tags-1', 'tags-2']}
-      />,
-    ))
-  test('should render correctly with default tags on initialValues Form', () =>
-    shouldMatchEmotionSnapshotFormWrapper(
-      <TagInputField name="formTags" placeholder="placeholder" />,
-      {},
-      {
-        initialValues: {
-          formTags: ['tags-1', 'tags-2'],
-        },
-      },
-    ))
+    renderWithTheme(
+      <Form onRawSubmit={onSubmit} errors={mockErrors} methods={result.current}>
+        <TagInputField label="Test" name="test" required clearable />
+        <Submit>Submit</Submit>
+      </Form>,
+    )
 
-  test('should render correctly with default tags on initialValues Form', () =>
-    shouldMatchEmotionSnapshotFormWrapper(
-      <TagInputField name="formTags" placeholder="placeholder" />,
-      {
-        transform: async () => {
-          const input = screen.getByDisplayValue<HTMLInputElement>('')
-          await userEvent.type(input, 'test{enter}')
-          await waitFor(() => expect(input.value).toBe(''))
-          expect(screen.getByText('test')).toBeInTheDocument()
-        },
-      },
-      {
-        initialValues: {
-          formTags: ['tags-1', 'tags-2'],
-        },
-      },
-    ))
+    await userEvent.click(screen.getByText('Submit'))
+    await waitFor(() => {
+      expect(onSubmit).toHaveBeenCalledTimes(1)
+    })
+    expect(onSubmit.mock.calls[0][0]).toEqual({
+      test: ['First'],
+    })
+  })
 })
--- packages/ui/src/components/TagInput/__stories__/index.stories.tsx
+++ packages/ui/src/components/TagInput/__stories__/index.stories.tsx
@@ -3,10 +3,16 @@ import { TagInput } from '..'
 
 export default {
   component: TagInput,
-  parameters: {
-    experimental: true,
-  },
   title: 'Components/Data Entry/TagInput',
 } as Meta
 
 export { Playground } from './Playground.stories'
+export { Size } from './Size.stories'
+export { Placeholder } from './Placeholder.stories'
+export { Helper } from './Helper.stories'
+export { Disabled } from './Disabled.stories'
+export { ReadOnly } from './ReadOnly.stories'
+export { Success } from './Success.stories'
+export { Error } from './Error.stories'
+export { LabelDescription } from './LabelDescription.stories'
+export { Tooltip } from './Tooltip.stories'
