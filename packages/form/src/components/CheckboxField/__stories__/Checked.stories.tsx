e2491feeffb8fb0de5ae49f26d8066513c4e92b9
Clément Lafont <clafont@scaleway.com>
2024-02-14 17:21:30 +0100

    fix: lint

--- packages/form/src/components/CheckboxField/__stories__/Checked.stories.tsx
+++ packages/form/src/components/CheckboxField/__stories__/Checked.stories.tsx
@@ -4,12 +4,8 @@ import type { FormErrors } from '../../../types'
 import { Form } from '../../Form'
 
 export const Checked: StoryFn<{ errors: FormErrors }> = ({ errors }) => (
-  <Form onRawSubmit={() => {}} errors={errors} initialValues={{ foo: ['bar'] }}>
-    <CheckboxField name="foo" value="bar">
-      Checked Item
-    </CheckboxField>
-    <CheckboxField name="foo" value="nope">
-      Not Checked Item
-    </CheckboxField>
+  <Form onRawSubmit={() => {}} errors={errors} initialValues={{ foo: true }}>
+    <CheckboxField name="foo">Checked Item</CheckboxField>
+    <CheckboxField name="bar">Not Checked Item</CheckboxField>
   </Form>
 )
