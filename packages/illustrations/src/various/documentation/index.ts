5e9cfb4b05fe488d67e3027b602856fb595e7b86
lisalupi <106706307+lisalupi@users.noreply.github.com>
2024-03-08 10:21:45 +0100

    feat: new various illustrations (#3535)

---
+++ packages/illustrations/src/various/documentation/index.ts
@@ -0,0 +1,6 @@
+import accountBilling from './account-billing.svg'
+import generic from './generic.svg'
+import learning from './learning.svg'
+import writeCommunity from './write-community.svg'
+
+export { accountBilling, generic, learning, writeCommunity }
