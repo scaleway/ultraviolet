e6e9ef4149434ca9a9c36053a83d8bc53729a5f3
Matthias <mprost@scaleway.com>
2024-03-15 12:06:29 +0100

    fix: add resizable nav border

--- packages/plus/src/components/Navigation/Navigation.tsx
+++ packages/plus/src/components/Navigation/Navigation.tsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled'
 import { Button, Stack } from '@ultraviolet/ui'
 import type { ReactNode } from 'react'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Group } from './Group'
 import { Item } from './Item'
 import { NavigationProvider, useNavigation } from './NavigationProvider'
@@ -12,18 +12,30 @@ const ANIMATION_DURATION = 300
 
 const StyledNav = styled.nav`
   transition: width ${ANIMATION_DURATION}ms ease-in-out;
+  &[data-disabled-width-animation='true'] {
+    transition: none;
+  }
+
   width: 280px;
+
+  &[data-expanded='true'] {
+    max-width: 320px;
+    min-width: 220px;
+  }
 
   &[data-expanded='false'] {
     width: 64px;
   }
 
+  display: flex;
+  flex-direction: row;
+  position: relative;
+`
+
+const Container = styled.div`
   background: ${({ theme }) => theme.colors.neutral.background};
   display: flex;
   flex-direction: column;
-  position: fixed;
-  top: 0;
-  bottom: 0;
 `
 
 const StickyFooter = styled.div`
@@ -44,7 +56,7 @@ const LogoContainer = styled(Stack)`
     `${theme.space['3']} ${theme.space['2']} ${theme.space['2']} ${theme.space['2']}`};
 `
 
-const Container = styled.div`
+const ContentContainer = styled.div`
   overflow: hidden;
   display: flex;
   flex-direction: column;
@@ -62,14 +74,97 @@ const Content = styled(Stack)`
   padding: ${({ theme }) => theme.space['2']};
 `
 
+const Slider = styled.div`
+  background: transparent;
+  cursor: col-resize;
+  border: 2px solid transparent;
+  margin-right: -2px;
+  display: flex;
+
+  &:hover {
+    border: 2px solid ${({ theme }) => theme.colors.primary.border};
+  }
+`
+
 type NavigationContentProps = {
   children: ReactNode
   logo?: ReactNode | ((expanded: boolean) => ReactNode)
+  className?: string
 }
 
-const NavigationContent = ({ children, logo }: NavigationContentProps) => {
+const NavigationContent = ({
+  children,
+  logo,
+  className,
+}: NavigationContentProps) => {
+  const sliderRef = useRef<HTMLDivElement>(null)
+  const navigationRef = useRef<HTMLDivElement>(null)
+
+  const [disabledWidthAnimation, setDisabledWidthAnimation] = useState(false)
+
   const { expanded, setExpanded } = useNavigation()
   const [isAnimationPlaying, setIsAnimationPlaying] = useState(false)
+
+  useEffect(() => {
+    let prevX: number
+    let navRect: DOMRect | undefined
+    let shouldCollapseOnMouseUp = false
+
+    const mouseMove = (event: MouseEvent) => {
+      if (prevX !== undefined) {
+        const navWidth = navRect?.width ?? 0
+        const newWidth = navWidth + (event.clientX - prevX)
+
+        if (navigationRef.current && expanded) {
+          navigationRef.current.style.width = `${newWidth}px`
+        }
+
+        console.log(newWidth)
+        if (newWidth <= 220) {
+          shouldCollapseOnMouseUp = true
+        } else {
+          shouldCollapseOnMouseUp = false
+        }
+      }
+    }
+
+    const mousedown = (event: MouseEvent) => {
+      setDisabledWidthAnimation(true)
+
+      document.body.style.pointerEvents = 'none'
+      document.body.style.userSelect = 'none'
+
+      prevX = event.clientX
+      navRect = navigationRef.current?.getBoundingClientRect()
+
+      const mouseup = () => {
+        setDisabledWidthAnimation(false)
+        if (shouldCollapseOnMouseUp) {
+          setExpanded()
+        }
+
+        if (navigationRef.current && !expanded) {
+          navigationRef.current.style.width = ''
+        }
+
+        document.removeEventListener('mousemove', mouseMove)
+        window.removeEventListener('mouseup', mouseup)
+
+        document.body.style.pointerEvents = ''
+        document.body.style.userSelect = ''
+      }
+
+      document.addEventListener('mousemove', mouseMove)
+      window.addEventListener('mouseup', mouseup)
+    }
+
+    sliderRef.current?.addEventListener('mousedown', mousedown)
+
+    return () => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      sliderRef.current?.removeEventListener('mousedown', mousedown)
+    }
+  }, [])
 
   // This function will be triggered when expand/collapse button is clicked
   // It will also trigger a fade out animation when expanding the navigation
@@ -86,28 +181,36 @@ const NavigationContent = ({ children, logo }: NavigationContentProps) => {
   }
 
   return (
-    <StyledNav data-expanded={expanded || isAnimationPlaying}>
-      <Header>
-        <LogoContainer
-          justifyContent={!expanded ? 'center' : undefined}
-          alignItems={!expanded ? 'center' : undefined}
-          data-animation={isAnimationPlaying}
-        >
-          {typeof logo === 'function' ? logo(expanded) : logo}
-        </LogoContainer>
-      </Header>
-      <Container data-animation={isAnimationPlaying}>
-        <Content gap={0.25}>{children}</Content>
-        <StickyFooter>
-          <Button
-            variant="ghost"
-            sentiment="neutral"
-            size="small"
-            icon={expanded ? 'arrow-left-double' : 'arrow-right-double'}
-            onClick={triggerExpand}
-          />
-        </StickyFooter>
+    <StyledNav
+      data-expanded={expanded || isAnimationPlaying}
+      className={className}
+      ref={navigationRef}
+      data-disabled-width-animation={disabledWidthAnimation}
+    >
+      <Container>
+        <Header>
+          <LogoContainer
+            justifyContent={!expanded ? 'center' : undefined}
+            alignItems={!expanded ? 'center' : undefined}
+            data-animation={isAnimationPlaying}
+          >
+            {typeof logo === 'function' ? logo(expanded) : logo}
+          </LogoContainer>
+        </Header>
+        <ContentContainer data-animation={isAnimationPlaying}>
+          <Content gap={0.25}>{children}</Content>
+          <StickyFooter>
+            <Button
+              variant="ghost"
+              sentiment="neutral"
+              size="small"
+              icon={expanded ? 'arrow-left-double' : 'arrow-right-double'}
+              onClick={triggerExpand}
+            />
+          </StickyFooter>
+        </ContentContainer>
       </Container>
+      <Slider ref={sliderRef} />
     </StyledNav>
   )
 }
--- packages/plus/src/components/Navigation/__stories__/Playground.stories.tsx
+++ packages/plus/src/components/Navigation/__stories__/Playground.stories.tsx
@@ -95,14 +95,111 @@ Playground.decorators = [
     <div
       style={{
         height: '600px',
-        width: '100%',
         display: 'flex',
-        overflow: 'hidden',
         background: 'lightgray',
         margin: '-10px 0', // This is to compensate the border added by storybook around the story
       }}
     >
       <Story />
+      <div
+        style={{
+          overflow: 'scroll',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          flex: '1',
+          gap: '16px',
+          padding: '16px',
+        }}
+      >
+        <img
+          src="https://via.placeholder.com/256"
+          width="256px"
+          height="256px"
+          alt="logo"
+        />
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempor
+          aliquam dui, a laoreet ante faucibus a. In accumsan pharetra dui, a
+          lobortis eros suscipit ut. In dapibus quam et massa commodo tincidunt.
+          Cras bibendum pharetra ultricies. Sed eget vulputate dui. Morbi nisi
+          ipsum, gravida at tristique eu, varius ut nibh. Curabitur non odio
+          neque. Curabitur in ante non risus malesuada tempus. Maecenas vel
+          ipsum justo. Sed sed nulla eu diam vestibulum molestie vitae nec
+          risus. Cras volutpat sollicitudin est in blandit. Proin non sem non
+          dolor vulputate faucibus quis pulvinar justo. In porta quis risus
+          luctus volutpat. Quisque sagittis aliquam justo, vitae ultrices eros.
+          Sed ac arcu ligula. Pellentesque condimentum mattis lacus ut dapibus.
+          Nam in sapien eros. Phasellus sagittis ipsum congue augue porta
+          placerat. Maecenas quis egestas mauris, non eleifend elit. In turpis
+          risus, ultrices ut sem a, auctor lobortis nisi. In molestie porttitor
+          commodo. Pellentesque eu convallis nunc. Cras vel ipsum odio. Mauris
+          faucibus ac nunc eget vehicula. Quisque at eros aliquam, pretium lacus
+          eu, volutpat ligula. Fusce fringilla dolor non pulvinar accumsan.
+          Curabitur ullamcorper purus neque, fringilla sollicitudin urna tempus
+          quis. Etiam sagittis, purus et venenatis sodales, sapien ipsum
+          vestibulum nisl, ut laoreet dolor diam nec erat. Donec ullamcorper
+          ante non dolor lobortis, quis lacinia ante ultrices. Pellentesque quis
+          condimentum arcu. Nulla ac fringilla eros. Nam at leo at lorem
+          sagittis auctor. Suspendisse fringilla pulvinar efficitur. Suspendisse
+          non eleifend enim. Pellentesque sit amet lectus vitae enim luctus
+          feugiat. Pellentesque porttitor augue at dui placerat congue sed at
+          quam. Praesent imperdiet augue urna. Proin a ligula vitae enim
+          venenatis ultricies ac vel sapien. Ut dolor massa, vulputate commodo
+          accumsan quis, dapibus et purus. Suspendisse at purus massa. Nunc et
+          ante at felis pharetra laoreet vel et lectus. Integer euismod urna
+          eget mollis commodo. Phasellus blandit mauris sed fermentum accumsan.
+          Curabitur eros felis, rutrum vitae dolor sed, efficitur laoreet est.
+          Pellentesque blandit eros sed pellentesque vestibulum. Suspendisse nec
+          magna diam. Suspendisse feugiat nisi ut augue condimentum, sed
+          ullamcorper mauris porta. Sed porttitor libero in condimentum
+          scelerisque. Vestibulum ut felis maximus, mollis ante vitae, consequat
+          ante. Vivamus ac lacinia arcu, eget fermentum lectus. Vivamus gravida
+          sodales metus a facilisis. Nam condimentum massa ut magna sagittis,
+          quis varius augue sagittis. Aenean vitae ipsum ac lectus consequat
+          convallis ut nec augue. Donec orci tortor, tincidunt rutrum cursus
+          non, blandit sed quam. Nunc pulvinar euismod ultrices. Curabitur non
+          arcu sed augue faucibus aliquet vitae vitae odio. Cras tincidunt eget
+          mi quis rhoncus. Curabitur eleifend pharetra tincidunt. Cras quis
+          tortor pellentesque, porttitor massa eget, mattis felis. Nulla aliquet
+          sodales tortor sit amet accumsan. Sed vitae interdum ipsum. Sed ut
+          volutpat nunc. Praesent aliquam ipsum ex, ac interdum odio vestibulum
+          dictum. Suspendisse neque quam, rhoncus aliquam diam eu, finibus
+          fringilla metus. Aliquam tristique enim arcu, non dapibus augue
+          volutpat eu. Integer ut sollicitudin nulla. Integer et est rutrum,
+          placerat orci in, fermentum neque. Sed facilisis nulla a orci finibus
+          tempus. Duis consectetur nisi nisl, ut eleifend libero facilisis a.
+          Nam quis neque lacus. Morbi a risus vestibulum, varius ex nec, varius
+          purus. In non quam risus. Mauris finibus justo erat, sed laoreet sem
+          luctus a. In cursus augue id eros ultricies, a pulvinar turpis
+          lobortis. Donec malesuada pellentesque venenatis. Nullam semper metus
+          at ligula venenatis egestas. Aenean metus nisl, sodales eget
+          condimentum sit amet, ullamcorper a nunc. Nullam pulvinar ornare
+          turpis a cursus. Ut egestas, nunc eu tempor sollicitudin, eros est
+          porttitor magna, vel lobortis quam est in felis. Nam sed sem est.
+          Morbi congue egestas felis, eu auctor ex. Integer non diam eu nibh
+          congue malesuada. Suspendisse ullamcorper nec odio in eleifend. Nullam
+          vehicula, risus quis sodales semper, diam risus ornare odio, ac
+          consectetur nisi orci at arcu. Aliquam vulputate nulla non urna
+          venenatis aliquet. Aenean tincidunt purus nisi, sit amet semper orci
+          lobortis non. Phasellus turpis dolor, rhoncus id pretium eget, auctor
+          sed lacus. Cras rutrum urna bibendum augue feugiat tempus. Quisque
+          iaculis sapien tellus, consequat semper felis consectetur ac.
+          Suspendisse tristique orci pretium, venenatis ante et, venenatis
+          risus. Phasellus scelerisque, nisi quis egestas ultrices, arcu libero
+          fringilla libero, ut molestie arcu turpis sed leo. Nullam finibus
+          libero tristique neque eleifend hendrerit. Praesent non molestie
+          libero, sit amet dapibus est. In congue ac nibh id sodales. Mauris
+          aliquet ante sit amet est varius congue quis vitae elit. In lectus ex,
+          malesuada quis eros eu, gravida ullamcorper lorem. Aliquam porttitor
+          odio condimentum nisi pellentesque ullamcorper quis vitae mauris.
+          Pellentesque et tortor sem. Duis suscipit orci massa. Duis a tellus
+          vitae nunc semper eleifend. Pellentesque venenatis maximus tellus in
+          bibendum. Ut risus justo, viverra id lacinia quis, egestas at augue.
+          Nulla dolor odio, malesuada nec posuere sed, molestie vitae quam.
+          Suspendisse at augue augue.
+        </div>
+      </div>
     </div>
   ),
 ]
