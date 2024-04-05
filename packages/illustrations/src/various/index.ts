35fd221f302e25dfdaf8574960533689eb064295
Alexandre Philibeaux <aphilibeaux@scaleway.com>
2024-04-05 12:16:35 +0000

    chore(illustations): migrate banner to illustations package

--- packages/illustrations/src/various/index.ts
+++ packages/illustrations/src/various/index.ts
@@ -1,6 +1,7 @@
+import * as banner from './banner'
 import * as documentation from './documentation'
 import * as empty from './empty'
 import * as feedback from './feedback'
 import * as onboarding from './onboarding'
 
-export { onboarding, feedback, empty, documentation }
+export { banner, documentation, empty, feedback, onboarding }
