733952a0c1a402092c27df5f7c484d3d60e91cc3
lisalupi <106706307+lisalupi@users.noreply.github.com>
2024-04-17 17:34:00 +0200

    fix: workaround for autofocus problem (#3672)
    * fix: workaround for autofocus problem
    
    * fix: delete changeset

---
+++ packages/ui/src/components/Modal/__stories__/AutoFocus.stories.tsx
@@ -0,0 +1,28 @@
+import type { StoryFn } from '@storybook/react'
+import { Modal } from '..'
+import { Button } from '../../Button'
+import { TextInputV2 } from '../../TextInputV2'
+
+export const DefaultDisclosure = <Button>Open Modal</Button>
+
+export const AutoFocus: StoryFn = props => (
+  <Modal disclosure={DefaultDisclosure} {...props}>
+    <div>
+      The second input will autofocus
+      <TextInputV2 placeholder="placeholder" />
+      <TextInputV2
+        placeholder="placeholder"
+        ref={ref => setTimeout(() => ref?.focus(), 1)}
+      />
+    </div>
+  </Modal>
+)
+
+AutoFocus.parameters = {
+  docs: {
+    description: {
+      story:
+        'Autofocus will not work natively with the modal. To recreate autoFocus, set a ref on the element to "autofocus" and focus on the ref **with a timeout**.',
+    },
+  },
+}
--- packages/ui/src/components/Modal/__stories__/FunctionChildren.stories.tsx
+++ packages/ui/src/components/Modal/__stories__/FunctionChildren.stories.tsx
@@ -4,10 +4,8 @@ import { Button } from '../../Button'
 
 export const FunctionChildren: StoryFn = props => (
   <Modal {...props} disclosure={<Button>modal</Button>}>
-    {({ onClose }) => (
-      <Button onClick={onClose}>
-        A custom button that can close the modal
-      </Button>
+    {({ close }) => (
+      <Button onClick={close}>A custom button that can close the modal</Button>
     )}
   </Modal>
 )
