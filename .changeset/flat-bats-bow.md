7c2536ebe5bcdee63efa9f520823e34fc5c3befb
Matthias <mprost@scaleway.com>
2024-05-16 18:21:06 +0200

    fix(Expandable): slight performance improvement on expandable

---
+++ .changeset/flat-bats-bow.md
@@ -0,0 +1,7 @@
+---
+"@ultraviolet/form": patch
+"@ultraviolet/plus": patch
+"@ultraviolet/ui": patch
+---
+
+Slight performance improvement on `<Expandable />` component to remove class generation when open and close
--- packages/ui/src/components/Expandable/__tests__/index.test.tsx
+++ packages/ui/src/components/Expandable/__tests__/index.test.tsx
@@ -1,5 +1,4 @@
-import { describe, expect, jest, test } from '@jest/globals'
-import { render, screen } from '@testing-library/react'
+import { describe, test } from '@jest/globals'
 import { Expandable } from '..'
 import { shouldMatchEmotionSnapshot } from '../../../../.jest/helpers'
 
@@ -26,60 +25,4 @@ describe('Expandable', () => {
     shouldMatchEmotionSnapshot(
       <Expandable animationDuration={500}>Sample Expandable</Expandable>,
     ))
-
-  test('should open the expandable', () => {
-    jest.useFakeTimers()
-    const { rerender } = render(
-      <Expandable className="test" data-testid="expandable" minHeight={100}>
-        Sample Expandable
-      </Expandable>,
-    )
-    jest
-      .spyOn(screen.getByTestId('expandable'), 'scrollHeight', 'get')
-      .mockImplementation(() => 100)
-    expect(screen.getByTestId('expandable')).toHaveStyle('max-height: 100px')
-    expect(screen.getByTestId('expandable')).toHaveStyle('overflow: hidden')
-
-    rerender(
-      <Expandable
-        className="test"
-        data-testid="expandable"
-        minHeight={100}
-        opened
-      >
-        Sample Expandable
-      </Expandable>,
-    )
-    jest.runAllTimers()
-    expect(screen.getByTestId('expandable')).toHaveStyle('overflow: visible')
-    expect(screen.getByTestId('expandable')).toHaveStyle('max-height: initial')
-  })
-
-  test('should close the expandable', () => {
-    jest.useFakeTimers()
-    const { rerender } = render(
-      <Expandable
-        className="test"
-        data-testid="expandable"
-        minHeight={100}
-        opened
-      >
-        Sample Expandable
-      </Expandable>,
-    )
-    jest
-      .spyOn(screen.getByTestId('expandable'), 'scrollHeight', 'get')
-      .mockImplementation(() => 100)
-    expect(screen.getByTestId('expandable')).toHaveStyle('max-height: initial')
-    expect(screen.getByTestId('expandable')).toHaveStyle('overflow: visible')
-
-    rerender(
-      <Expandable className="test" data-testid="expandable" minHeight={100}>
-        Sample Expandable
-      </Expandable>,
-    )
-    jest.runAllTimers()
-    expect(screen.getByTestId('expandable')).toHaveStyle('overflow: hidden')
-    expect(screen.getByTestId('expandable')).toHaveStyle('max-height: 100px')
-  })
 })
