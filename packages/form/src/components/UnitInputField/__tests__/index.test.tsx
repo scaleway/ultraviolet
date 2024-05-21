cdbc3b7ac3915a812594d8e33996062c1114a6dd
lisa <llupi@scaleway.com>
2024-05-21 17:30:09 +0200

    fix: update tests for vite

--- packages/form/src/components/UnitInputField/__tests__/index.test.tsx
+++ packages/form/src/components/UnitInputField/__tests__/index.test.tsx
@@ -1,21 +1,9 @@
-import {
-  afterAll,
-  beforeAll,
-  describe,
-  expect,
-  jest,
-  test,
-} from '@jest/globals'
 import { renderHook, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import { renderWithForm, renderWithTheme } from '@utils/test'
 import { useForm } from 'react-hook-form'
+import { describe, expect, test, vi } from 'vitest'
 import { Submit, UnitInputField } from '../..'
-import {
-  mockRandom,
-  renderWithTheme,
-  restoreRandom,
-  shouldMatchEmotionSnapshotFormWrapper,
-} from '../../../../.jest/helpers'
 import { mockErrors } from '../../../mocks'
 import { Form } from '../../Form'
 
@@ -35,19 +23,16 @@ const optionsSelect = [
 ]
 
 describe('UnitInputField', () => {
-  beforeAll(() => {
-    mockRandom()
+  test('should render correctly', () => {
+    const { asFragment } = renderWithForm(
+      <UnitInputField label="Test" name="test" options={optionsSelect} />,
+    )
+
+    expect(asFragment()).toMatchSnapshot()
   })
 
-  afterAll(restoreRandom)
-
-  test('should render correctly', () =>
-    shouldMatchEmotionSnapshotFormWrapper(
-      <UnitInputField label="Test" name="test" options={optionsSelect} />,
-    ))
-
   test('should handles onChange and selection', async () => {
-    const onSubmit = jest.fn<(values: { test: number | null }) => void>()
+    const onSubmit = vi.fn<[values: { test: number | null }], void>()
     const { result } = renderHook(() =>
       useForm<{ test: number | null }>({
         defaultValues: {
@@ -57,7 +42,7 @@ describe('UnitInputField', () => {
       }),
     )
 
-    renderWithTheme(
+    const { asFragment } = renderWithTheme(
       <Form
         onRawSubmit={value => onSubmit(value)}
         errors={mockErrors}
@@ -88,5 +73,7 @@ describe('UnitInputField', () => {
       test: 100,
       'test-unit': 'days',
     })
+
+    expect(asFragment()).toMatchSnapshot()
   })
 })
