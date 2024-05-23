52c0b6d3ef87feabd346c007ff72ccb0a2987a41
lisa <llupi@scaleway.com>
2024-05-23 16:51:22 +0200

    feat: new illustrations and icons

---
+++ packages/illustrations/src/products/dediboxVPS/index.ts
@@ -0,0 +1,27 @@
+import dediboxVpsContent from './dedibox-vps-content.webp'
+import dediboxVpsKeyvisual from './dedibox-vps-keyvisual.webp'
+import dediboxVpsOriginal from './dedibox-vps-original.webp'
+import dediboxVpsProContent from './dedibox-vps-pro-content.webp'
+import dediboxVpsProKeyvisual from './dedibox-vps-pro-keyvisual.webp'
+import dediboxVpsProOriginal from './dedibox-vps-pro-original.webp'
+import dediboxVpsProWire from './dedibox-vps-pro-wire.svg'
+import dediboxVpsStartContent from './dedibox-vps-start-content.webp'
+import dediboxVpsStartKeyvisual from './dedibox-vps-start-keyvisual.webp'
+import dediboxVpsStartOriginal from './dedibox-vps-start-original.webp'
+import dediboxVpsStartWire from './dedibox-vps-start-wire.svg'
+import dediboxVpsWire from './dedibox-vps-wire.svg'
+
+export {
+  dediboxVpsContent,
+  dediboxVpsKeyvisual,
+  dediboxVpsOriginal,
+  dediboxVpsWire,
+  dediboxVpsProContent,
+  dediboxVpsProKeyvisual,
+  dediboxVpsProOriginal,
+  dediboxVpsProWire,
+  dediboxVpsStartContent,
+  dediboxVpsStartKeyvisual,
+  dediboxVpsStartOriginal,
+  dediboxVpsStartWire,
+}
