90173d406f378b2b8f1f1b4e98701f76a5b3250d
Radhi NASSER <rnasser@scaleway.com>
2024-07-17 14:27:27 +0200

    test: imporve existing and add new ones

--- packages/ui/src/components/TagList/__tests__/index.test.tsx
+++ packages/ui/src/components/TagList/__tests__/index.test.tsx
@@ -1,117 +1,189 @@
 import { screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { renderWithTheme, shouldMatchEmotionSnapshot } from '@utils/test'
-import { describe, expect, test } from 'vitest'
+import { renderWithTheme } from '@utils/test'
+import { afterAll, describe, expect, test, vi } from 'vitest'
+import type { TagType } from '..'
 import { TagList } from '..'
 
+// - This function mocks the offsetWidth of DOM elements:
+// as JSDOM ( used by testing-library ) only emulates the DOM elements
+// so it does not render the elements in any way to be able to know their size or position.
+// - These mocks are done in a way to follow the code execution,
+// so if you change the code you might need to change this as well
+const mockOffsetWidth = (tags: TagType[], customContainerWidth?: number) => {
+  const TAGS_PARENT_WIDTH = 500
+  const TAG_WIDTH = 100
+  const POPOVER_TRIGGER_WIDTH = 50
+
+  const containerWidth = customContainerWidth || TAGS_PARENT_WIDTH
+
+  const mockOffsetWidthFunction = vi.spyOn(
+    HTMLElement.prototype,
+    'offsetWidth',
+    'get',
+  )
+
+  mockOffsetWidthFunction.mockReturnValueOnce(containerWidth)
+  tags.forEach(() => {
+    mockOffsetWidthFunction.mockReturnValueOnce(TAG_WIDTH)
+  })
+  mockOffsetWidthFunction.mockReturnValueOnce(POPOVER_TRIGGER_WIDTH)
+  mockOffsetWidthFunction.mockReturnValueOnce(TAG_WIDTH * tags.length)
+  mockOffsetWidthFunction.mockReturnValueOnce(containerWidth)
+}
+
 describe('TagList', () => {
-  test('renders correctly', () =>
-    shouldMatchEmotionSnapshot(
-      <div>
-        <TagList popoverTitle="Additional" tags={['scaleway', 'cloud']} />
-      </div>,
-    ))
+  afterAll(() => {
+    vi.resetAllMocks()
+  })
 
-  test('renders correctly with not tags', () =>
-    shouldMatchEmotionSnapshot(
-      <div>
-        <TagList popoverTitle="Additional" />
-      </div>,
-    ))
+  test('renders correctly', () => {
+    const tags = ['scaleway', 'cloud']
 
-  test('renders correctly with placement', () =>
-    shouldMatchEmotionSnapshot(
-      <div>
-        <TagList
-          popoverTitle="Additional"
-          tags={['scaleway', 'cloud']}
-          popoverPlacement="top"
-        />
-      </div>,
-    ))
+    mockOffsetWidth(tags)
 
-  test('renders correctly with custom threshold', () =>
-    shouldMatchEmotionSnapshot(
-      <div>
-        <TagList
-          popoverTitle="Additional"
-          threshold={2}
-          tags={['scaleway', 'cloud']}
-        />
-      </div>,
-    ))
+    const { asFragment } = renderWithTheme(
+      <TagList popoverTitle="Additional" tags={tags} />,
+    )
 
-  test('renders correctly with custom threshold and extra tags', () =>
-    shouldMatchEmotionSnapshot(
-      <div>
-        <TagList
-          popoverTitle="Additional"
-          threshold={2}
-          tags={['scaleway', 'cloud', 'provider']}
-        />
-      </div>,
-    ))
+    expect(asFragment()).toMatchSnapshot()
+  })
 
-  test('renders correctly with custom threshold and extra tags and maxLength inferior to the combined size of tags', () =>
-    shouldMatchEmotionSnapshot(
-      <div>
-        <TagList
-          popoverTitle="Additional"
-          maxLength={10}
-          threshold={2}
-          tags={['scaleway', 'cloud', 'provider']}
-        />
-      </div>,
-    ))
+  test('renders correctly with no tags', () => {
+    mockOffsetWidth([])
 
-  test('renders correctly with multiline', () =>
-    shouldMatchEmotionSnapshot(
-      <div>
-        <TagList
-          popoverTitle="Additional"
-          multiline
-          threshold={2}
-          tags={['scaleway', 'cloud', 'provider']}
-        />
-      </div>,
-    ))
+    const { asFragment } = renderWithTheme(
+      <TagList popoverTitle="Additional" />,
+    )
 
-  test('renders correctly with copiable', () =>
-    shouldMatchEmotionSnapshot(
-      <div>
-        <TagList
-          popoverTitle="Additional"
-          copiable
-          threshold={2}
-          tags={['scaleway', 'cloud']}
-        />
-      </div>,
-    ))
+    expect(asFragment()).toMatchSnapshot()
+  })
 
-  test('renders correctly with icons', () =>
-    shouldMatchEmotionSnapshot(
-      <div>
-        <TagList
-          popoverTitle="Additional"
-          copiable
-          threshold={4}
-          tags={['scaleway', 'cloud']}
-        />
-      </div>,
-    ))
+  test('renders correctly with placement', () => {
+    const tags = ['scaleway', 'cloud']
 
-  test('renders correctly when clicking on popover', async () => {
-    renderWithTheme(
+    mockOffsetWidth(tags)
+
+    const { asFragment } = renderWithTheme(
+      <TagList popoverTitle="Additional" tags={tags} popoverPlacement="top" />,
+    )
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('renders correctly with custom threshold', () => {
+    const tags = ['scaleway', 'cloud']
+
+    mockOffsetWidth(tags)
+
+    const { asFragment } = renderWithTheme(
+      <TagList popoverTitle="Additional" threshold={2} tags={tags} />,
+    )
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('renders correctly with custom threshold and extra tags', () => {
+    const tags = ['scaleway', 'cloud', 'provider']
+
+    mockOffsetWidth(tags)
+
+    const { asFragment } = renderWithTheme(
+      <TagList popoverTitle="Additional" threshold={2} tags={tags} />,
+    )
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('renders correctly and ignore custom threshold as it does not fit the parent', () => {
+    const tags = ['scaleway', 'cloud', 'provider']
+
+    mockOffsetWidth(tags, 150)
+
+    const { asFragment } = renderWithTheme(
+      <TagList popoverTitle="Additional" threshold={2} tags={tags} />,
+    )
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('renders correctly and add ellipsis to the first tag as it too long', () => {
+    const tags = ['scaleway is the best cloud provider ever invented']
+
+    mockOffsetWidth(tags, 50)
+
+    const { asFragment } = renderWithTheme(
+      <TagList popoverTitle="Additional" tags={tags} />,
+    )
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('renders correctly with custom threshold and extra tags and maxLength inferior to the combined size of tags', () => {
+    const tags = ['scaleway', 'cloud', 'provider']
+
+    mockOffsetWidth(tags)
+
+    const { asFragment } = renderWithTheme(
       <TagList
         popoverTitle="Additional"
-        threshold={1}
-        tags={[
-          { label: 'smooth', icon: 'id' },
-          'code',
-          { label: 'hello', icon: 'lock' },
-          { label: 'world', icon: 'plus' },
-        ]}
+        maxLength={10}
+        threshold={2}
+        tags={tags}
       />,
+    )
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('renders correctly with multiline', () => {
+    const tags = ['scaleway', 'cloud', 'provider']
+
+    mockOffsetWidth(tags)
+
+    const { asFragment } = renderWithTheme(
+      <TagList popoverTitle="Additional" multiline threshold={2} tags={tags} />,
+    )
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('renders correctly with copiable', () => {
+    const tags = ['scaleway', 'cloud']
+
+    mockOffsetWidth(tags)
+
+    const { asFragment } = renderWithTheme(
+      <TagList popoverTitle="Additional" copiable threshold={2} tags={tags} />,
+    )
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('renders correctly with icons', () => {
+    const tags = ['scaleway', 'cloud']
+
+    mockOffsetWidth(tags)
+
+    const { asFragment } = renderWithTheme(
+      <TagList popoverTitle="Additional" copiable threshold={4} tags={tags} />,
+    )
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('renders correctly when clicking on popover', async () => {
+    const tags: TagType[] = [
+      { label: 'smooth', icon: 'id' },
+      'code',
+      { label: 'hello', icon: 'lock' },
+      { label: 'world', icon: 'plus' },
+    ]
+
+    mockOffsetWidth(tags)
+
+    renderWithTheme(
+      <TagList popoverTitle="Additional" threshold={1} tags={tags} />,
     )
 
     expect(screen.queryByText('Additional')).not.toBeInTheDocument()
