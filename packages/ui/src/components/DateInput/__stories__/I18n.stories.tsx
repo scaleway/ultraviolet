a17e3c281502d2030f7c0b0f7066bbd2942de530
Alexandre Philibeaux <aphilibeaux@scaleway.com>
2024-08-22 16:06:24 +0200

    fix(i18n): remove broken usage of i18n
    Signed-off-by: Alexandre Philibeaux <aphilibeaux@scaleway.com>

--- packages/ui/src/components/DateInput/__stories__/I18n.stories.tsx
+++ packages/ui/src/components/DateInput/__stories__/I18n.stories.tsx
@@ -1,43 +1,66 @@
-import { useI18n } from '@scaleway/use-i18n'
 import type { StoryFn } from '@storybook/react'
+import type { Locale } from 'date-fns'
+import { enGB } from 'date-fns/locale/en-GB'
 import type { ComponentProps } from 'react'
+import { useState } from 'react'
 import { DateInput } from '..'
-import type { SelectOption } from '../../SelectInput'
-import { SelectInput } from '../../SelectInput'
+import { SelectInputV2 } from '../../SelectInputV2'
+
+const locales = ['en', 'fr', 'es', 'de', 'ru'] as const
+
+type LocalSupportedType = (typeof locales)[number]
+
+const isLocales = (locale: string): locale is LocalSupportedType =>
+  locales.includes(locale as LocalSupportedType)
+
+const loadDateFNS = async (locale: LocalSupportedType) =>
+  ({
+    en: (await import('date-fns/locale/en-GB')).enGB,
+    fr: (await import('date-fns/locale/fr')).fr,
+    es: (await import('date-fns/locale/es')).es,
+    de: (await import('date-fns/locale/de')).de,
+    ru: (await import('date-fns/locale/ru')).ru,
+  })[locale] ?? (await import('date-fns/locale/en-GB')).enGB
+
+const localesOptions = locales.map(locale => ({
+  label: locale,
+  value: locale,
+}))
 
 export const I18n: StoryFn<ComponentProps<typeof DateInput>> = args => {
-  const { locales, currentLocale, switchLocale, dateFnsLocale } = useI18n()
+  const [currentLocale, setCurrentLocale] = useState<LocalSupportedType>(
+    locales[0],
+  )
+  const [value, setValue] =
+    useState<ComponentProps<typeof DateInput>['value']>(undefined)
 
-  if (!dateFnsLocale) {
-    return <>Loading</>
+  const [dateFns, setDateFns] = useState<Locale>(enGB)
+
+  const onChange = async (locale: string) => {
+    if (isLocales(locale)) {
+      setCurrentLocale(locale)
+      setDateFns(await loadDateFNS(locale))
+    }
   }
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: 24 }}>
-      <SelectInput
+      <SelectInputV2
         name="locale-switcher"
-        isSearchable={false}
-        value={{
-          label: currentLocale,
-          value: currentLocale,
-        }}
-        onChange={value => {
-          switchLocale((value as SelectOption).value)
-            .then()
-            .catch(() => null)
-        }}
-        noTopLabel
-      >
-        {locales.map(locale => (
-          <SelectInput.Option value={locale} key={locale}>
-            {locale}
-          </SelectInput.Option>
-        ))}
-      </SelectInput>
+        label="Switch Locale"
+        searchable={false}
+        options={localesOptions}
+        value={currentLocale}
+        onChange={onChange}
+      />
+
       <DateInput
         {...args}
-        locale={dateFnsLocale}
-        onChange={() => {}}
+        locale={dateFns}
+        value={value}
+        onChange={v => {
+          setValue(v as Date)
+        }}
         label={currentLocale}
       />
     </div>
