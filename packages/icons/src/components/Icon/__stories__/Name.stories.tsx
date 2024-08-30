e7cf1a147ceadd39fd44dee8297b3de6c97b18f2
Matthias <mprost@scaleway.com>
2024-08-30 17:00:24 +0200

    fix: typecheck

--- packages/icons/src/components/Icon/__stories__/Name.stories.tsx
+++ packages/icons/src/components/Icon/__stories__/Name.stories.tsx
@@ -1,7 +1,8 @@
 import type { Decorator } from '@storybook/react'
 import { Stack } from '@ultraviolet/ui'
 import type { ComponentProps } from 'react'
-import { Icon, icons } from '..'
+import { Icon } from '..'
+import { icons } from '../legacy'
 
 export const Name = (args: ComponentProps<typeof Icon>) =>
   icons.map(name => (
