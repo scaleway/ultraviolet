ee84bc9586b76acb54f7518580324121c6d7f88c
Fabien Hébert <fhebert@scaleway.com>
2024-09-02 15:21:45 +0200

    feat(Illustrations): partner space

--- packages/illustrations/src/assets/various/index.ts
+++ packages/illustrations/src/assets/various/index.ts
@@ -2,5 +2,6 @@ import * as documentation from './documentation'
 import * as empty from './empty'
 import * as feedback from './feedback'
 import * as onboarding from './onboarding'
+import * as people from './people'
 
-export { onboarding, feedback, empty, documentation }
+export { onboarding, feedback, empty, documentation, people }
