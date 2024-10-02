73e45813d7fc63d6f2798874539908bf60df0fb5
Matthias Prost <mprost@scaleway.com>
2024-10-02 11:22:52 +0200

    docs(ProductIcon): display all sizes on storybook (#4293)

--- packages/icons/src/components/ProductIcon/__stories__/Sizes.stories.tsx
+++ packages/icons/src/components/ProductIcon/__stories__/Sizes.stories.tsx
@@ -1,12 +1,13 @@
 import type { StoryFn } from '@storybook/react'
 import { Stack, Text } from '@ultraviolet/ui'
 import { ConsoleProductIcon } from '..'
+import { SIZES } from '../Icon'
 
 export const Sizes: StoryFn<typeof ConsoleProductIcon> = props => (
   <Stack gap={1}>
-    {(['small', 'medium', 'large', 'xlarge'] as const).map(size => (
+    {Object.keys(SIZES).map(size => (
       <Stack direction="row" gap={1} alignItems="center">
-        <ConsoleProductIcon {...props} size={size} />
+        <ConsoleProductIcon {...props} size={size as keyof typeof SIZES} />
         <Text as="span" variant="bodyStrong">
           {size}
         </Text>
