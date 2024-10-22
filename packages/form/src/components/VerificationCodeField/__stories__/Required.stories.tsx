ff15811578854da7ccaab45d68f4ba339fbeb669
Matthias Prost <mprost@scaleway.com>
2024-10-22 10:39:04 +0200

    feat(form): add verification field (#4360)
    * feat(form): add verification field
    
    * fix: import and unit tests

---
+++ packages/form/src/components/VerificationCodeField/__stories__/Required.stories.tsx
@@ -0,0 +1,17 @@
+import type { StoryFn } from '@storybook/react'
+import { Stack } from '@ultraviolet/ui'
+import type { ComponentProps } from 'react'
+import { VerificationCodeField } from '..'
+import { Submit } from '../../Submit'
+import { Template } from './Template.stories'
+
+export const Required: StoryFn<
+  ComponentProps<typeof VerificationCodeField>
+> = args => (
+  <Stack gap={1} width="fit-content">
+    <VerificationCodeField {...args} />
+    <Submit>Submit</Submit>
+  </Stack>
+)
+
+Required.args = { ...Template.args, required: true }
---
+++ packages/form/src/components/VerificationCodeField/index.tsx
@@ -0,0 +1,118 @@
+import { Stack, Text, VerificationCode } from '@ultraviolet/ui'
+import type { ComponentProps } from 'react'
+import type { FieldPath, FieldValues } from 'react-hook-form'
+import { useController } from 'react-hook-form'
+import { useErrors } from '../../providers'
+import type { BaseFieldProps } from '../../types'
+
+type VerificationCodeFieldProps<
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues>,
+> = BaseFieldProps<TFieldValues, TName> &
+  Partial<
+    Pick<
+      ComponentProps<typeof VerificationCode>,
+      | 'disabled'
+      | 'error'
+      | 'fields'
+      | 'initialValue'
+      | 'onChange'
+      | 'onComplete'
+      | 'placeholder'
+      | 'required'
+      | 'type'
+    >
+  > & {
+    className?: string
+    id?: string
+    name: string
+    label?: string
+  }
+
+export const VerificationCodeField = <
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>({
+  className,
+  fields,
+  id = 'verification-code-input',
+  label,
+  name,
+  onChange,
+  onComplete,
+  placeholder,
+  required,
+  type = 'number',
+  disabled,
+  validate,
+}: VerificationCodeFieldProps<TFieldValues, TName>) => {
+  const { getError } = useErrors()
+
+  const {
+    field,
+    fieldState: { error },
+  } = useController({
+    name,
+    rules: {
+      required,
+      validate: {
+        required: localValue => {
+          if (required && localValue.length !== (fields ?? 4)) {
+            return false
+          }
+
+          return true
+        },
+        ...validate,
+      },
+    },
+  })
+
+  return (
+    <Stack
+      className={className}
+      gap={1}
+      justifyContent="center"
+      alignItems="center"
+      direction="column"
+      aria-label="verification-code-field"
+    >
+      {label ? (
+        <label
+          id={`${id}-label`}
+          htmlFor={`${id}-0`}
+          style={{
+            cursor: 'pointer',
+            flexShrink: 0,
+          }}
+        >
+          <Text as="p" variant="body" prominence="strong">
+            {label}
+          </Text>
+        </label>
+      ) : null}
+
+      <VerificationCode
+        inputId={id}
+        error={!!error}
+        placeholder={placeholder}
+        fields={fields}
+        onChange={event => {
+          onChange?.(event)
+          field.onChange(event)
+        }}
+        onComplete={event => {
+          onComplete?.(event)
+        }}
+        type={type}
+        disabled={disabled}
+        required={required}
+      />
+      {error ? (
+        <Text as="small" variant="caption" sentiment="danger">
+          {getError({ label: label || 'verification-code-field' }, error)}
+        </Text>
+      ) : null}
+    </Stack>
+  )
+}
