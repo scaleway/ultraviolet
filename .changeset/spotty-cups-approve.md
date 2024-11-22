d5f360122b76e328713c09379f960762cdf767b1
lisalupi <106706307+lisalupi@users.noreply.github.com>
2024-11-22 10:48:16 +0100

    feat: switchButtonField (#4473)
    * feat: switch-button-field
    
    * fix: spread props
    
    * fix: add onChange
    
    * fix: typecheck and tests
    
    * fix: tests

---
+++ .changeset/spotty-cups-approve.md
@@ -0,0 +1,5 @@
+---
+"@ultraviolet/form": minor
+---
+
+New component `<SwitchButtonField />`
---
+++ packages/form/src/components/SwitchButtonField/index.tsx
@@ -0,0 +1,62 @@
+import { SwitchButton } from '@ultraviolet/ui'
+import type { ComponentProps } from 'react'
+import type { FieldPath, FieldValues, PathValue } from 'react-hook-form'
+import { useController } from 'react-hook-form'
+import type { BaseFieldProps } from '../../types'
+
+type SwitchButtonFieldProps<
+  TFieldValues extends FieldValues,
+  TFieldName extends FieldPath<TFieldValues>,
+> = BaseFieldProps<TFieldValues, TFieldName> &
+  Omit<ComponentProps<typeof SwitchButton>, 'value' | 'name' | 'onChange'> &
+  Partial<Pick<ComponentProps<typeof SwitchButton>, 'onChange'>> & {
+    name: FieldPath<TFieldValues>
+  }
+
+export const SwitchButtonField = <
+  TFieldValues extends FieldValues,
+  TFieldName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>({
+  name,
+  leftButton,
+  rightButton,
+  size,
+  control,
+  shouldUnregister,
+  onBlur,
+  onChange,
+  onFocus,
+  tooltip,
+  className,
+  ...props
+}: SwitchButtonFieldProps<TFieldValues, TFieldName>) => {
+  const { field } = useController<TFieldValues>({
+    name,
+    shouldUnregister,
+    control,
+  })
+
+  return (
+    <SwitchButton
+      {...props}
+      name={name}
+      leftButton={leftButton}
+      rightButton={rightButton}
+      onChange={event => {
+        field.onChange(event)
+        onChange?.(
+          event.target as PathValue<TFieldValues, TFieldName> | undefined,
+        )
+      }}
+      value={field.value}
+      tooltip={tooltip}
+      size={size}
+      className={className}
+      onFocus={onFocus}
+      onBlur={event => {
+        field.onBlur()
+        onBlur?.(event)
+      }}
+    />
+  )
+}
