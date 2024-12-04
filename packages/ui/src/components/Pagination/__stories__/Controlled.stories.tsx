b0dfa6cc49c7d542c0c49399ce2a8ad5fad2c1ef
lisa <llupi@scaleway.com>
2024-12-04 15:58:07 +0100

    refactor: pagination

---
+++ packages/ui/src/components/Pagination/__stories__/Controlled.stories.tsx
@@ -0,0 +1,28 @@
+import type { StoryFn } from '@storybook/react'
+import { useState } from 'react'
+import { Pagination } from '..'
+import { Badge } from '../../Badge'
+
+export const Controlled: StoryFn = props => {
+  const [page, setPage] = useState(1)
+
+  return <Pagination {...props} onChange={setPage} page={page} pageCount={10} />
+}
+
+Controlled.args = {
+  value: 40,
+  labelDescription: (
+    <Badge sentiment="primary" size="small">
+      New
+    </Badge>
+  ),
+  label: 'Label',
+}
+
+Controlled.parameters = {
+  docs: {
+    description: {
+      story: 'Define number of elements to show per page',
+    },
+  },
+}
