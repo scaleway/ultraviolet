35833f58208a74c931aca482b4cecc404ebe7a0f
Alexandre Philibeaux <aphilibeaux@scaleway.com>
2024-12-24 12:27:21 +0100

    feat(playwright): add playwright e2e on checkbox list

---
+++ e2e/tests/list-checkbox.spec.ts
@@ -0,0 +1,20 @@
+import { expect, test } from '@playwright/test'
+
+const defaultLocator = 'iframe[title="storybook-preview-iframe"]'
+
+test('has title', async ({ page, baseURL }) => {
+  const url = `${baseURL}/?path=/story/components-data-display-list--selectable`
+  await page.goto(url)
+
+  const rootLocator = page.locator(defaultLocator).contentFrame()
+
+  await rootLocator
+    .getByRole('row', { name: 'select Venus 0.718AU 0.728AU' })
+    .getByLabel('select')
+    .check()
+
+  const checkbox = rootLocator.locator(
+    `input[type='checkbox'][name='list-select-checkbox'][value="venus"]`,
+  )
+  await expect(checkbox).toBeChecked()
+})
