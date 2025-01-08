794161c352b92c918c55e249d8b15a120a68adb1
Alexandre Philibeaux <aphilibeaux@scaleway.com>
2025-01-08 11:46:36 +0100

    docs(vite): add vite example
    Signed-off-by: Alexandre Philibeaux <aphilibeaux@scaleway.com>

---
+++ examples/next/src/pages/index.tsx
@@ -0,0 +1,17 @@
+import { Link } from '../components/Link'
+
+const Index = () => (
+  <>
+    <Link href="/simple" sentiment="primary">
+      Simple
+    </Link>
+    <Link href="/login" sentiment="primary">
+      login
+    </Link>
+    <Link href="/advanced" sentiment="primary">
+      advanced
+    </Link>
+  </>
+)
+
+export default Index
