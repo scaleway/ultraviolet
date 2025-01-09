8f2e11bf67599d0e8388e0a0194a0bdf6da55afa
lisa <llupi@scaleway.com>
2025-01-09 10:14:09 +0100

    fix: typecheck storybook

--- packages/ui/src/components/TimeInputV2/__stories__/Size.stories.tsx
+++ packages/ui/src/components/TimeInputV2/__stories__/Size.stories.tsx
@@ -2,10 +2,10 @@ import type { StoryFn } from '@storybook/react'
 import { TimeInputV2 } from '..'
 import { Stack } from '../../Stack'
 
-export const Size: StoryFn<typeof TimeInputV2> = args => (
+export const Size: StoryFn<typeof TimeInputV2> = () => (
   <Stack gap="2">
-    <TimeInputV2 {...args} label="small" size="small" />
-    <TimeInputV2 {...args} label="medium" size="medium" />
-    <TimeInputV2 {...args} label="large" size="large" />
+    <TimeInputV2 label="small" size="small" />
+    <TimeInputV2 label="medium" size="medium" />
+    <TimeInputV2 label="large" size="large" />
   </Stack>
 )
