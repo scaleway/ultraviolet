df65556c4d6fad8e4ba42cf6899510ed08b937db
lisa <llupi@scaleway.com>
2025-02-06 16:06:04 +0100

    fix: date input range

--- packages/ui/src/components/DateInput/__tests__/index.test.tsx
+++ packages/ui/src/components/DateInput/__tests__/index.test.tsx
@@ -365,10 +365,11 @@ describe('DateInput', () => {
     await userEvent.unhover(month)
 
     await userEvent.click(month)
-    expect(input.value).toBe('02/1995 - ')
+    expect(input.value).toBe('02/1995 - 08/1995')
 
+    await userEvent.click(input)
     await userEvent.click(screen.getByText('Sep'))
-    expect(input.value).toBe('02/1995 - 09/1995')
+    expect(input.value).toBe('09/1995 - ')
   })
 
   test('renders correctly custom format with range', () => {
