0c10119f0e9a4261eca24d59e0071ad93d83cae6
lisa <llupi@scaleway.com>
2025-02-26 11:49:08 +0100

    fix: feedbacks

--- packages/ui/src/components/UnitInput/index.tsx
+++ packages/ui/src/components/UnitInput/index.tsx
@@ -180,7 +180,7 @@ const UnitInputWrapper = styled(Stack)<{
 const SelectInputWrapper = styled.div<{
   width: number | string
 }>`
-${({ width }) => width && `width: ${typeof width === 'string' ? width : `${width}px`};`}
+${({ width }) => width && `width: ${typeof width === 'number' ? `${width}px` : width};`}
 display: flex;
 `
 
