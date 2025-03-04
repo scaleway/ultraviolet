e2b112c46745e025af3995afbb3256263cf3d199
Matthias Prost <mprost@scaleway.com>
2025-03-04 11:46:02 +0100

    fix(Dialog): add disabled prop on button cancel (#4828)

--- packages/ui/src/components/Dialog/components/CancelButton.tsx
+++ packages/ui/src/components/Dialog/components/CancelButton.tsx
@@ -4,13 +4,20 @@ import { Button } from '../../Button'
 type DialogCancelButtonProps = {
   children: ReactNode
   onClick: ComponentProps<typeof Button>['onClick']
+  disabled?: ComponentProps<typeof Button>['disabled']
 }
 
 export const DialogCancelButton = ({
   children,
   onClick,
+  disabled,
 }: DialogCancelButtonProps) => (
-  <Button variant="outlined" sentiment="neutral" onClick={onClick}>
+  <Button
+    variant="outlined"
+    sentiment="neutral"
+    onClick={onClick}
+    disabled={disabled}
+  >
     {children}
   </Button>
 )
