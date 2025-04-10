5ec8c0973220ce30f7c18082e9e3add07fdf2828
Matthias <mprost@scaleway.com>
2025-04-10 15:28:20 +0200

    fix(navigation): improve animation

--- packages/plus/src/components/Navigation/components/PinnedItems.tsx
+++ packages/plus/src/components/Navigation/components/PinnedItems.tsx
@@ -72,8 +72,15 @@ export const PinnedItems = ({
     )
   }
 
-  const { locales, pinnedItems, pinnedFeature, reorderItems, expanded, items } =
-    context
+  const {
+    locales,
+    pinnedItems,
+    pinnedFeature,
+    reorderItems,
+    expanded,
+    items,
+    animation,
+  } = context
   const theme = useTheme()
 
   const onDrop = useCallback(
@@ -119,7 +126,7 @@ export const PinnedItems = ({
 
   if (pinnedFeature) {
     return (
-      <div>
+      <div style={{ width: animation ? '100%' : undefined }}>
         <Item
           label={locales['navigation.pinned.item.group.label']}
           categoryIcon="pin"
