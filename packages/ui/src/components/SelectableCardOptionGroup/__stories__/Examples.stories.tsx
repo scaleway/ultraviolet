95f46317a1c7072dac8609469da4ca08cfdefc96
Matthias <mprost@scaleway.com>
2025-04-15 17:45:07 +0200

    fix(SelectableCardOptionGroup): image height to be same as width

--- packages/ui/src/components/SelectableCardOptionGroup/__stories__/Examples.stories.tsx
+++ packages/ui/src/components/SelectableCardOptionGroup/__stories__/Examples.stories.tsx
@@ -6,97 +6,134 @@ import { Stack } from '../../Stack'
 import { Text } from '../../Text'
 import centos from './assets/centos.svg'
 import debian from './assets/debian.svg'
+import mysql from './assets/mysql.svg'
+import postgresql from './assets/postgresql.svg'
 import ubuntu from './assets/ubuntu.svg'
-import { centosOptions, debianOptions, ubuntuOptionsLegacy } from './constants'
+import {
+  centosOptions,
+  debianOptions,
+  mysqlOptions,
+  postgresqlOptions,
+  ubuntuOptionsLegacy,
+} from './constants'
 
 export const Examples: StoryFn<typeof SelectableCardOptionGroup> = args => {
   const [value, onChange] = useState<string>()
   const [option, onChangeOption] = useState<string>()
 
+  const [value2, onChange2] = useState<string>()
+  const [option2, onChangeOption2] = useState<string>()
+
   return (
     <Stack direction="column" gap={8}>
+      <Stack direction="column" gap={8}>
+        <SelectableCardOptionGroup
+          {...args}
+          value={value}
+          optionValue={option}
+          onChangeOption={(newValue: string) => onChangeOption(newValue)}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            onChange(event.currentTarget.value)
+          }
+          size="large"
+          columns={4}
+        >
+          <SelectableCardOptionGroup.Option
+            value="ubuntu"
+            label="Ubuntu"
+            labelDescription={
+              <Badge sentiment="primary" size="small">
+                NEW
+              </Badge>
+            }
+            options={ubuntuOptionsLegacy}
+            image={ubuntu}
+          >
+            <Text
+              as="p"
+              variant="bodySmall"
+              sentiment="neutral"
+              prominence="weak"
+              placement="center"
+            >
+              New versions has been added recently
+            </Text>
+          </SelectableCardOptionGroup.Option>
+          <SelectableCardOptionGroup.Option
+            value="debian"
+            label="Debian"
+            options={debianOptions}
+            image={debian}
+          >
+            <Text
+              as="p"
+              variant="bodySmall"
+              sentiment="neutral"
+              prominence="weak"
+              placement="center"
+            >
+              Easy to configure and maintain
+            </Text>
+          </SelectableCardOptionGroup.Option>
+          <SelectableCardOptionGroup.Option
+            value="centos"
+            label="CentOS"
+            options={centosOptions}
+            image={centos}
+          >
+            <Text
+              as="p"
+              variant="bodySmall"
+              sentiment="neutral"
+              prominence="weak"
+              placement="center"
+            >
+              Used by many enterprises
+            </Text>
+          </SelectableCardOptionGroup.Option>
+        </SelectableCardOptionGroup>
+        <Stack>
+          <Text as="h2" variant="bodyStrong">
+            Debug:
+          </Text>
+          <Text as="p" variant="body">
+            Selected OS:{' '}
+            <Text as="span" variant="bodyStrong">
+              {value}
+            </Text>
+          </Text>
+          <Text as="p" variant="body">
+            Selected OS version:{' '}
+            <Text as="span" variant="bodyStrong">
+              {option}
+            </Text>
+          </Text>
+        </Stack>
+      </Stack>
       <SelectableCardOptionGroup
         {...args}
-        value={value}
-        optionValue={option}
-        onChangeOption={(newValue: string) => onChangeOption(newValue)}
+        value={value2}
+        optionValue={option2}
+        onChangeOption={(newValue: string) => onChangeOption2(newValue)}
         onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-          onChange(event.currentTarget.value)
+          onChange2(event.currentTarget.value)
         }
         size="large"
         columns={4}
       >
         <SelectableCardOptionGroup.Option
-          value="ubuntu"
-          label="Ubuntu"
-          labelDescription={
-            <Badge sentiment="primary" size="small">
-              NEW
-            </Badge>
-          }
-          options={ubuntuOptionsLegacy}
-          image={ubuntu}
-        >
-          <Text
-            as="p"
-            variant="bodySmall"
-            sentiment="neutral"
-            prominence="weak"
-            placement="center"
-          >
-            New versions has been added recently
-          </Text>
-        </SelectableCardOptionGroup.Option>
+          value="mysql"
+          label="MySQL"
+          options={mysqlOptions}
+          image={mysql}
+        />
         <SelectableCardOptionGroup.Option
-          value="debian"
-          label="Debian"
-          options={debianOptions}
-          image={debian}
-        >
-          <Text
-            as="p"
-            variant="bodySmall"
-            sentiment="neutral"
-            prominence="weak"
-            placement="center"
-          >
-            Easy to configure and maintain
-          </Text>
-        </SelectableCardOptionGroup.Option>
-        <SelectableCardOptionGroup.Option
-          value="centos"
-          label="CentOS"
-          options={centosOptions}
-          image={centos}
-        >
-          <Text
-            as="p"
-            variant="bodySmall"
-            sentiment="neutral"
-            prominence="weak"
-            placement="center"
-          >
-            Used by many enterprises
-          </Text>
-        </SelectableCardOptionGroup.Option>
+          value="postgresql"
+          label="PostgreSQL"
+          options={postgresqlOptions}
+          image={postgresql}
+        />
       </SelectableCardOptionGroup>
-      <Stack>
-        <Text as="h2" variant="bodyStrong">
-          Debug:
-        </Text>
-        <Text as="p" variant="body">
-          Selected OS:{' '}
-          <Text as="span" variant="bodyStrong">
-            {value}
-          </Text>
-        </Text>
-        <Text as="p" variant="body">
-          Selected OS version:{' '}
-          <Text as="span" variant="bodyStrong">
-            {option}
-          </Text>
-        </Text>
-      </Stack>
     </Stack>
   )
 }
@@ -108,7 +145,7 @@ Examples.args = {
 
 Examples.decorators = [
   Story => (
-    <div style={{ height: '400px' }}>
+    <div style={{ height: '800px' }}>
       <Story />
     </div>
   ),
--- packages/ui/src/components/SelectableCardOptionGroup/__stories__/constants.tsx
+++ packages/ui/src/components/SelectableCardOptionGroup/__stories__/constants.tsx
@@ -2,6 +2,30 @@ import type { ComponentProps } from 'react'
 import { Badge } from '../../Badge'
 import type { SelectInputV2 } from '../../SelectInputV2'
 
+export const mysqlOptions: ComponentProps<typeof SelectInputV2>['options'] = [
+  {
+    label: 'MySQL 8.0',
+    value: 'mysql-8.0',
+  },
+  {
+    label: 'MySQL 5.7',
+    value: 'mysql-5.7',
+  },
+]
+
+export const postgresqlOptions: ComponentProps<
+  typeof SelectInputV2
+>['options'] = [
+  {
+    label: 'PostgreSQL 13',
+    value: 'postgresql-13',
+  },
+  {
+    label: 'PostgreSQL 12',
+    value: 'postgresql-12',
+  },
+]
+
 export const ubuntuOptions: ComponentProps<typeof SelectInputV2>['options'] = [
   {
     label: 'Ubuntu 20.04 Focal Fossa',
