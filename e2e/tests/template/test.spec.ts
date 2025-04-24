d7fe9ead4a849a379b03950a7e0a7916bf109643
Matthias <mprost@scaleway.com>
2025-04-24 16:10:37 +0200

    ci: unit test update

--- e2e/tests/template/test.spec.ts
+++ e2e/tests/template/test.spec.ts
@@ -1,7 +1,8 @@
 import { test } from '@playwright/test'
 
+// replace [Change me] by the name of your test
 test('[Change me]', async ({ page, baseURL }) => {
-  await page.goto(`${baseURL}/[folderName])`)
+  await page.goto(`${baseURL}/template`) // replace "template" by the folder name. Foldername should be camelCase.
 
   await page.getByRole('button', { name: 'Click me' }).click()
 })
