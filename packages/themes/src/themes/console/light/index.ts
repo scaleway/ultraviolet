4e38f58ad2c98c9892962912c28dda2cdb450140
Matthias Prost <mprost@scaleway.com>
2025-05-20 10:49:07 +0200

    feat: removal of all deprecated tokens (#5149)

--- packages/themes/src/themes/console/light/index.ts
+++
@@ -1,6 +0,0 @@
-import deepmerge from 'deepmerge'
-import { deprecatedLightTokens } from '../deprecated/light'
-import { lightTheme } from './__generated__'
-
-export const consoleLightTheme: typeof lightTheme &
-  typeof deprecatedLightTokens = deepmerge(lightTheme, deprecatedLightTokens)
