8767a18d133b984156ba569994347333f9088b5b
Matthias Prost <mprost@scaleway.com>
2025-05-20 10:49:07 +0200

    feat: removal of all deprecated tokens (#5149)

--- packages/themes/src/themes/console/darker/index.ts
+++
@@ -1,6 +0,0 @@
-import deepmerge from 'deepmerge'
-import { deprecatedDarkerTokens } from '../deprecated/darker'
-import { darkerTheme } from './__generated__'
-
-export const consoleDarkerTheme: typeof darkerTheme &
-  typeof deprecatedDarkerTokens = deepmerge(darkerTheme, deprecatedDarkerTokens)
