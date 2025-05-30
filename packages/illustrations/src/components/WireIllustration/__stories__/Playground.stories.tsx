fbed6c10f3945af066636240de731d4216937772
lisalupi <106706307+lisalupi@users.noreply.github.com>
2025-05-30 10:33:06 +0200

    feat(illustration): wire illustrations (#5166)
    * feat: illustrations improvements
    
    * chore(illustrations): automatic export update
    
    * fix: storybook
    
    * fix: use prop name for wire
    
    * fix: remove auto-commit

---
+++ packages/illustrations/src/components/WireIllustration/__stories__/Playground.stories.tsx
@@ -0,0 +1,9 @@
+import { Template } from './Template.stories'
+
+export const Playground = Template.bind({})
+Playground.args = {
+  sentiment: 'success',
+  width: 200,
+  height: 200,
+  name: 'appleSilicon',
+}
