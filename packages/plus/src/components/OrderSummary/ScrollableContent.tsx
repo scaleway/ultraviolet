b2d35f45a7411b11c879c9aeb42f475931d138cb
lisalupi <106706307+lisalupi@users.noreply.github.com>
2025-06-25 17:29:37 +0200

    feat: order summary (#4794)
    * feat: order summary
    
    * feat: order summary
    
    * feat: order summary without tests
    
    * feat: order summary tests
    
    * fix: feedbacks
    
    * feat: number input and various improvements
    
    * fix: rebase
    
    * fix: add tests
    
    * fix: rebase
    
    * fix: discounts and more tests
    
    * fix: add range
    
    * feat: onChange WIP
    
    * feat: onchange and various changes
    
    * fix: stylinh and tests
    
    * feat: new prop to unit input needed for ordersummaryo
    
    * fix: designers feedback
    
    * fix: rebase
    
    * fix: update ordersummary and rebase
    
    * fix: tests after rebase
    
    * fix: rebase
    
    * fix: tests and detail hidden if empty string

---
+++ packages/plus/src/components/OrderSummary/ScrollableContent.tsx
@@ -0,0 +1,242 @@
+import styled from '@emotion/styled'
+import { NumberInput, Stack, Text } from '@ultraviolet/ui'
+import { useContext } from 'react'
+import { OrderSummaryContext } from './Provider'
+import {
+  DisplayPrice,
+  calculateSubCategoryPrice,
+  formatNumber,
+} from './helpers'
+import type { ItemsType, SubCategoryType } from './types'
+
+const StyledNumberInputV2 = styled(NumberInput)`
+max-width: 12.5rem;
+background-color: ${({ theme }) => theme.colors.neutral.background};
+`
+const ContainerScrollable = styled(Stack)`
+overflow-y: scroll;
+padding: ${({ theme }) => theme.space[3]};
+height: 100%;
+`
+
+const DetailsStack = styled(Stack)`
+padding-left: ${({ theme }) => theme.space[1]};
+`
+const CategoryStack = styled(Stack)`
+  :not(:last-child){
+    border-bottom: 1px solid ${({ theme }) => theme.colors.neutral.border};
+    padding-bottom: ${({ theme }) => theme.space[3]};
+  }
+`
+const CategoryName = ({ category }: { category: ItemsType }) => {
+  const { categoriesPrice } = useContext(OrderSummaryContext)
+
+  const categoryPrice = categoriesPrice[category.category] ?? {
+    totalPrice: 0,
+    totalPriceWithDiscount: 0,
+    maxPrice: 0,
+    maxPriceWithDiscount: 0,
+    timeUnit: 'hours',
+  }
+
+  return category.category ? (
+    <Stack justifyContent="space-between" direction="row" alignItems="center">
+      {category.additionalInfo ? (
+        <Stack direction="row" gap={1} alignItems="center">
+          <Text
+            as="span"
+            variant="bodyStrong"
+            sentiment="neutral"
+            prominence="strong"
+          >
+            {category.category}
+          </Text>
+          <Text as="span" variant="bodySmall" italic sentiment="primary">
+            {category.additionalInfo}
+          </Text>
+        </Stack>
+      ) : (
+        <Text
+          as="span"
+          variant="bodyStrong"
+          sentiment="neutral"
+          prominence="strong"
+        >
+          {category.category}
+        </Text>
+      )}
+      {category.customContent}
+      {category.numberInput ? (
+        <StyledNumberInputV2
+          value={category.numberInputValue}
+          onChange={category.onChangeInput}
+          size="small"
+          controls={category.numberInputControls}
+          unit={category.numberInputUnit}
+        />
+      ) : null}
+      {!category.customContent &&
+      !category.numberInput &&
+      categoryPrice.totalPrice === categoryPrice.totalPriceWithDiscount ? (
+        <Text
+          as="span"
+          variant="bodyStrong"
+          sentiment="neutral"
+          prominence="strong"
+        >
+          <DisplayPrice price={categoryPrice} beforeOrAfter="after" />
+        </Text>
+      ) : null}
+
+      {!category.customContent &&
+      !category.numberInput &&
+      categoryPrice.totalPrice !== categoryPrice.totalPriceWithDiscount ? (
+        <Stack direction="row" gap={1} alignItems="center">
+          <Text
+            as="span"
+            variant="bodySmallStrong"
+            sentiment="neutral"
+            prominence="weak"
+            strikeThrough
+          >
+            <DisplayPrice price={categoryPrice} beforeOrAfter="before" />
+          </Text>
+          <Text
+            as="span"
+            variant="bodyStrong"
+            sentiment="neutral"
+            prominence="strong"
+          >
+            <DisplayPrice price={categoryPrice} beforeOrAfter="after" />
+          </Text>
+        </Stack>
+      ) : null}
+    </Stack>
+  ) : null
+}
+
+const SubCategory = ({ subCategory }: { subCategory: SubCategoryType }) => {
+  const {
+    currency,
+    localeFormat,
+    hideTimeUnit,
+    timePeriodAmount,
+    timePeriodUnit,
+    fractionDigits,
+  } = useContext(OrderSummaryContext)
+  const subCategoryPrice = calculateSubCategoryPrice(
+    subCategory,
+    hideTimeUnit,
+    timePeriodAmount,
+    timePeriodUnit,
+  )
+
+  return (
+    <Stack direction="column" gap={1}>
+      <Stack justifyContent="space-between" direction="row" alignItems="center">
+        {subCategory.title ? (
+          <Text
+            as="span"
+            variant="bodySmallStrong"
+            sentiment="neutral"
+            prominence="strong"
+          >
+            {subCategory.title}
+          </Text>
+        ) : null}
+        {subCategory.customContent ? (
+          <Text
+            as="span"
+            variant="bodySmallStrong"
+            sentiment="neutral"
+            prominence="strong"
+          >
+            {subCategory.customContent}
+          </Text>
+        ) : null}
+        {subCategory.numberInput ? (
+          <StyledNumberInputV2
+            value={subCategory.numberInputValue}
+            onChange={subCategory.onChangeInput}
+            size="small"
+            controls={subCategory.numberInputControls}
+            unit={subCategory.numberInputUnit}
+          />
+        ) : null}
+        {subCategory.price !== undefined && !subCategory.hidePrice ? (
+          <Text
+            as="span"
+            variant="bodySmallStrong"
+            sentiment="neutral"
+            prominence="strong"
+          >
+            {subCategoryPrice[0] === subCategoryPrice[1] ||
+            subCategory.priceUnit
+              ? formatNumber(
+                  subCategory.priceUnit
+                    ? subCategory.price
+                    : subCategoryPrice[0],
+                  localeFormat,
+                  currency,
+                  fractionDigits,
+                )
+              : `${formatNumber(
+                  subCategoryPrice[0],
+                  localeFormat,
+                  currency,
+                  fractionDigits,
+                )} - ${formatNumber(
+                  subCategoryPrice[1],
+                  localeFormat,
+                  currency,
+                  fractionDigits,
+                )}`}
+            {subCategory.priceUnit ? ` /${subCategory.priceUnit}` : ''}
+          </Text>
+        ) : null}
+      </Stack>
+
+      <DetailsStack direction="column" gap={0.5}>
+        {subCategory.details?.map(detail =>
+          detail ? (
+            <Text
+              key={detail}
+              as="span"
+              variant="bodySmall"
+              sentiment="neutral"
+            >
+              {detail}
+            </Text>
+          ) : null,
+        )}
+      </DetailsStack>
+    </Stack>
+  )
+}
+
+export const ScrollableContent = () => {
+  const { items } = useContext(OrderSummaryContext)
+
+  return (
+    <ContainerScrollable gap={3}>
+      {items.map(category =>
+        Object.keys(category).length > 0 && category.category ? (
+          <CategoryStack key={category.category} gap={1.5}>
+            <CategoryName category={category} />
+            <Stack gap={1}>
+              {category.subCategories &&
+              Object.keys(category.subCategories).length > 0
+                ? category.subCategories.map((subCategory, index) => (
+                    <SubCategory
+                      key={subCategory.title ?? `${index}`}
+                      subCategory={subCategory}
+                    />
+                  ))
+                : null}
+            </Stack>
+          </CategoryStack>
+        ) : null,
+      )}
+    </ContainerScrollable>
+  )
+}
---
+++ packages/plus/src/components/OrderSummary/__tests__/index.test.tsx
@@ -0,0 +1,208 @@
+import { screen } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
+import { renderWithTheme, shouldMatchEmotionSnapshot } from '@utils/test'
+import { describe, expect, test } from 'vitest'
+import { OrderSummary } from '..'
+import {
+  categoryAZ,
+  categoryCustomContent,
+  categoryDefault,
+  categoryM2,
+  categoryOptions,
+  categoryRequest,
+  categoryStorage,
+  fixePrice,
+  negativeItem,
+  numberInputCategory,
+  numberInputSubCategory,
+  rangePriceContent,
+  simpleCategory,
+} from './resources'
+
+const mockItems = [
+  categoryAZ,
+  categoryCustomContent,
+  categoryDefault,
+  categoryM2,
+  categoryOptions,
+  negativeItem,
+  fixePrice,
+  categoryRequest,
+  categoryStorage,
+]
+
+describe('OrderSummary', () => {
+  test('should work with default props', () =>
+    shouldMatchEmotionSnapshot(<OrderSummary items={mockItems} />))
+
+  test('should work with an empty list of item', () =>
+    shouldMatchEmotionSnapshot(<OrderSummary items={[]} />))
+
+  test('should work without unitInput', () =>
+    shouldMatchEmotionSnapshot(
+      <OrderSummary
+        header="summary"
+        items={mockItems}
+        currency="EUR"
+        localeFormat="en-EN"
+        hideTimeUnit
+      />,
+    ))
+
+  test('should work with custom timeUnit', async () => {
+    renderWithTheme(
+      <OrderSummary
+        header="summary"
+        items={[categoryAZ]}
+        currency="EUR"
+        localeFormat="en-EN"
+        periodOptions={['minutes', 'hours', 'days', 'months']}
+        valueUnitInput={2}
+        unitUnitInput="days"
+      />,
+    )
+    const unitInput = screen.getByTestId('unit-input')
+    expect(screen.getByTestId('total-price').textContent).toBe('€120.00')
+
+    await userEvent.type(unitInput, '[Backspace]1')
+    expect(screen.getByTestId('total-price').textContent).toBe('€60.00')
+
+    const unitInputUnit = screen.getByTestId('select-input--unit')
+    await userEvent.click(unitInputUnit)
+
+    const hours = screen.getByTestId('option-hours')
+    await userEvent.click(hours)
+    expect(screen.getByTestId('total-price').textContent).toBe('€2.50')
+  })
+
+  test('should work with children', () =>
+    shouldMatchEmotionSnapshot(
+      <OrderSummary
+        header="summary"
+        items={mockItems}
+        currency="EUR"
+        localeFormat="en-EN"
+      >
+        children
+      </OrderSummary>,
+    ))
+
+  test('should work with footer', () =>
+    shouldMatchEmotionSnapshot(
+      <OrderSummary
+        header="summary"
+        items={mockItems}
+        currency="EUR"
+        localeFormat="en-EN"
+        footer="footer"
+      />,
+    ))
+
+  test('should work with price as a range', () =>
+    shouldMatchEmotionSnapshot(
+      <OrderSummary
+        header="summary"
+        items={[rangePriceContent]}
+        currency="EUR"
+        localeFormat="en-EN"
+        footer="footer"
+      />,
+    ))
+
+  test('should work with totalPriceInfo', () =>
+    shouldMatchEmotionSnapshot(
+      <OrderSummary
+        header="summary"
+        items={mockItems}
+        currency="EUR"
+        localeFormat="en-EN"
+        totalPriceInfo="total price info"
+      />,
+    ))
+
+  test('should work with numberInputs', () =>
+    shouldMatchEmotionSnapshot(
+      <OrderSummary
+        header="summary"
+        items={[numberInputCategory, numberInputSubCategory]}
+        currency="EUR"
+        localeFormat="en-EN"
+        totalPriceInfo="total price info"
+      />,
+    ))
+
+  test('should work with discount in  %', () => {
+    const { asFragment } = renderWithTheme(
+      <OrderSummary
+        header="summary"
+        items={[simpleCategory]}
+        currency="EUR"
+        localeFormat="en-EN"
+        discount={0.5}
+      />,
+    )
+
+    const price = screen.getByTestId('total-price').textContent
+    expect(price).toBe('€2.50')
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('should work with discount', () => {
+    const { asFragment } = renderWithTheme(
+      <OrderSummary
+        header="summary"
+        items={[simpleCategory]}
+        currency="EUR"
+        localeFormat="en-EN"
+        discount={10}
+      />,
+    )
+
+    const price = screen.getByTestId('total-price').textContent
+    expect(price).toBe('€0.00')
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('should work with fractionDigits', () => {
+    const { asFragment } = renderWithTheme(
+      <OrderSummary
+        header="summary"
+        items={[simpleCategory]}
+        currency="EUR"
+        localeFormat="en-EN"
+        discount={0.5}
+        fractionDigits={0}
+      />,
+    )
+
+    const price = screen.getByTestId('total-price').textContent
+    expect(price).toBe('€3')
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('works with hideDetails', () =>
+    shouldMatchEmotionSnapshot(
+      <>
+        <OrderSummary header="summary" items={[categoryAZ]} hideDetails />
+        <OrderSummary
+          header="summary"
+          items={[categoryAZ]}
+          hideDetails
+          discount={0.5}
+        />
+      </>,
+    ))
+
+  test('works with negative category price', () => {
+    const { asFragment } = renderWithTheme(
+      <OrderSummary header="summary" items={[categoryAZ, negativeItem]} />,
+    )
+    const price = screen.getByTestId('total-price').textContent
+    expect(price).toBe('€0.00')
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
