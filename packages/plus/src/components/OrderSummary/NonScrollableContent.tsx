631550bc8c76d37ec28d31d1207389748030470e
lisalupi <106706307+lisalupi@users.noreply.github.com>
2025-06-25 17:29:37 +0200

    feat: order summary (#4794)
    * feat: order summary
    
    * feat: order summary
    
    * feat: order summary without tests
    
    * feat: order summary tests
    
    * fix: feedbacks
    
    * feat: number input and various improvements
    
    * fix: rebase
    
    * fix: add tests
    
    * fix: rebase
    
    * fix: discounts and more tests
    
    * fix: add range
    
    * feat: onChange WIP
    
    * feat: onchange and various changes
    
    * fix: stylinh and tests
    
    * feat: new prop to unit input needed for ordersummaryo
    
    * fix: designers feedback
    
    * fix: rebase
    
    * fix: update ordersummary and rebase
    
    * fix: tests after rebase
    
    * fix: rebase
    
    * fix: tests and detail hidden if empty string

---
+++ packages/plus/src/components/OrderSummary/NonScrollableContent.tsx
@@ -0,0 +1,106 @@
+import styled from '@emotion/styled'
+import { Stack, Text } from '@ultraviolet/ui'
+import { useContext } from 'react'
+import type { ReactNode } from 'react'
+import { OrderSummaryContext } from './Provider'
+import { DisplayPrice } from './helpers'
+import type { PriceTypeSingle, TimeUnit } from './types'
+
+const NonScrollableContainer = styled(Stack)`
+padding: ${({ theme }) => theme.space[3]};
+border-top: 1px solid ${({ theme }) => theme.colors.neutral.border};
+`
+
+type NonScrollableContentProps = {
+  discount: number
+  totalPrice: PriceTypeSingle
+  footer: ReactNode
+  children: ReactNode
+  totalPriceInfo?: ReactNode
+  totalPriceDescription?: ReactNode
+  hideDetails: boolean
+  unit: TimeUnit
+}
+
+export const NonScrollableContent = ({
+  totalPrice,
+  footer,
+  children,
+  totalPriceInfo,
+  hideDetails,
+  unit,
+  totalPriceDescription,
+}: NonScrollableContentProps) => {
+  const { locales } = useContext(OrderSummaryContext)
+
+  return (
+    <NonScrollableContainer gap={3}>
+      {children}
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        {totalPriceInfo ? (
+          <Stack>
+            <Stack direction="row" gap={1} alignItems="center">
+              <Text
+                as="span"
+                variant="bodyStrong"
+                sentiment="neutral"
+                prominence="strong"
+              >
+                {locales['order.summary.total']}:
+              </Text>
+              {totalPriceDescription}
+            </Stack>
+            {totalPriceInfo}
+          </Stack>
+        ) : (
+          <Stack direction="row" gap={1} alignItems="center">
+            <Text
+              as="span"
+              variant="bodyStrong"
+              sentiment="neutral"
+              prominence="strong"
+            >
+              {locales['order.summary.total']}:
+            </Text>
+            {totalPriceDescription}
+          </Stack>
+        )}
+        {totalPrice.totalPrice === totalPrice.totalPriceWithDiscount ? (
+          <Text
+            as="span"
+            variant="headingSmallStrong"
+            sentiment="neutral"
+            data-testid="total-price"
+            prominence="strong"
+          >
+            <DisplayPrice price={totalPrice} beforeOrAfter="after" />
+            {hideDetails ? `/${unit}` : null}
+          </Text>
+        ) : (
+          <Stack direction="row" gap={1} alignItems="center">
+            <Text
+              as="span"
+              variant="bodySmallStrong"
+              sentiment="neutral"
+              prominence="weak"
+              strikeThrough
+            >
+              <DisplayPrice price={totalPrice} beforeOrAfter="before" />
+            </Text>
+            <Text
+              as="span"
+              variant="headingSmallStrong"
+              sentiment="neutral"
+              data-testid="total-price"
+              prominence="strong"
+            >
+              <DisplayPrice price={totalPrice} beforeOrAfter="after" />
+              {hideDetails ? `/${unit}` : null}
+            </Text>
+          </Stack>
+        )}
+      </Stack>
+      {footer}
+    </NonScrollableContainer>
+  )
+}
