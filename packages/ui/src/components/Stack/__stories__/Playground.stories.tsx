378806d1a486d383a65163235630cc71cc95b4c4
Matthias <mprost@scaleway.com>
2025-07-03 15:36:37 +0200

    feat: responsive capacities based on breakpoints

--- packages/ui/src/components/Stack/__stories__/Playground.stories.tsx
+++ packages/ui/src/components/Stack/__stories__/Playground.stories.tsx
@@ -3,10 +3,5 @@ import { Template } from './Template.stories'
 export const Playground = Template.bind({})
 
 Playground.args = {
-  children: [
-    <div>First child</div>,
-    <div>Second child</div>,
-    <div>Third child</div>,
-  ],
-  gap: 2,
+  gap: 1,
 }
