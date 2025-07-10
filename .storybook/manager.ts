88857dcacc4132f83ac2db8b4ee964929630e691
Matthias <mprost@scaleway.com>
2025-07-10 17:25:48 +0200

    chore: update storybook to v9

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,4 +1,4 @@
-import { addons } from '@storybook/manager-api'
+import { addons } from 'storybook/manager-api'
 import { consoleLightTheme } from '@ultraviolet/themes'
 import { light } from './storybookThemes'
 
