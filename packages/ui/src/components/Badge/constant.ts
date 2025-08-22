70159777e54217381c472494a00e99a23d29ae57
lisalupi <106706307+lisalupi@users.noreply.github.com>
2025-08-22 11:06:09 +0200

    feat: vanilla extract on badge and actionBar (#5461)
    * feat: vanilla extract on badge and actionBar
    
    * fix: create vanilla extract fadeIn animation

---
+++ packages/ui/src/components/Badge/constant.ts
@@ -0,0 +1,16 @@
+export const SIZES = {
+  large: '400', // sizing token from theme
+  medium: '300',
+  small: '200',
+} as const
+
+export const PROMINENCES = {
+  default: 'default',
+  strong: 'strong',
+}
+
+export const TEXT_VARIANT = {
+  large: 'bodySmall',
+  medium: 'caption',
+  small: 'captionSmall',
+} as const
