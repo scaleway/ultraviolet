0ce72ef33b9b21dfa4404f54d1b0aaa1f156e41c
lisalupi <106706307+lisalupi@users.noreply.github.com>
2025-08-22 11:06:09 +0200

    feat: vanilla extract on badge and actionBar (#5461)
    * feat: vanilla extract on badge and actionBar
    
    * fix: create vanilla extract fadeIn animation

---
+++ packages/ui/src/components/ActionBar/styles.css.ts
@@ -0,0 +1,20 @@
+import { theme } from '@ultraviolet/themes'
+import { fadeInVanillaExtract } from '../../utils'
+import { style } from '@vanilla-extract/css'
+
+export const stackActionBar = style({
+  height: '100%',
+  padding: `0 ${theme.space[2]}`,
+})
+
+export const actionBar = style({
+  animation: `${fadeInVanillaExtract} 0.2s ease-in-out`,
+  backgroundColor: theme.colors.other.elevation.background.fixed,
+  borderRadius: theme.radii.default,
+  boxShadow: `${theme.shadows.fixed[0]}, ${theme.shadows.fixed[1]}`,
+  height: theme.sizing[700],
+  left: '50%',
+  position: 'fixed',
+  transform: 'translate(-50%, 0)',
+  width: '37.5rem',
+})
