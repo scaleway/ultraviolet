269ab2fe7d9264a97268f65b45553212c29b8da4
lisalupi <106706307+lisalupi@users.noreply.github.com>
2025-08-29 15:46:31 +0200

    refactor: vanilla extract on Separator and Loader (#5474)
    * feat: vanilla extract separator and loader
    
    * fix: use vanilla extract css var

---
+++ packages/ui/src/components/Loader/styles.css.ts
@@ -0,0 +1,17 @@
+import { theme } from '@ultraviolet/themes'
+import { keyframes, style } from '@vanilla-extract/css'
+
+const spin = keyframes({
+  '0%': { transform: 'rotate(0deg)' },
+  '100%': { transform: 'rotate(360deg)' },
+})
+export const circle = style({
+  stroke: theme.colors.neutral.border,
+})
+
+export const loader = style({
+  animationDuration: '0.75s',
+  animationIterationCount: 'infinite',
+  animationName: spin,
+  animationTimingFunction: 'linear',
+})
