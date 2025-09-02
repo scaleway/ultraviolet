3540f4d419f7fbd38c332e1ed62e9f106d5d4a56
lisalupi <106706307+lisalupi@users.noreply.github.com>
2025-09-02 10:58:17 +0200

    feat(Popover): vanilla extract popup (#5477)
    * feat: popup, popover and tooltip
    
    * fix: var in new file

---
+++ packages/ui/src/components/Popup/variables.css.ts
@@ -0,0 +1,21 @@
+import { createVar } from '@vanilla-extract/css'
+
+const arrowTop = createVar()
+const arrowLeft = createVar()
+const arrowTransform = createVar()
+const maxHeightPopup = createVar()
+const popupPosition = createVar()
+const animationDurationPopup = createVar()
+const popupInitialPosition = createVar()
+const maxWidthPopup = createVar()
+
+export {
+  arrowTop,
+  arrowLeft,
+  arrowTransform,
+  maxHeightPopup,
+  popupInitialPosition,
+  popupPosition,
+  animationDurationPopup,
+  maxWidthPopup,
+}
