61fe7d90983ae36699c38d01836149278efaf1ca
lisalupi <106706307+lisalupi@users.noreply.github.com>
2025-09-05 12:10:38 +0200

    feat: icons vanilla extract (#5481)
    * feat: icons vanilla extract wip
    
    * feat: update tests
    
    * fix: changeset

---
+++ packages/icons/src/components/ProductIcon/constants.ts
@@ -0,0 +1,9 @@
+export const SIZES = {
+  large: '600',
+  medium: '500',
+  small: '400',
+  xlarge: '800',
+  xsmall: '300',
+} as const
+
+export const VARIANTS = ['primary', 'danger', 'warning', 'original'] as const
---
+++ packages/icons/src/components/ProductIcon/styles.css.ts
@@ -0,0 +1,82 @@
+import { theme } from '@ultraviolet/themes'
+import { SIZES, VARIANTS } from './constants'
+import { globalStyle, styleVariants } from '@vanilla-extract/css'
+
+const PROMINENCES = ['Weak', 'Strong', ''] as const
+
+function generateSizeStyle(size: keyof typeof SIZES) {
+  return {
+    height: theme.sizing[SIZES[size]],
+    width: theme.sizing[SIZES[size]],
+    minWidth: theme.sizing[SIZES[size]],
+  }
+}
+
+function generateVariantStyle(
+  variant: (typeof VARIANTS)[number],
+  disabled: boolean,
+  prominence: (typeof PROMINENCES)[number],
+) {
+  const fillColor = `fill${prominence}${disabled ? 'Disabled' : ''}` as const
+
+  return {
+    fill: theme.colors.other.icon.product[variant][fillColor],
+  }
+}
+export const productIconSizes = styleVariants(
+  Object.fromEntries(
+    Object.keys(SIZES).map(size => [
+      size,
+      generateSizeStyle(size as keyof typeof SIZES),
+    ]),
+  ),
+)
+
+export const productIconVariants = styleVariants({
+  ...Object.fromEntries(VARIANTS.map(variant => [variant, {}])),
+  ...Object.fromEntries(VARIANTS.map(variant => [`${variant}Disabled`, []])),
+})
+
+PROMINENCES.forEach(prominence => {
+  VARIANTS.forEach(variant => {
+    const variantClass = productIconVariants[variant]
+    const variantClassDisabled = productIconVariants[`${variant}Disabled`]
+
+    const selectorDefault = (disabled?: boolean) =>
+      `${disabled ? variantClassDisabled : variantClass} .fill${prominence}`
+
+    const selectorGfill = (disabled?: boolean) =>
+      `${disabled ? variantClassDisabled : variantClass} g.fill${prominence} > *`
+
+    const selectorPathField = (disabled?: boolean) =>
+      `${disabled ? variantClassDisabled : variantClass} path[fill].fill${prominence}`
+
+    // Disabled style
+    globalStyle(
+      selectorDefault(true),
+      generateVariantStyle(variant, true, prominence),
+    )
+    globalStyle(
+      selectorGfill(true),
+      generateVariantStyle(variant, true, prominence),
+    )
+    globalStyle(
+      selectorPathField(true),
+      generateVariantStyle(variant, true, prominence),
+    )
+
+    // Non-disabled style
+    globalStyle(
+      selectorDefault(),
+      generateVariantStyle(variant, false, prominence),
+    )
+    globalStyle(
+      selectorGfill(),
+      generateVariantStyle(variant, false, prominence),
+    )
+    globalStyle(
+      selectorPathField(),
+      generateVariantStyle(variant, false, prominence),
+    )
+  })
+})
