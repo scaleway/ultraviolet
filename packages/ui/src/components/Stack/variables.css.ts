be21b81f1a776a7b7edcb0ef96eed3e58264c33d
Matthias Prost <mprost@scaleway.com>
2025-09-09 14:34:49 +0200

    refactor(Stack): vanilla extract (#5492)
    * refactor(Stack): vanilla extract
    
    * fix: add changeset
    
    * fix: issue with css type

---
+++ packages/ui/src/components/Stack/variables.css.ts
@@ -0,0 +1,6 @@
+import { createVar } from '@vanilla-extract/css'
+
+export const width = createVar()
+export const maxWidth = createVar()
+export const minWidth = createVar()
+export const flex = createVar()
