f4291467313f1f95ecf85dbd46c348cc6dafc4ed
Matthias Prost <mprost@scaleway.com>
2025-09-09 14:34:49 +0200

    refactor(Stack): vanilla extract (#5492)
    * refactor(Stack): vanilla extract
    
    * fix: add changeset
    
    * fix: issue with css type

--- packages/ui/src/components/Stack/index.tsx
+++ packages/ui/src/components/Stack/index.tsx
@@ -1,221 +1,133 @@
 'use client'
 
-import styled from '@emotion/styled'
-import type { CSSProperties, ReactNode } from 'react'
+import { consoleLightTheme } from '@ultraviolet/themes'
+import { assignInlineVars } from '@vanilla-extract/dynamic'
+import type { CSSProperties, HTMLAttributes, ReactNode, Ref } from 'react'
+import { useMemo } from 'react'
 import type { UltravioletUITheme } from '../../theme'
+import type { AlignItemsType, JustifyContentType } from './styles.css'
+import { sprinkles, stack } from './styles.css'
+import { flex, maxWidth, minWidth, width } from './variables.css'
 
-type StackProps = {
-  gap?:
+type ResponsiveProp<T> =
+  | T
+  | Partial<Record<keyof UltravioletUITheme['breakpoints'], T>>
+
+// convert union type string into same union type in number
+type ToNumber<T extends string> = T extends `${infer N extends number}`
+  ? N
+  : never
+
+export type StackProps = {
+  gap?: ResponsiveProp<
     | keyof UltravioletUITheme['space']
-    | number
-    | Partial<
-        Record<
-          keyof UltravioletUITheme['breakpoints'],
-          keyof UltravioletUITheme['space'] | number
-        >
-      >
-  direction?:
-    | 'row'
-    | 'column'
-    | Partial<Record<keyof UltravioletUITheme['breakpoints'], 'row' | 'column'>>
-  alignItems?:
-    | CSSProperties['alignItems']
-    | Partial<
-        Record<
-          keyof UltravioletUITheme['breakpoints'],
-          CSSProperties['alignItems']
-        >
-      >
-  justifyContent?:
-    | CSSProperties['justifyContent']
-    | Partial<
-        Record<
-          keyof UltravioletUITheme['breakpoints'],
-          CSSProperties['justifyContent']
-        >
-      >
-  wrap?:
-    | boolean
-    | CSSProperties['flexWrap']
-    | Partial<
-        Record<
-          keyof UltravioletUITheme['breakpoints'],
-          boolean | CSSProperties['flexWrap']
-        >
-      >
-  width?:
-    | CSSProperties['width']
-    | Partial<
-        Record<keyof UltravioletUITheme['breakpoints'], CSSProperties['width']>
-      >
-  maxWidth?:
-    | CSSProperties['maxWidth']
-    | Partial<
-        Record<
-          keyof UltravioletUITheme['breakpoints'],
-          CSSProperties['maxWidth']
-        >
-      >
-  minWidth?:
-    | CSSProperties['minWidth']
-    | Partial<
-        Record<
-          keyof UltravioletUITheme['breakpoints'],
-          CSSProperties['minWidth']
-        >
-      >
-  flex?:
-    | CSSProperties['flex']
-    | Partial<
-        Record<keyof UltravioletUITheme['breakpoints'], CSSProperties['flex']>
-      >
+    | ToNumber<keyof UltravioletUITheme['space']>
+  >
+  direction?: ResponsiveProp<
+    'row' | 'column' | 'row-reverse' | 'column-reverse'
+  >
+  alignItems?: ResponsiveProp<AlignItemsType>
+  justifyContent?: ResponsiveProp<JustifyContentType>
+  wrap?: ResponsiveProp<boolean | CSSProperties['flexWrap']>
+  width?: CSSProperties['width']
+  maxWidth?: CSSProperties['maxWidth']
+  minWidth?: CSSProperties['minWidth']
+  flex?: CSSProperties['flex']
   className?: string
   children: ReactNode
   'data-testid'?: string
   id?: string
+  ref?: Ref<HTMLDivElement>
+} & Omit<HTMLAttributes<HTMLDivElement>, 'children'>
+
+// It will give a rem value for each breakpoint key
+// Ex: gap={{ small: 2, xsmall: 1 }} => { small: '0.5rem', xsmall: '0.25rem' }
+const mapRepsonsiveGap = (
+  object?: Partial<
+    Record<
+      keyof typeof consoleLightTheme.breakpoints,
+      | keyof typeof consoleLightTheme.space
+      | ToNumber<keyof typeof consoleLightTheme.space>
+    >
+  >,
+) =>
+  object
+    ? Object.keys(object).reduce(
+        (acc, key) => ({
+          ...acc,
+          [key]:
+            consoleLightTheme.space[
+              object[
+                key as keyof typeof object
+              ] as keyof typeof consoleLightTheme.space
+            ],
+        }),
+        {},
+      )
+    : {}
+
+export const Stack: React.FC<StackProps> = ({
+  gap,
+  direction = 'column',
+  alignItems = 'normal',
+  justifyContent = 'normal',
+  wrap = 'nowrap',
+  className,
+  children,
+  id,
+  'data-testid': dataTestId,
+  ref,
+  ...props
+}) => {
+  const wrapValue = useMemo(() => {
+    if (typeof wrap === 'boolean') {
+      return wrap ? 'wrap' : 'nowrap'
+    }
+
+    if (typeof wrap === 'object') {
+      return Object.keys(wrap).reduce(
+        (acc, key) => ({
+          ...acc,
+          [key]: wrap[key as keyof typeof wrap] ? 'wrap' : 'nowrap',
+        }),
+        {},
+      )
+    }
+
+    return wrap
+  }, [wrap])
+
+  return (
+    <div
+      className={`${className ? `${className} ` : ''}${stack} ${sprinkles({
+        alignItems:
+          typeof alignItems === 'object' ? alignItems : { xxsmall: alignItems },
+        flexDirection:
+          typeof direction === 'object' ? direction : { xxsmall: direction },
+        flexWrap:
+          typeof wrapValue === 'object' ? wrapValue : { xxsmall: wrapValue },
+        gap:
+          typeof gap === 'object'
+            ? mapRepsonsiveGap(gap)
+            : { xxsmall: gap ? consoleLightTheme.space[gap] : undefined },
+        justifyContent:
+          typeof justifyContent === 'object'
+            ? justifyContent
+            : { xxsmall: justifyContent },
+      })}`}
+      data-testid={dataTestId}
+      id={id}
+      ref={ref}
+      style={assignInlineVars({
+        [width]: width,
+        [maxWidth]: maxWidth,
+        [minWidth]: minWidth,
+        [flex]: flex,
+      })}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+    >
+      {children}
+    </div>
+  )
 }
-
-/**
- * A Stack is a widget that organize children in a vertical or horizontal layout based on css Flex,
- * it accepts few props to deal with spacing and align.
- */
-export const Stack = styled('div', {
-  shouldForwardProp: prop =>
-    ![
-      'gap',
-      'direction',
-      'alignItems',
-      'justifyContent',
-      'wrap',
-      'width',
-      'maxWidth',
-      'minWidth',
-      'flex',
-    ].includes(prop),
-})<StackProps>`
-  display: flex;
-
-  ${({
-    theme,
-    gap,
-    direction = 'column',
-    alignItems = 'normal',
-    justifyContent = 'normal',
-    wrap = 'nowrap',
-    width,
-    maxWidth,
-    minWidth,
-    flex,
-  }) => `
-  ${wrap && typeof wrap === 'boolean' ? 'flex-wrap: wrap;' : ''}
-    ${
-      wrap && typeof wrap === 'object'
-        ? Object.entries(wrap)
-            .map(
-              ([breakpoint, value]) =>
-                `@media (min-width: ${theme.breakpoints[breakpoint as keyof UltravioletUITheme['breakpoints']]}) {
-               flex-wrap: ${value};
-              }`,
-            )
-            .join(' ')
-        : `${wrap && typeof wrap !== 'boolean' ? `flex-wrap: ${wrap};` : ''}`
-    }
-    ${
-      minWidth && typeof minWidth === 'object'
-        ? Object.entries(minWidth)
-            .map(
-              ([breakpoint, value]) =>
-                `@media (min-width: ${theme.breakpoints[breakpoint as keyof UltravioletUITheme['breakpoints']]}) {
-                min-width: ${value};
-              }`,
-            )
-            .join(' ')
-        : `${minWidth ? `min-width: ${minWidth};` : ''}`
-    }
-    ${
-      maxWidth && typeof maxWidth === 'object'
-        ? Object.entries(maxWidth)
-            .map(
-              ([breakpoint, value]) =>
-                `@media (min-width: ${theme.breakpoints[breakpoint as keyof UltravioletUITheme['breakpoints']]}) {
-                max-width: ${value};
-              }`,
-            )
-            .join(' ')
-        : `${maxWidth ? `max-width: ${maxWidth};` : ''}`
-    }
-    ${
-      width && typeof width === 'object'
-        ? Object.entries(width)
-            .map(
-              ([breakpoint, value]) =>
-                `@media (min-width: ${theme.breakpoints[breakpoint as keyof UltravioletUITheme['breakpoints']]}) {
-                width: ${value};
-              }`,
-            )
-            .join(' ')
-        : `${width ? `width: ${width};` : ''}`
-    }
-    ${
-      flex && typeof flex === 'object'
-        ? Object.entries(flex)
-            .map(
-              ([breakpoint, value]) =>
-                `@media (min-width: ${theme.breakpoints[breakpoint as keyof UltravioletUITheme['breakpoints']]}) {
-                flex: ${value};
-              }`,
-            )
-            .join(' ')
-        : `${flex ? `flex: ${flex};` : ''}`
-    }
-    ${
-      alignItems && typeof alignItems === 'object'
-        ? Object.entries(alignItems)
-            .map(
-              ([breakpoint, value]) =>
-                `@media (min-width: ${theme.breakpoints[breakpoint as keyof UltravioletUITheme['breakpoints']]}) {
-                align-items: ${value};
-              }`,
-            )
-            .join(' ')
-        : `${alignItems ? `align-items: ${alignItems};` : ''}`
-    }
-    ${
-      justifyContent && typeof justifyContent === 'object'
-        ? Object.entries(justifyContent)
-            .map(
-              ([breakpoint, value]) =>
-                `@media (min-width: ${theme.breakpoints[breakpoint as keyof UltravioletUITheme['breakpoints']]}) {
-                justify-content: ${value};
-              }`,
-            )
-            .join(' ')
-        : `${justifyContent ? `justify-content: ${justifyContent};` : ''}`
-    }
-    ${
-      direction && typeof direction === 'object'
-        ? Object.entries(direction)
-            .map(
-              ([breakpoint, value]) =>
-                `@media (min-width: ${theme.breakpoints[breakpoint as keyof UltravioletUITheme['breakpoints']]}) {
-                flex-direction: ${value};
-              }`,
-            )
-            .join(' ')
-        : `${direction ? `flex-direction: ${direction};` : ''}`
-    }
-
-    ${
-      gap && typeof gap === 'object'
-        ? Object.entries(gap)
-            .map(
-              ([breakpoint, value]) =>
-                `@media (min-width: ${theme.breakpoints[breakpoint as keyof UltravioletUITheme['breakpoints']]}) {
-                gap: ${theme.space[value.toString() as keyof UltravioletUITheme['space']]};
-              }`,
-            )
-            .join(' ')
-        : `${gap ? `gap: ${theme.space[gap as keyof UltravioletUITheme['space']]};` : ''}`
-    }
-  `}
-`
