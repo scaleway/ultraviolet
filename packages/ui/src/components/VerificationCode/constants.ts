bb7cc37681f4a350c54c0ad3c4cc71e349900d2e
Matthias <mprost@scaleway.com>
2025-09-09 18:10:53 +0200

    refactor(VerificationCode): migrate to use vanilla extract

---
+++ packages/ui/src/components/VerificationCode/constants.ts
@@ -0,0 +1,13 @@
+export const SIZE_HEIGHT = {
+  large: '600',
+  medium: '500',
+  small: '400',
+  xlarge: '800',
+} as const
+
+export const SIZE_WIDTH = {
+  large: '500',
+  medium: '400',
+  small: '300',
+  xlarge: '700',
+} as const
