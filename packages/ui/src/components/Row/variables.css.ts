6e8b98dc150ae7abe2361dddc68291a32033f791
Matthias Prost <mprost@scaleway.com>
2025-09-17 11:15:35 +0200

    fix: stack and row initial values on dynamic variables (#5547)

--- packages/ui/src/components/Row/variables.css.ts
+++ packages/ui/src/components/Row/variables.css.ts
@@ -5,22 +5,70 @@ export const templateColumn: Record<
   keyof typeof theme.breakpoints,
   ReturnType<typeof createVar>
 > = {
-  large: createVar(),
-  xlarge: createVar(),
-  medium: createVar(),
-  small: createVar(),
-  xsmall: createVar(),
-  xxsmall: createVar(),
+  large: createVar({
+    syntax: '*',
+    inherits: false,
+    initialValue: 'none',
+  }),
+  xlarge: createVar({
+    syntax: '*',
+    inherits: false,
+    initialValue: 'none',
+  }),
+  medium: createVar({
+    syntax: '*',
+    inherits: false,
+    initialValue: 'none',
+  }),
+  small: createVar({
+    syntax: '*',
+    inherits: false,
+    initialValue: 'none',
+  }),
+  xsmall: createVar({
+    syntax: '*',
+    inherits: false,
+    initialValue: 'none',
+  }),
+  xxsmall: createVar({
+    syntax: '*',
+    inherits: false,
+    initialValue: 'none',
+  }),
 }
 
 export const paddings: Record<
   keyof typeof theme.breakpoints,
   ReturnType<typeof createVar>
 > = {
-  large: createVar(),
-  xlarge: createVar(),
-  medium: createVar(),
-  small: createVar(),
-  xsmall: createVar(),
-  xxsmall: createVar(),
+  large: createVar({
+    syntax: '*',
+    inherits: false,
+    initialValue: '0',
+  }),
+  xlarge: createVar({
+    syntax: '*',
+    inherits: false,
+    initialValue: '0',
+  }),
+  medium: createVar({
+    syntax: '*',
+    inherits: false,
+    initialValue: '0',
+  }),
+  small: createVar({
+    syntax: '*',
+    inherits: false,
+    initialValue: '0',
+  }),
+  xsmall: createVar({
+    syntax: '*',
+    inherits: false,
+    initialValue: '0',
+  }),
+  xxsmall: createVar({
+    syntax: '*',
+    inherits: false,
+    initialValue: '0',
+  }),
 }
--- packages/ui/src/components/Stack/variables.css.ts
+++ packages/ui/src/components/Stack/variables.css.ts
@@ -1,6 +1,22 @@
 import { createVar } from '@vanilla-extract/css'
 
-export const widthVar = createVar()
-export const maxWidthVar = createVar()
-export const minWidthVar = createVar()
-export const flexVar = createVar()
+export const widthVar = createVar({
+  syntax: '*',
+  inherits: false,
+  initialValue: 'auto',
+})
+export const maxWidthVar = createVar({
+  syntax: '*',
+  inherits: false,
+  initialValue: 'none',
+})
+export const minWidthVar = createVar({
+  syntax: '*',
+  inherits: false,
+  initialValue: 'none',
+})
+export const flexVar = createVar({
+  syntax: '*',
+  inherits: false,
+  initialValue: '0 1 auto',
+})
