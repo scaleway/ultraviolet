d475219f187c91973281cf3da2dd16ca4809e433
lisalupi <106706307+lisalupi@users.noreply.github.com>
2025-09-30 10:09:05 +0200

    refactor(SelectableCard): migration vanilla extract (#5596)
    * refactor: vanilla extract selectableCard
    
    * fix: style checkbox in selectable card

--- packages/ui/src/components/SelectableCard/index.tsx
+++ packages/ui/src/components/SelectableCard/index.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import { useTheme } from '@emotion/react'
-import styled from '@emotion/styled'
 import * as ProductIcon from '@ultraviolet/icons/product'
+import { assignInlineVars } from '@vanilla-extract/dynamic'
 import type {
   ChangeEventHandler,
   FocusEventHandler,
@@ -21,243 +21,19 @@ import {
 } from 'react'
 import type { LabelProp, PascalToCamelCaseWithoutSuffix } from '../../types'
 import { Checkbox } from '../Checkbox'
-import {
-  checkboxContainer,
-  icon as checkboxIcon,
-  checkboxInput,
-  innerCheckbox,
-} from '../Checkbox/styles.css'
 import { Radio } from '../Radio'
-import {
-  innerCircleRing,
-  radio as radioInput,
-  radioStack,
-  ring,
-} from '../Radio/styles.css'
 import { Stack } from '../Stack'
 import { Tooltip } from '../Tooltip'
-
-const Container = styled(Stack)`
-  position: relative;
-  // This is to remove the gap when there is no label because if we do not there
-  // will be an empty space above the children due to the invisible input
-  // if you find a better way to do this feel free to do it
-  &[data-has-label='false'] > :first-child {
-    margin-bottom: -${({ theme }) => theme.space['0.5']};
-  }
-
-  padding: ${({ theme }) => theme.space['2']};
-  border-radius: ${({ theme }) => theme.radii.default};
-  transition:
-    border-color 200ms ease,
-    box-shadow 200ms ease;
-  cursor: pointer;
-  &[data-has-default-cursor='true'] {
-    cursor: default;
-  }
-  background: ${({ theme }) => theme.colors.neutral.background};
-
-  border: 1px solid ${({ theme }) => theme.colors.neutral.border};
-  color: ${({ theme }) => theme.colors.neutral.text};
-
-  &[data-checked='true'] {
-    border: 1px solid ${({ theme }) => theme.colors.primary.border};
-  }
-
-  &[data-error='true'] {
-    border: 1px solid ${({ theme }) => theme.colors.danger.border};
-  }
-
-  &[data-disabled='true'] {
-    border: 1px solid ${({ theme }) => theme.colors.neutral.borderDisabled};
-    color: ${({ theme }) => theme.colors.neutral.textDisabled};
-    background: ${({ theme }) => theme.colors.neutral.backgroundDisabled};
-    cursor: not-allowed;
-  }
-
-  &[data-image="illustration"] {
-    padding: ${({ theme }) => theme.space[0]};
-  }
-
-  &[data-image="icon"] {
-    padding: ${({ theme }) => theme.space[0]};
-    padding-right: ${({ theme }) => theme.space['2']};
-  }
-  &:hover,
-  &:active {
-    &:not([data-error='true']):not([data-disabled='true']) {
-      border: 1px solid ${({ theme }) => theme.colors.primary.border};
-
-      &[data-cheked='false'] {
-        box-shadow: ${({ theme }) => theme.shadows.hoverPrimary};
-      }
-    }
-  }
-
-  &[data-has-label='true'] {
-    ${radioStack}, ${checkboxContainer} {
-      width: 100%;
-    }
-  }
-`
-const StyledDiv = styled.div`
-  display: flex;
-  gap: 0;
-  flex-flow: column;
-  align-items: normal;
-  justify-content: center;
-  min-width: 11.25rem;
-  position: relative;
-  overflow: hidden;
-`
-
-const StyledImg = styled.img`
-  object-fit: cover;
-  position: absolute;
-  min-width: 13.75rem;
-  height: auto;
-  left: ${({ theme }) => theme.space[1]};
-`
-
-const StyledSVG = styled.div`
-  object-fit: cover;
-  position: absolute;
-  min-width: 13.75rem;
-  height: auto;
-  left: ${({ theme }) => theme.space[1]};
-`
-
-const IllustrationStack = styled(Stack)`
-  padding: ${({ theme }) => theme.space[2]};
-  max-width:  calc(100% - 10rem);
-  flex: 0 1 auto;
-`
-
-const StyledStack = styled(Stack)`
-  &[data-has-label='true'] {
-    padding-left: ${({ theme }) => theme.space['4']};
-  }
-  &[data-has-label='false'] {
-    display: contents;
-  }
-  &[data-has-default-cursor='true'] {
-    cursor: default;
-  }
-`
-
-const StyledElement = styled('div', {
-  shouldForwardProp: prop => !['showTick', 'hasLabel'].includes(prop),
-})<{ showTick?: boolean; hasLabel?: boolean }>`
-  display: inline-flex;
-  align-items: start;
-
-  &[data-checked='true'] {
-    color: ${({ theme }) => theme.colors.primary.text};
-  }
-
-  &[data-error='true'] {
-    color: ${({ theme }) => theme.colors.danger.text};
-  }
-
-  &[aria-disabled='true'] {
-    color: ${({ theme }) => theme.colors.neutral.textDisabled};
-  }
-
-  input + svg {
-    ${({ showTick }) => (!showTick ? `display: none;` : null)}
-  }
-
-  label {
-    ${({ showTick, hasLabel }) =>
-      !showTick && !hasLabel ? `display: none;` : null}
-  }
-`
-
-const OverloadedRadio = StyledElement.withComponent(Radio)
-const StyledRadio = styled(OverloadedRadio)`
-  &:hover[aria-disabled='false']:not([data-checked='true']) {
-    ${radioInput} + ${ring} {
-      fill: ${({ theme }) => theme.colors.neutral.border};
-      ${innerCircleRing} {
-        fill: ${({ theme }) => theme.colors.neutral.background};
-      }
-    }
-
-    ${radioInput}[aria-invalid='true'] + ${ring} {
-      fill: ${({ theme }) => theme.colors.danger.border};
-      ${innerCircleRing} {
-        fill: ${({ theme }) => theme.colors.neutral.background};
-      }
-    }
-  }
-
-  &:hover[aria-disabled='false'] {
-    ${radioInput} + ${ring} {
-      fill: ${({ theme }) => theme.colors.primary.border};
-      ${innerCircleRing} {
-        fill: ${({ theme }) => theme.colors.neutral.background};
-      }
-    }
-
-    ${radioInput}[aria-invalid='true'] + ${ring} {
-      fill: ${({ theme }) => theme.colors.danger.border};
-      ${innerCircleRing} {
-        fill: ${({ theme }) => theme.colors.neutral.background};
-      }
-    }
-  }
-
-  ${radioInput} {
-    &[aria-disabled='false']:active + ${ring} {
-      background: none;
-      fill: ${({ theme }) => theme.colors.primary.backgroundStrong};
-      ${innerCircleRing} {
-        fill: ${({ theme }) => theme.colors.neutral.background};
-      }
-    }
-  }
-`
-
-const OverloadedCheckbox = StyledElement.withComponent(Checkbox)
-const StyledCheckbox = styled(OverloadedCheckbox)`
-  label {
-    width: 100%;
-  }
-
-  &:hover[aria-disabled='false'] {
-    ${checkboxInput}[aria-invalid='false'] {
-      &[aria-checked='false'] + ${checkboxIcon} ${innerCheckbox} {
-        fill: ${({ theme }) => theme.colors.neutral.background};
-        stroke: ${({ theme }) => theme.colors.neutral.border};
-      }
-
-      &[aria-checked='true'] + ${checkboxIcon} ${innerCheckbox} {
-        stroke: ${({ theme }) => theme.colors.primary.borderStrong};
-        fill: ${({ theme }) => theme.colors.primary.backgroundStrong};
-      }
-
-      &[aria-checked='mixed'] + ${checkboxIcon} ${innerCheckbox} {
-        stroke: ${({ theme }) => theme.colors.primary.borderStrong};
-        fill: ${({ theme }) => theme.colors.primary.backgroundStrong};
-      }
-    }
-  }
-
-  ${checkboxInput} {
-    &:focus + ${checkboxIcon}, &:active + ${checkboxIcon} {
-      outline: none;
-      background-color: ${({ theme }) => theme.colors.neutral.background};
-      fill: ${({ theme }) => theme.colors.neutral.background};
-    }
-
-    &[aria-checked='false'] {
-      ${innerCheckbox} {
-        fill: ${({ theme }) => theme.colors.neutral.background};
-        stroke: ${({ theme }) => theme.colors.neutral.border};
-      }
-    }
-  }
-`
+import {
+  containerSelectableCard,
+  divSelectableCard,
+  illustrationSelectableCard,
+  imageSelectableCard,
+  labelContainerSelectableCard,
+  selectableElementSelectableCard,
+  stackSelectableCard,
+} from './styles.css'
+import { inputDisplay, labelDisplay, widthSelectable } from './variables.css'
 
 export type SelectableCardProps = {
   name?: string
@@ -394,26 +170,30 @@ export const SelectableCard = forwardRef(
               justifyContent="space-between"
               width="100%"
             >
-              <IllustrationStack>{subChildren}</IllustrationStack>
+              <Stack className={illustrationSelectableCard}>
+                {subChildren}
+              </Stack>
               <Stack justifyContent="center">
                 {ProductIconUsed ? <ProductIconUsed size="large" /> : null}
               </Stack>
 
               {illustration ? (
-                <StyledDiv>
+                <div className={divSelectableCard}>
                   {illustration.endsWith('.svg') && svgContent ? (
-                    <StyledSVG
+                    <div
+                      className={imageSelectableCard}
                       // oxlint-disable-next-line  react/no-danger
                       dangerouslySetInnerHTML={{ __html: svgContent }}
                     />
                   ) : (
-                    <StyledImg
+                    <img
                       alt="illustration"
+                      className={imageSelectableCard}
                       src={illustration}
                       width={220}
                     />
                   )}
-                </StyledDiv>
+                </div>
               ) : null}
             </Stack>
           )
@@ -462,15 +242,12 @@ export const SelectableCard = forwardRef(
 
     return (
       <ParentContainer>
-        <Container
+        <Stack
           alignItems="start"
-          className={className}
+          className={`${className ? `${className} ` : ''}${containerSelectableCard({ cursor: type === 'checkbox' && isComplexChildren ? 'default' : 'custom', image })} ${labelContainerSelectableCard[label ? 'label' : 'noLabel']}`}
           data-checked={checked}
           data-disabled={disabled}
           data-error={isError}
-          data-has-default-cursor={type === 'checkbox' && isComplexChildren}
-          data-has-label={!!label}
-          data-image={image}
           data-testid={dataTestId}
           data-type={type}
           direction={label ? 'column' : 'row'}
@@ -484,41 +261,44 @@ export const SelectableCard = forwardRef(
           onKeyDown={onKeyDown}
           ref={ref}
           role="button"
+          style={assignInlineVars({
+            [inputDisplay]: !showTick ? 'none' : 'inline',
+            [labelDisplay]: !showTick && !label ? 'none' : 'inline',
+            [widthSelectable]: label ? '100%' : 'auto',
+          })}
           tabIndex={disabled ? undefined : 0}
         >
           <IllustrationContainer>
             {type === 'radio' ? (
-              <StyledRadio
+              <Radio
                 checked={checked}
+                className={selectableElementSelectableCard}
                 data-error={isError}
                 disabled={disabled}
                 error={isError}
-                hasLabel={!!label}
                 id={id}
                 name={name}
                 onBlur={onBlur}
                 onChange={onChange}
                 onFocus={onFocus}
                 ref={innerRef}
-                showTick={showTick}
                 tabIndex={-1}
                 value={value}
                 {...(label ? { label } : { 'aria-label': ariaLabel as string })}
               />
             ) : (
-              <StyledCheckbox
+              <Checkbox
                 checked={checked}
+                className={selectableElementSelectableCard}
                 data-error={isError}
                 disabled={disabled}
                 error={isError}
-                hasLabel={!!label}
                 id={id}
                 name={name}
                 onBlur={onBlur}
                 onChange={onChange}
                 onFocus={onFocus}
                 ref={innerRef}
-                showTick={showTick}
                 tabIndex={-1}
                 value={value}
                 {...(label
@@ -527,7 +307,8 @@ export const SelectableCard = forwardRef(
               />
             )}
             {children ? (
-              <StyledStack
+              <Stack
+                className={stackSelectableCard}
                 data-has-default-cursor={
                   type === 'checkbox' && isComplexChildren
                 }
@@ -538,10 +319,10 @@ export const SelectableCard = forwardRef(
                 {typeof children === 'function'
                   ? children({ checked, disabled })
                   : children}
-              </StyledStack>
+              </Stack>
             ) : null}
           </IllustrationContainer>
-        </Container>
+        </Stack>
       </ParentContainer>
     )
   },
---
+++ packages/ui/src/components/SelectableCard/styles.css.ts
@@ -0,0 +1,155 @@
+import { theme } from '@ultraviolet/themes'
+import { globalStyle, style, styleVariants } from '@vanilla-extract/css'
+import { recipe } from '@vanilla-extract/recipes'
+import { radioStack } from '../Radio/styles.css'
+import { inputDisplay, labelDisplay, widthSelectable } from './variables.css'
+import { checkboxContainer } from '../Checkbox/styles.css'
+
+export const containerSelectableCard = recipe({
+  base: {
+    position: 'relative',
+    padding: theme.space[2],
+    borderRadius: theme.radii.default,
+    transition: `
+    border-color 200ms ease,
+    box-shadow 200ms ease`,
+    cursor: 'pointer',
+    background: theme.colors.neutral.background,
+    border: `1px solid ${theme.colors.neutral.border}`,
+    color: theme.colors.neutral.text,
+    width: '100%',
+    selectors: {
+      '&[data-checked="true"]': {
+        border: `1px solid ${theme.colors.primary.border}`,
+      },
+      '&[data-error="true"]': {
+        border: `1px solid ${theme.colors.danger.border}`,
+      },
+      '&[data-disabled="true"]': {
+        border: `1px solid ${theme.colors.neutral.borderDisabled}`,
+        color: theme.colors.neutral.textDisabled,
+        background: theme.colors.neutral.backgroundDisabled,
+        cursor: 'not-allowed',
+      },
+      '&:hover:not([data-error="true"]):not([data-disabled="true"]), &:active:not([data-error="true"]):not([data-disabled="true"])':
+        {
+          border: `1px solid ${theme.colors.primary.border}`,
+        },
+
+      '&[data-checked="false"]:hover:not([data-error="true"]):not([data-disabled="true"]), &[data-checked="false"]:active:not([data-error="true"]):not([data-disabled="true"])':
+        {
+          boxShadow: theme.shadows.hoverPrimary,
+        },
+    },
+  },
+  variants: {
+    image: {
+      none: {},
+      icon: {
+        padding: theme.space[0],
+        paddingRight: theme.space[2],
+      },
+      illustration: {
+        padding: theme.space[0],
+      },
+    },
+    cursor: {
+      default: {
+        cursor: 'default',
+      },
+      custom: {
+        cursor: 'pointer',
+      },
+    },
+  },
+  defaultVariants: {
+    image: 'none',
+    cursor: 'custom',
+  },
+})
+
+export const labelContainerSelectableCard = styleVariants({
+  label: {
+    selectors: {
+      '&:first-child': {
+        marginBottom: `-${theme.space['0.5']}`,
+      },
+    },
+  },
+  noLabel: {},
+})
+
+globalStyle(`${labelContainerSelectableCard.label} ${radioStack}`, {
+  width: '100%',
+})
+
+globalStyle(`${labelContainerSelectableCard.label} ${checkboxContainer}`, {
+  width: '100%',
+})
+
+export const divSelectableCard = style({
+  display: 'flex',
+  gap: 0,
+  flexFlow: 'column',
+  alignItems: 'normal',
+  justifyContent: 'center',
+  minWidth: '11.25rem',
+  position: 'relative',
+  overflow: 'hidden',
+})
+
+export const imageSelectableCard = style({
+  objectFit: 'cover',
+  position: 'absolute',
+  minWidth: '13.75rem',
+  height: 'auto',
+  left: theme.space[1],
+})
+
+export const illustrationSelectableCard = style({
+  padding: theme.space[2],
+  maxWidth: 'calc(100% - 10rem)',
+  flex: '0 1 auto',
+})
+
+export const stackSelectableCard = style({
+  selectors: {
+    '&[data-has-label="true"]': {
+      paddingLeft: theme.space[4],
+    },
+    '&[data-has-label="false"]': {
+      display: 'contents',
+    },
+    '&[data-has-default-cursor="true"]': {
+      cursor: 'default',
+    },
+  },
+})
+
+export const selectableElementSelectableCard = style({
+  display: 'inline-flex',
+  alignItems: 'start',
+  selectors: {
+    '&[data-checked="true"]': {
+      color: theme.colors.primary.text,
+    },
+    '&[data-error="true"]': {
+      color: theme.colors.danger.text,
+    },
+    '&[data-disabled="true"]': {
+      color: theme.colors.neutral.textDisabled,
+    },
+  },
+})
+
+globalStyle(`${selectableElementSelectableCard} > input + svg`, {
+  display: inputDisplay,
+})
+
+globalStyle(`${selectableElementSelectableCard} > label`, {
+  display: labelDisplay,
+})
+
+globalStyle(`${selectableElementSelectableCard} > ${radioStack}`, {
+  width: widthSelectable,
+})
