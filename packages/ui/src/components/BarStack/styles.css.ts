45a469c0105e092a3a031e034b4a7a56e4413f6f
lisalupi <106706307+lisalupi@users.noreply.github.com>
2025-10-01 10:23:31 +0200

    refactor(Skeleton): vanilla extract (#5598)
    * refactor: skeleton vanilla extract
    
    * fix: css variables

--- packages/ui/src/components/BarStack/styles.css.ts
+++ packages/ui/src/components/BarStack/styles.css.ts
@@ -1,27 +1,20 @@
 import { theme } from '@ultraviolet/themes'
-import { style } from '@vanilla-extract/css'
-import {
-  neutralStrong33,
-  neutralStrongerB3,
-  neutralWeak1A,
-  neutralWeak33,
-  neutralWeak40,
-  primaryStrong12,
-  primaryStrong4D,
-  secondaryStrong40,
-  secondaryStrongBF,
-  wrapperWidth,
-} from './variables.css'
+import { createVar, style } from '@vanilla-extract/css'
 
+export const wrapperWidth = createVar()
+
+function createColor(color: string, opacity: number) {
+  return `rgb(from ${color} r g b / ${opacity})`
+}
 export const wrapperBarStack = style({
   width: wrapperWidth,
   transition: 'width 500ms',
   backgroundColor: theme.colors.neutral.backgroundWeak,
   selectors: {
     '&:nth-child(5n+1)': {
-      background: `linear-gradient(-45deg, ${neutralWeak1A} 25%,
+      background: `linear-gradient(-45deg, ${createColor(theme.colors.neutral.backgroundWeak, 0.1)} 25%,
       ${theme.colors.primary.backgroundStrong} 25%, ${theme.colors.primary.backgroundStrong} 50%,
-      ${neutralWeak1A} 50%, ${neutralWeak1A} 75%, ${theme.colors.primary.backgroundStrong}
+      ${createColor(theme.colors.neutral.backgroundWeak, 0.1)} 50%, ${createColor(theme.colors.neutral.backgroundWeak, 0.1)} 75%, ${theme.colors.primary.backgroundStrong}
        75%)`,
       backgroundSize: '30px 30px',
       backgroundColor: theme.colors.primary.backgroundStrong,
@@ -30,32 +23,32 @@ export const wrapperBarStack = style({
       backgroundColor: `color-mix(in srgb, ${theme.colors.primary.backgroundStrong} 85%, transparent)`,
       backgroundImage: `linear-gradient(
         135deg,
-        ${neutralStrong33} 25%,
+        ${createColor(theme.colors.neutral.backgroundStrong, 0.2)} 25%,
         transparent 25%
       ),
       linear-gradient(
         225deg,
-        ${neutralStrong33} 25%,
+        ${createColor(theme.colors.neutral.backgroundStrong, 0.2)} 25%,
         transparent 25%
       ),
       linear-gradient(
         45deg,
-        ${neutralStrong33} 25%,
+        ${createColor(theme.colors.neutral.backgroundStrong, 0.2)} 25%,
         transparent 25%
       ),
       linear-gradient(
         315deg,
-        ${neutralStrong33} 25%,
-        ${primaryStrong12} 25%
+        ${createColor(theme.colors.neutral.backgroundStrong, 0.2)} 25%,
+        ${createColor(theme.colors.primary.backgroundStrong, 0.07)} 25%
       )`,
       backgroundPosition: '10px 0, 10px 0, 0 0, 0 0',
       backgroundSize: '10px 10px',
       backgroundRepeat: 'repeat',
     },
     '&:nth-child(5n+3)': {
-      background: `linear-gradient(-45deg, ${neutralWeak1A} 25%,
-      ${primaryStrong4D} 25%, ${primaryStrong4D} 50%,
-      ${neutralWeak1A} 50%, ${neutralWeak1A} 75%, ${primaryStrong4D}
+      background: `linear-gradient(-45deg, ${createColor(theme.colors.neutral.backgroundWeak, 0.1)} 25%,
+      ${createColor(theme.colors.primary.backgroundStrong, 0.3)} 25%, ${createColor(theme.colors.primary.backgroundStrong, 0.3)} 50%,
+      ${createColor(theme.colors.neutral.backgroundWeak, 0.1)} 50%, ${createColor(theme.colors.neutral.backgroundWeak, 0.1)} 75%, ${createColor(theme.colors.primary.backgroundStrong, 0.3)}
        75%)`,
       backgroundSize: '30px 30px',
       backgroundColor: theme.colors.secondary.backgroundStrong,
@@ -64,22 +57,22 @@ export const wrapperBarStack = style({
       backgroundColor: theme.colors.secondary.backgroundStrong,
       backgroundImage: ` linear-gradient(
         135deg,
-        ${neutralWeak33} 25%,
+        ${createColor(theme.colors.neutral.backgroundWeak, 0.2)} 25%,
         transparent 25%
       ),
       linear-gradient(
         225deg,
-        ${neutralWeak33} 25%,
+        ${createColor(theme.colors.neutral.backgroundWeak, 0.2)} 25%,
         transparent 25%
       ),
       linear-gradient(
         45deg,
-        ${neutralWeak33} 25%,
+        ${createColor(theme.colors.neutral.backgroundWeak, 0.2)} 25%,
         transparent 25%
       ),
       linear-gradient(
         315deg,
-        ${neutralWeak33} 25%,
+        ${createColor(theme.colors.neutral.backgroundWeak, 0.2)} 25%,
         ${theme.colors.secondary.backgroundStrong} 25%
       )`,
       backgroundPosition: '10px 0, 10px 0, 0 0, 0 0',
@@ -87,12 +80,12 @@ export const wrapperBarStack = style({
       backgroundRepeat: 'repeat',
     },
     '&:nth-child(5n+5)': {
-      background: `linear-gradient(-45deg, ${neutralWeak40} 25%,
-      ${secondaryStrong40} 25%, ${secondaryStrong40} 50%,
-      ${neutralWeak40} 50%, ${neutralWeak40} 75%, ${secondaryStrong40}
+      background: `linear-gradient(-45deg, ${createColor(theme.colors.neutral.backgroundWeak, 0.25)} 25%,
+      ${createColor(theme.colors.secondary.borderStrong, 0.25)} 25%, ${createColor(theme.colors.secondary.borderStrong, 0.25)} 50%,
+      ${createColor(theme.colors.neutral.backgroundWeak, 0.25)} 50%, ${createColor(theme.colors.neutral.backgroundWeak, 0.25)} 75%, ${createColor(theme.colors.secondary.borderStrong, 0.25)}
        75%);`,
       backgroundSize: '30px 30px',
-      backgroundColor: `${secondaryStrongBF}`,
+      backgroundColor: `${createColor(theme.colors.secondary.borderStrong, 0.75)}`,
     },
   },
 })
@@ -109,10 +102,10 @@ export const barStack = style({
   whiteSpace: 'nowrap',
   overflow: 'hidden',
   textShadow: `-1px 0
-      ${neutralStrongerB3},
-    0 1px ${neutralStrongerB3},
-    1px 0 ${neutralStrongerB3},
-    0 -1px ${neutralStrongerB3}`,
+      ${theme.colors.neutral.backgroundStronger},
+    0 1px ${createColor(theme.colors.neutral.backgroundStronger, 0.7)},
+    1px 0 ${createColor(theme.colors.neutral.backgroundStronger, 0.7)},
+    0 -1px ${createColor(theme.colors.neutral.backgroundStronger, 0.7)}`,
 })
 
 export const containerBarStack = style({
