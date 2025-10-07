4e038106bf90dc1a44db45b19417d3ad04955c5c
lisalupi <106706307+lisalupi@users.noreply.github.com>
2025-10-07 11:51:05 +0200

    refactor: switch button vanilla extract (#5634)

--- packages/ui/src/components/SwitchButton/Option.tsx
+++ packages/ui/src/components/SwitchButton/Option.tsx
@@ -1,87 +1,10 @@
 'use client'
 
-import styled from '@emotion/styled'
 import type { ReactNode } from 'react'
 import { useEffect, useRef } from 'react'
 import { SelectableCard } from '../SelectableCard'
 import { useSwitchButton } from './SwitchButtonContext'
-
-const StyledSelectableCard = styled(SelectableCard, {
-  shouldForwardProp: prop => !['sentiment'].includes(prop),
-})<{ sentiment: 'neutral' | 'primary'; disabled?: boolean }>`
-  border: none;
-  padding: ${({ theme }) => theme.space['1']} ${({ theme }) => theme.space['2']};
-  font-weight: ${({ theme }) => theme.typography.bodyStrong.weight};
-  justify-content: center;
-  align-items: center;
-  transition: all 200ms ease-in-out;
-  white-space: nowrap;
-  background: transparent;
-  height: 100%;
-  user-select: none;
-
-  &[data-checked="false"]:active,  &[data-checked="false"]:hover {
-    &:not([data-error="true"]):not([data-disabled="true"]) { 
-      box-shadow: none;
-    }
-  }
-
-  &:hover,
-  &:active {
-    box-shadow: none;
-    border: none;
-    &:not([data-error='true'][data-disabled='true']) {
-      border: none;
-    }
-  }
-
-  &[data-checked='true'] {
-    border: none;
-  }
-
-  & label {
-    transition: color 300ms;
-    color: ${({ theme }) => theme.colors.neutral.textStrong};
-  }
-
-  &[data-checked='true'] label {
-    color: ${({ theme, sentiment }) =>
-      sentiment === 'neutral'
-        ? theme.colors.neutral.textStrong
-        : theme.colors.primary.textStrong};
-  }
-
-  &:not([data-checked='true']) label {
-    &:hover {
-      color: ${({ theme, sentiment }) =>
-        sentiment === 'neutral'
-          ? theme.colors.neutral.textHover
-          : theme.colors.primary.text};
-    }
-}
-
-  ${({ disabled, theme }) =>
-    disabled
-      ? `
-      &[data-disabled='true'] {
-        background: transparent;
-        border: none;
-      }
-
-      label {
-        color: ${theme.colors.neutral.textDisabled};
-      }
-
-      &:not([data-checked='true']) label {
-        &:hover {
-          background: transparent;
-          color: ${theme.colors.neutral.textDisabled};
-        }
-      }
-  `
-      : ''}
-
-`
+import { switchButtonOption } from './styles.css'
 
 type OptionProps = {
   value: string
@@ -124,18 +47,18 @@ export const Option = ({
   }, [refOptions, setRefOptions, value])
 
   return (
-    <StyledSelectableCard
+    <SelectableCard
       checked={localValue === value}
+      className={switchButtonOption[sentiment]}
       data-checked={localValue === value}
       data-testid={dataTestId ?? `switch-button-${value}`}
-      disabled={disabled}
+      disabled={!!disabled}
       label={children}
       name={name}
       onBlur={onBlur}
       onChange={handleOnChange}
       onFocus={onFocus}
       ref={ref}
-      sentiment={sentiment}
       tooltip={tooltip}
       value={value}
     />
