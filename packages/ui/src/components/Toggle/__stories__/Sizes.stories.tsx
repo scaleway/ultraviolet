ae20c4b9f3a4153e1098a180508db4b37d9d80a0
Matthias Prost <mprost@scaleway.com>
2025-10-07 16:33:50 +0200

    refactor(Toggle): to use vanilla extract (#5615)
    * refactor(Toggle): to use vanilla extract
    
    * fix: feedback lisa

--- packages/ui/src/components/Toggle/__stories__/Sizes.stories.tsx
+++ packages/ui/src/components/Toggle/__stories__/Sizes.stories.tsx
@@ -1,17 +1,11 @@
 import type { StoryFn } from '@storybook/react-vite'
 import { Stack } from '../../Stack'
-import { SIZES, Toggle } from '..'
+import { Toggle } from '..'
 
 export const Sizes: StoryFn = props => (
   <>
-    {Object.keys(SIZES).map(size => (
-      <Toggle
-        key={size}
-        label={size}
-        name="label"
-        size={size as keyof typeof SIZES}
-        {...props}
-      />
+    {(['small', 'large'] as const).map(size => (
+      <Toggle key={size} label={size} name="label" size={size} {...props} />
     ))}
   </>
 )
--- packages/ui/src/components/Toggle/index.tsx
+++ packages/ui/src/components/Toggle/index.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import styled from '@emotion/styled'
 import type {
   ChangeEventHandler,
   InputHTMLAttributes,
@@ -12,158 +11,13 @@ import { Row } from '../Row'
 import { Stack } from '../Stack'
 import { Text } from '../Text'
 import { Tooltip } from '../Tooltip'
-
-export const SIZES = {
-  large: {
-    ball: '200', // sizing token from theme
-    height: '300',
-    width: '600',
-  },
-  small: {
-    ball: '150',
-    height: '250',
-    width: '500',
-  },
-} as const
-
-const StyledCheckbox = styled.input`
-  position: absolute;
-  opacity: 0;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  cursor: pointer;
-
-  &[disabled] {
-    cursor: not-allowed;
-  }
-`
-
-const StyledToggle = styled.div<{
-  size: 'small' | 'large'
-}>`
-  box-sizing: content-box;
-  outline: none;
-  overflow: hidden;
-  display: flex;
-  align-items: center;
-  border: none;
-  border-radius: ${({ theme }) => theme.radii.xlarge};
-  position: relative;
-  transition: all 300ms;
-  background-color: ${({ theme }) => theme.colors.neutral.backgroundStrong};
-  width: ${({ size, theme }) => theme.sizing[SIZES[size].width]};
-  height: ${({ size, theme }) => theme.sizing[SIZES[size].height]};
-
-  &:hover {
-    background-color: ${({ theme }) =>
-      theme.colors.neutral.backgroundStrongHover};
-  }
-
-  &:after {
-    content: "";
-    position: absolute;
-    top: ${({ size, theme }) => `calc(${theme.sizing[SIZES[size].height]} / 2 - ${theme.sizing[SIZES[size].ball]} / 2)`};
-    left: 5px;
-    width: ${({ size, theme }) => theme.sizing[SIZES[size].ball]};
-    height: ${({ size, theme }) => theme.sizing[SIZES[size].ball]};
-    border-radius: ${({ theme }) => theme.radii.circle};
-    background-color: ${({ theme }) => theme.colors.neutral.background};
-    transition: all 300ms;
-  }
-
-  &:focus-within,
-  &:focus {
-    box-shadow: ${({ theme }) => theme.shadows.focusNeutral};
-  }
-
-  &[data-disabled="false"]:active:after {
-    width: ${({ size, theme }) => `calc(${theme.sizing[SIZES[size].ball]} * 1.3775)`};
-  }
-
-  &:has(:checked) {
-    color: ${({ theme }) => theme.colors.neutral.textStrong};
-    background-color: ${({ theme }) => theme.colors.primary.backgroundStrong};
-
-    &:hover {
-      background-color: ${({ theme }) =>
-        theme.colors.primary.backgroundStrongHover};
-    }
-
-    &:after {
-      left: calc(100% - 5px);
-      transform: translateX(-100%);
-    }
-
-    &:focus-within,
-    &:focus {
-      box-shadow: ${({ theme }) => theme.shadows.focusPrimary};
-    }
-  }
-
-  &[data-disabled="true"] {
-    background: ${({ theme }) => theme.colors.neutral.backgroundStrongDisabled};
-
-    &:has(:checked) {
-      background: ${({ theme }) =>
-        theme.colors.primary.backgroundStrongDisabled};
-    }
-  }
-
-  &[data-error="true"] {
-    background-color: ${({ theme }) => theme.colors.danger.background};
-
-    &:focus-within,
-    &:focus {
-      box-shadow: ${({ theme }) => theme.shadows.focusDanger};
-    }
-
-    &:has(:checked) {
-      background-color: ${({ theme }) => theme.colors.danger.backgroundStrong};
-    }
-
-    &[data-disabled="true"] {
-      background-color: ${({ theme }) => theme.colors.danger.backgroundDisabled};
-
-      &:has(:checked) {
-        background-color: ${({ theme }) =>
-          theme.colors.danger.backgroundStrongDisabled};
-      }
-    }
-  }
-`
-
-const StyledLabel = styled.label<{
-  size: 'small' | 'large'
-  labelPosition: 'left' | 'right'
-}>`
-  display: flex;
-  gap: ${({ theme }) => theme.space['1']};
-  align-items: start;
-  width: fit-content;
-  cursor: pointer;
-  flex-direction: ${({ labelPosition }) =>
-    labelPosition === 'left' ? 'row' : 'row-reverse'};
-
-  &:active ${StyledToggle}[data-disabled='false']:after {
-  width: ${({ size, theme }) => `calc(${theme.sizing[SIZES[size].ball]} * 1.3775)`};
-  }
-
-  &[aria-disabled="true"] {
-    cursor: not-allowed;
-    color: ${({ theme }) => theme.colors.neutral.textDisabled};
-  }
-`
+import { checkbox, label as labelReceipe, toggle } from './styles.css'
 
 type ToggleProps = {
   id?: string
   checked?: boolean
   name?: string
   tooltip?: string
-  /**
-   * If `onChange` is given component will work as a controlled component if not it will work as an uncontrolled component.
-   */
   onChange?: ChangeEventHandler<HTMLInputElement>
   size?: 'large' | 'small'
   labelPosition?: 'left' | 'right'
@@ -177,9 +31,6 @@ type ToggleProps = {
   'data-testid'?: string
 } & Pick<InputHTMLAttributes<HTMLInputElement>, 'value'>
 
-/**
- * Toggle component is used to toggle between two states (on/off, true/false, etc.).
- */
 export const Toggle = forwardRef(
   (
     {
@@ -206,12 +57,13 @@ export const Toggle = forwardRef(
 
     return (
       <Tooltip text={tooltip}>
-        <StyledLabel
+        <label
           aria-disabled={disabled}
-          className={className}
+          className={`${className ? `${className} ` : ''}${labelReceipe({
+            disabled,
+            labelPosition,
+          })}`}
           data-testid={dataTestId}
-          labelPosition={labelPosition}
-          size={size}
         >
           <Stack alignItems="baseline" gap={0.25}>
             {label ? (
@@ -257,15 +109,18 @@ export const Toggle = forwardRef(
               </Text>
             ) : null}
           </Stack>
-          <StyledToggle
-            data-disabled={disabled}
-            data-error={!!error}
-            size={size}
+          <div
+            className={toggle({
+              disabled,
+              error: !!error,
+              size,
+            })}
           >
-            <StyledCheckbox
+            <input
               aria-invalid={!!error}
               aria-label={ariaLabel}
               checked={checked}
+              className={checkbox}
               disabled={disabled}
               id={id ?? uniqueId}
               name={name}
@@ -275,8 +130,8 @@ export const Toggle = forwardRef(
               type="checkbox"
               value={value}
             />
-          </StyledToggle>
-        </StyledLabel>
+          </div>
+        </label>
       </Tooltip>
     )
   },
