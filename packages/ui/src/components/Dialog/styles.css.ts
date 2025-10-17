29119a0a5b30d48a4d031442754b8ffca2f06e1d
lisa <llupi@scaleway.com>
2025-10-17 15:26:03 +0200

    refactor: dialog vanilla extract

---
+++ packages/ui/src/components/Dialog/styles.css.ts
@@ -0,0 +1,9 @@
+import { theme } from '@ultraviolet/themes'
+import { style } from '@vanilla-extract/css'
+
+export const dialogTitle = style({
+  marginTop: theme.space[2],
+  marginBottom: theme.space[1],
+})
+
+export const dialogXsmall = style({ width: '32.5rem' })
