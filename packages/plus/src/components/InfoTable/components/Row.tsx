68006e14d373276ce7d77638ec6261229cce60b5
lisalupi <106706307+lisalupi@users.noreply.github.com>
2025-10-21 14:21:18 +0200

    refactor: infotable vanilla extract (#5714)

--- packages/plus/src/components/InfoTable/components/Row.tsx
+++ packages/plus/src/components/InfoTable/components/Row.tsx
@@ -1,33 +1,29 @@
 'use client'
 
-import styled from '@emotion/styled'
 import { Row } from '@ultraviolet/ui'
+import { assignInlineVars } from '@vanilla-extract/dynamic'
 import type { ReactNode } from 'react'
+import { useContext } from 'react'
+import { InfoTableContext } from '../context'
+import { infoTableRow, rowWidth } from '../styles.css'
 
-export const StyledRow = styled(Row)`
-  border-bottom: 1px ${({ theme }) => theme.colors.neutral.border} solid;
-  padding-block: ${({ theme }) => theme.space[2]};
-
-  :first-of-type {
-    padding-top: 0;
-  }
-
-  :last-of-type {
-    padding-bottom: 0;
-    border-bottom-color: transparent;
-
-  }
-
-  & > *:not(:last-child) {
-    padding-right: ${({ theme }) => theme.space[2]}; // Use this instead of gap so that elements can be aligned even if there is not the same number of columns
-
-  }
-`
 type RowProps = {
   children: ReactNode
   templateColumns: string
 }
 
-export const InfoTableRow = ({ children, templateColumns }: RowProps) => (
-  <StyledRow templateColumns={templateColumns}>{children}</StyledRow>
-)
+export const InfoTableRow = ({ children, templateColumns }: RowProps) => {
+  const { width } = useContext(InfoTableContext)
+
+  return (
+    <Row
+      className={infoTableRow}
+      style={assignInlineVars({
+        [rowWidth]: width ?? '100%',
+      })}
+      templateColumns={templateColumns}
+    >
+      {children}
+    </Row>
+  )
+}
---
+++ packages/plus/src/components/InfoTable/context.tsx
@@ -0,0 +1,3 @@
+import { createContext } from 'react'
+
+export const InfoTableContext = createContext<{ width?: string }>({})
