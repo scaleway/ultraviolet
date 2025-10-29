944d85e28ce400810cb16e97097862a5ed184a15
lisalupi <106706307+lisalupi@users.noreply.github.com>
2025-10-29 15:27:22 +0100

    fix: export toggle group field (#5748)

--- packages/form/src/components/ToggleGroupField/__stories__/Template.stories.tsx
+++ packages/form/src/components/ToggleGroupField/__stories__/Template.stories.tsx
@@ -1,37 +1,19 @@
 import type { StoryFn } from '@storybook/react-vite'
-import { Stack } from '@ultraviolet/ui'
-import type { ComponentProps } from 'react'
-import { useWatch } from '../../..'
 import { ToggleGroupField } from '..'
 
-const ToggleGroupFieldStory = (
-  args: ComponentProps<typeof ToggleGroupField>,
-) => {
-  const values = useWatch()
-
-  return (
-    <Stack gap={2}>
-      <ToggleGroupField {...args}>
-        <ToggleGroupField.Toggle
-          label="Automatically run a save every monday at 6 am"
-          name="weekly-save"
-          value="weekly-save"
-        />
-        <ToggleGroupField.Toggle
-          label="Reboot server every day at 9 am"
-          name="daily-reboot"
-          value="daily-reboot"
-        />
-      </ToggleGroupField>
-      <span>
-        <b>Form content:</b> {JSON.stringify(values)}
-      </span>
-    </Stack>
-  )
-}
-
 export const Template: StoryFn<typeof ToggleGroupField> = args => (
-  <ToggleGroupFieldStory {...args} />
+  <ToggleGroupField {...args}>
+    <ToggleGroupField.Toggle
+      label="Automatically run a save every monday at 6 am"
+      name="weekly-save"
+      value="weekly-save"
+    />
+    <ToggleGroupField.Toggle
+      label="Reboot server every day at 9 am"
+      name="daily-reboot"
+      value="daily-reboot"
+    />
+  </ToggleGroupField>
 )
 
 Template.args = {
