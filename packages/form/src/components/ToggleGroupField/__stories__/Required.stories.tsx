c547316274609a8858a2602a0d69d6fae10dab1e
lisalupi <106706307+lisalupi@users.noreply.github.com>
2025-10-29 15:27:22 +0100

    fix: export toggle group field (#5748)

--- packages/form/src/components/ToggleGroupField/__stories__/Required.stories.tsx
+++ packages/form/src/components/ToggleGroupField/__stories__/Required.stories.tsx
@@ -1,39 +1,24 @@
 import type { StoryFn } from '@storybook/react-vite'
 import { Stack } from '@ultraviolet/ui'
-import type { ComponentProps } from 'react'
-import { useWatch } from '../../..'
 import { Submit } from '../..'
 import { ToggleGroupField } from '..'
 
-export const RequiredStory = (
-  args: ComponentProps<typeof ToggleGroupField>,
-) => {
-  const values = useWatch()
-
-  return (
-    <Stack gap={1}>
-      <ToggleGroupField {...args}>
-        <ToggleGroupField.Toggle
-          label="Automatically run a save every monday at 6 am"
-          name="weekly-save"
-          value="weekly-save"
-        />
-        <ToggleGroupField.Toggle
-          label="Reboot server every day at 9 am"
-          name="daily-reboot"
-          value="daily-reboot"
-        />
-      </ToggleGroupField>
-      <Submit>Submit</Submit>
-      <span>
-        <b>Form content:</b> {JSON.stringify(values)}
-      </span>
-    </Stack>
-  )
-}
-
 export const Required: StoryFn<typeof ToggleGroupField> = args => (
-  <RequiredStory {...args} />
+  <Stack gap={1}>
+    <ToggleGroupField {...args}>
+      <ToggleGroupField.Toggle
+        label="Automatically run a save every monday at 6 am"
+        name="weekly-save"
+        value="weekly-save"
+      />
+      <ToggleGroupField.Toggle
+        label="Reboot server every day at 9 am"
+        name="daily-reboot"
+        value="daily-reboot"
+      />
+    </ToggleGroupField>
+    <Submit>Submit</Submit>
+  </Stack>
 )
 
 Required.parameters = {
