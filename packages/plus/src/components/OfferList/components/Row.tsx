395189e82144d6f5f15e36adac889fa38601a1aa
lisa <llupi@scaleway.com>
2025-11-06 18:06:07 +0100

    fix: add prominence on badge

--- packages/plus/src/components/OfferList/components/Row.tsx
+++ packages/plus/src/components/OfferList/components/Row.tsx
@@ -38,6 +38,7 @@ type RowProps = ComponentProps<typeof List.Row> & {
   badge?: {
     text: string
     sentiment?: ComponentProps<typeof BadgeUV>['sentiment']
+    prominence?: ComponentProps<typeof BadgeUV>['prominence']
   }
   style?: CSSProperties
 }
@@ -158,8 +159,8 @@ export const Row = ({
               <BadgeUV
                 className={offerListBadge}
                 disabled={disabled}
-                sentiment={badge.sentiment}
                 prominence={badge.prominence}
+                sentiment={badge.sentiment}
                 size="small"
               >
                 {badge.text}
