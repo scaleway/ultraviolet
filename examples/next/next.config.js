afd48706de80646ae57c534ee58a51ca0a411230
lisa <llupi@scaleway.com>
2025-11-19 15:10:41 +0100

    fix: next config

--- examples/next/next.config.js
+++ examples/next/next.config.js
@@ -1,21 +1,16 @@
-const nextConfig = () => {
-  /** @type {import('next/dist/server/config').NextConfig} */
-  const config = {
-    compress: true,
-    images: {
-      formats: ['image/avif', 'image/webp'],
-      loader: 'imgix',
-      path: 'https://ultraviolet.scaleway.com',
-    },
-    poweredByHeader: false,
-    reactStrictMode: true,
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    transpilePackages: ['react-syntax-highlighter'],
-  }
-
-  return config
+const nextConfig = {
+  compress: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    loader: 'imgix',
+    path: 'https://ultraviolet.scaleway.com',
+  },
+  poweredByHeader: false,
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  transpilePackages: ['react-syntax-highlighter'],
 }
 
-export default nextConfig()
+export default nextConfig
