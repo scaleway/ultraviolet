cb6a516c8be8baf8cfdd2c6799c9481c835dd2c2
lisa <llupi@scaleway.com>
2025-12-01 17:24:40 +0100

    feat: add label and helper to snippet

---
+++ .changeset/many-ghosts-show.md
@@ -0,0 +1,5 @@
+---
+"@ultraviolet/ui": patch
+---
+
+`Snippet`: add label and helper
---
+++ packages/ui/src/components/Snippet/__stories__/LabelHelper.stories.tsx
@@ -0,0 +1,23 @@
+import { Badge } from '../../Badge'
+import { Template } from './Template.stories'
+
+export const LabelHelper = Template.bind({})
+
+LabelHelper.args = {
+  children: `pnpm add @ultraviolet/ui`,
+  helper: 'helper',
+  label: 'Label',
+  labelDescription: (
+    <Badge sentiment="success" size="small">
+      new
+    </Badge>
+  ),
+}
+
+LabelHelper.parameters = {
+  docs: {
+    description: {
+      story: `Like an input, it is possible to add a label, labelDescription and helper.`,
+    },
+  },
+}
