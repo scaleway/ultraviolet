b22b4907f2e9cb0a0b48eec064923f2d9a82009b
lisa <llupi@scaleway.com>
2025-12-12 10:43:00 +0100

    fix: add tests

--- packages/ui/src/components/FileInput/components/Button.tsx
+++ packages/ui/src/components/FileInput/components/Button.tsx
@@ -9,12 +9,6 @@ export const FileInputButton = ({
   ...props
 }: ComponentProps<typeof Button>) => {
   const context = useFileInput()
-
-  if (!context) {
-    throw new Error(
-      'FileInput.Button should be inside FileInput to work properly.',
-    )
-  }
   const isDisabled = disabled || context.disabled
 
   return (
