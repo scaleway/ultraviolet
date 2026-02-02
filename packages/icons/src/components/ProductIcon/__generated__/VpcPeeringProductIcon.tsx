5a0bb321edcc86efde0d5e78385e97d38cfe732d
lisa <llupi@scaleway.com>
2026-02-02 10:27:57 +0100

    fix: re generate new icons

--- packages/icons/src/components/ProductIcon/__generated__/VpcPeeringProductIcon.tsx
+++ packages/icons/src/components/ProductIcon/__generated__/VpcPeeringProductIcon.tsx
@@ -10,9 +10,9 @@ import { Icon } from '../Icon'
 
 export const VpcPeeringProductIcon = ({
   ...props
-}: Omit<IconProps, 'children'>) => (
+}: Omit<IconProps, 'children' | 'title'>) => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Icon {...props}>
+  <Icon {...props} title="VpcPeeringProductIcon">
     <g>
       <path
         className="fillWeak"
