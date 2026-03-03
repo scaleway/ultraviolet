1fd02bd7693c40cab585f2a5edab915646a6427f
lisa <llupi@scaleway.com>
2026-03-03 16:09:13 +0100

    chore: rename file and remove duplicates

---
+++ packages/utils/src/__tests__/isJson.ts
@@ -0,0 +1,19 @@
+// biome-ignore-all lint/style/noDoneCallback: test ok
+import { describe, expect, test } from 'vitest'
+import { isJSONString } from '../isJson'
+
+describe(isJSONString, () => {
+  test.each`
+    test                      | value            | expected
+    ${'is correct JSON'}      | ${'{ "a": 10 }'} | ${true}
+    ${'is Boolean'}           | ${true}          | ${true}
+    ${'is Number'}            | ${10}            | ${true}
+    ${'is null'}              | ${null}          | ${true}
+    ${'JSON is unterminated'} | ${'{ "a": 10'}   | ${false}
+    ${'no argument passed'}   | ${undefined}     | ${false}
+    ${'is Array'}             | ${[1, 2, 3]}     | ${false}
+    ${'is String'}            | ${'hello'}       | ${false}
+  `('returns $expected when $test', current => {
+    expect(isJSONString(current.value as string)).toBe(current.expected)
+  })
+})
