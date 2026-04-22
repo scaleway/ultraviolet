1ca4ae7b18b1a1f0acf976c60b8e7466100b0687
Julien Saguez <jsaguez@scaleway.com>
2026-04-22 10:27:04 +0200

    fix(RichTextInput): fix another threads

--- packages/form/src/compositions/RichTextInputField/__stories__/Required.stories.tsx
+++ packages/form/src/compositions/RichTextInputField/__stories__/Required.stories.tsx
@@ -1,20 +1,5 @@
-import { Stack } from '@ultraviolet/ui'
-
-import { RichTextInputField } from '..'
-import { Submit } from '../../../components/Submit'
-
 import { Template } from './Template.stories'
 
-import type { StoryFn } from '@storybook/react-vite'
-import type { ComponentProps } from 'react'
-
-export const Required: StoryFn<
-  ComponentProps<typeof RichTextInputField>
-> = args => (
-  <Stack gap={1}>
-    <RichTextInputField {...args} />
-    <Submit>Submit</Submit>
-  </Stack>
-)
+export const Required = Template.bind({})
 
 Required.args = { ...Template.args, required: true }
