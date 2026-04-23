149e0b77decbe54a7b5530513c60594ab3449a96
Lisa LUPI <llupi@scaleway.com>
2026-04-23 15:59:57 +0200

    fix(OptionSelector): remove unwanted padding and margin

--- packages/ui/src/compositions/OptionSelector/styles.css.ts
+++ packages/ui/src/compositions/OptionSelector/styles.css.ts
@@ -7,6 +7,8 @@ import { selectInputStyle } from '../../components/styles'
 const wrapper = recipe({
   base: {
     border: 'none',
+    padding: 0,
+    margin: 0,
   },
   variants: {
     direction: {
