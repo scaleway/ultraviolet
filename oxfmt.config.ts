3770daf1fafaec40de200bc4f1338c2ff38693b5
Emmanuel CHAMBON <echambon@scaleway.com>
2026-05-11 16:47:26 +0200

    fix: update config
    Signed-off-by: Emmanuel CHAMBON <echambon@scaleway.com>

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -1,36 +1,15 @@
-import config from '@scaleway/oxfmt-config'
+import config, { mergeConfig } from '@scaleway/oxfmt-config'
 import { defineConfig } from 'oxfmt'
 
-export default defineConfig({
-  ...config,
-  printWidth: 80,
-  sortImports: {
-    order: 'asc',
-    groups: ['builtin', 'internal', 'external', 'parent', 'sibling', 'type'],
-    newlinesBetween: true,
-  },
-  ignorePatterns: [
-    'CHANGELOG.md',
-    '.changeset',
-    '**/_generated',
-    '**/*.gen.*',
-    '**/.next',
-    '**/next-env.d.ts',
-    '**/.output',
-    '**/dist',
-    '**/build',
-    '**/out',
-    '**/.turbo',
-    '**/storybook-static',
-    '**/.cache',
-    '**/public/build',
-    '**/.vite',
-    '**/coverage',
-    '**/.nyc_output',
-    '**/*.auto.*',
-    '**/graphql-types.*',
-    '**/schema.d.ts',
-    '**/schema.graphql.d.ts',
-    '**/*.d.ts.map',
-  ],
-})
+export default mergeConfig(
+  config,
+  defineConfig({
+    ...config,
+    printWidth: 80,
+    sortImports: {
+      order: 'asc',
+      groups: ['builtin', 'internal', 'external', 'parent', 'sibling', 'type'],
+      newlinesBetween: true,
+    },
+  }),
+)
