1f6ff9858fd8c71ad04652ef92a5aaed3d9c0d23
Julien Sulpis <jsulpis@scaleway.com>
2026-05-13 11:54:36 +0200

    feat(Link): update the decoration and sentiment behavior, fix styles for the different sizes

--- packages/ui/src/components/Action/Link/__stories__/Target.stories.tsx
+++ packages/ui/src/components/Action/Link/__stories__/Target.stories.tsx
@@ -1,12 +1,33 @@
-import { Template } from './Template.stories'
+import type { ComponentProps } from 'react'
+import { Link } from '..'
+import { Stack } from '../../Stack'
 
-export const Target = Template.bind({})
+const sizes = ['large', 'small', 'xsmall'] as const
+
+export const Target = (props: ComponentProps<typeof Link>) => (
+  <Stack gap={2}>
+    <Stack direction="row" alignItems="end" gap="3">
+      {sizes.map(size => (
+        <Link key={size} {...props} href={undefined} size={size} target="_blank">
+          {size}
+        </Link>
+      ))}
+    </Stack>
+    <Stack direction="row" alignItems="end" gap="3">
+      {sizes.map(size => (
+        <Link key={size} {...props} size={size} target="_blank">
+          {size}
+        </Link>
+      ))}
+    </Stack>
+  </Stack>
+)
 
 Target.parameters = {
   docs: {
     description: {
       story:
-        'Edit the `target` prop to specify the target you want for your link. By using `_blank`, an icon is added to show that it is an external link',
+        'Edit the `target` property to specify the target you want for your link. External links (`target="_blank"`) have an arrow icon automatically added, they have an `info` sentiment by default, and can have a visited state.',
     },
   },
 }
--- packages/ui/src/components/Action/Link/constants.ts
+++ packages/ui/src/components/Action/Link/constants.ts
@@ -1,6 +1,9 @@
-export const PROMINENCES = {
+export const PROMINENCE_VALUES = ['default', 'strong', 'stronger', 'weak'] as const
+export type ProminenceType = (typeof PROMINENCE_VALUES)[number]
+
+export const PROMINENCES: Record<ProminenceType, '' | 'strong'> = {
   default: '',
   strong: 'strong',
-  stronger: 'stronger',
-  weak: 'weak',
-} as const
+  stronger: 'strong',
+  weak: '',
+}
