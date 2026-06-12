fadc9aea429a5a53b63ca7f5038c6c12f050f9d6
Julien Sulpis <jsulpis@scaleway.com>
2026-06-12 12:21:46 +0200

    feat: add function as child component to the filters context

---
+++ packages/ui/src/compositions/Filters/__stories__/Context.stories.tsx
@@ -0,0 +1,46 @@
+import type { StoryFn } from '@storybook/react-vite'
+import { useState } from 'react'
+import { Button, Stack } from '../../../components'
+import { FiltersMainRow } from '../parts/FiltersMainRow'
+import { FiltersProvider } from '../parts/FiltersProvider'
+import { defaultValues, demoFilters } from './demo.config'
+
+export const Context: StoryFn = () => {
+  const [filterValues, setFilterValues] = useState(defaultValues)
+
+  return (
+    <Stack gap="4">
+      <FiltersProvider defaultValues={defaultValues} onSubmit={setFilterValues}>
+        {({ filters }) => (
+          <>
+            <FiltersMainRow
+              config={demoFilters.slice(0, 3)}
+              labels={{
+                clearAll: 'Clear all',
+                seeAll: 'All filters',
+              }}
+            />
+            <Button
+              variant="outlined"
+              onClick={() => {
+                filters.reset()
+                filters.submit(filters.defaultValues)
+              }}
+            >
+              Reset from outside of the component
+            </Button>
+          </>
+        )}
+      </FiltersProvider>
+      <Stack direction="row" gap="4">
+        <div>
+          <strong>Submitted values</strong>
+          <p>Updated when changing a filter in the main row or when submitting in the Drawer.</p>
+          <small>
+            <pre>{JSON.stringify(filterValues, null, 2)}</pre>
+          </small>
+        </div>
+      </Stack>
+    </Stack>
+  )
+}
