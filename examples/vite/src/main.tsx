72630fdf6cca150d8552af7e35e2e1fa3208c965
Alexandre Philibeaux <aphilibeaux@scaleway.com>
2026-06-17 12:24:26 +0200

    feat(font): add bundled fonts
    Signed-off-by: Alexandre Philibeaux <aphilibeaux@scaleway.com>

--- examples/vite/src/main.tsx
+++ examples/vite/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App.tsx'
+import '@ultraviolet/fonts/fonts-bundled.css'
 
 const root = createRoot(document.getElementById('root')!)
 
