f1d770423c3929fd332ab61c9b84d904b58b7098
Julien Sulpis <jsulpis@scaleway.com>
2026-07-28 09:58:14 +0200

    chore: remove react-intersection-observer from utils/tests

--- utils/test/src/vitest/setup.ts
+++ utils/test/src/vitest/setup.ts
@@ -1,6 +1,5 @@
 import * as domMatchers from '@testing-library/jest-dom/matchers'
 import { cleanup } from '@testing-library/react'
-import { resetIntersectionMocking, setupIntersectionMocking } from 'react-intersection-observer/test-utils'
 import { afterEach, beforeEach, expect, vi } from 'vitest'
 import * as axeMatchers from 'vitest-axe/matchers'
 
@@ -37,7 +36,6 @@ export const setup = () => {
   expect.extend(axeMatchers)
 
   beforeEach(() => {
-    setupIntersectionMocking(vi.fn)
     vi.spyOn(globalThis.Math, 'random').mockReturnValue(0.415_591_366_944_480_4)
 
     window.ResizeObserver = vi.fn().mockImplementation(MockResize)
@@ -60,7 +58,6 @@ export const setup = () => {
   })
 
   afterEach(() => {
-    resetIntersectionMocking()
     vi.spyOn(globalThis.Math, 'random').mockRestore()
 
     cleanup()
