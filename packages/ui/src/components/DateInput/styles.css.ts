072c21aea33646f4543aba8e63db2bec8453acb9
Lisa LUPI <llupi@scaleway.com>
2026-08-24 17:30:09 +0200

    fix(Popup): css specificity

--- packages/ui/src/components/DateInput/styles.css.ts
+++ packages/ui/src/components/DateInput/styles.css.ts
@@ -10,11 +10,15 @@ const container = style({
 
 const calendarContentWrapper = recipe({
   base: {
-    backgroundColor: theme.colors.other.elevation.background.raised,
-    borderRadius: theme.radii.default,
-    color: theme.colors.neutral.text,
-    padding: theme.space[2],
-    width: '16.5rem',
+    selectors: {
+      '&&': {
+        backgroundColor: theme.colors.other.elevation.background.raised,
+        borderRadius: theme.radii.default,
+        color: theme.colors.neutral.text,
+        padding: theme.space[2],
+        width: '16.5rem',
+      },
+    },
   },
   variants: {
     disabled: {
