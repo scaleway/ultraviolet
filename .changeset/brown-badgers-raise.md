005e6e2e8cde2c5a61a65722d6b74e67d7e7cea7
lisalupi <106706307+lisalupi@users.noreply.github.com>
2026-08-25 10:21:44 +0200

    fix(Popup): css specificity (#6825)

---
+++ .changeset/brown-badgers-raise.md
@@ -0,0 +1,5 @@
+---
+"@ultraviolet/ui": patch
+---
+
+`DateInput`, `SearchInput`: increase popup style specificity to ensure that it overrides Popup's default style
--- packages/ui/src/components/SearchInput/styles.css.ts
+++ packages/ui/src/components/SearchInput/styles.css.ts
@@ -2,12 +2,16 @@ import { theme } from '@ultraviolet/themes'
 import { style } from '@vanilla-extract/css'
 
 const popup = style({
-  background: theme.colors.other.elevation.background.raised,
-  boxShadow: `${theme.shadows.raised[0]}, ${theme.shadows.raised[1]}`,
-  minWidth: '38.125rem',
-  padding: `${theme.space['2']} ${theme.space['1']}`,
-  textAlign: 'initial',
-  width: '100%',
+  selectors: {
+    '&&': {
+      background: theme.colors.other.elevation.background.raised,
+      boxShadow: `${theme.shadows.raised[0]}, ${theme.shadows.raised[1]}`,
+      minWidth: '38.125rem',
+      padding: `${theme.space['2']} ${theme.space['1']}`,
+      textAlign: 'initial',
+      width: '100%',
+    },
+  },
 })
 
 const searchInput = style({})
