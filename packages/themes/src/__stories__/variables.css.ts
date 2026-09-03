d3864840831f62d57513f3b2cef42aa838d371ca
Julien Sulpis <jsulpis@scaleway.com>
2026-09-03 11:51:39 +0200

    docs: use UV styles and components

---
+++ packages/themes/src/__stories__/variables.css.ts
@@ -0,0 +1,6 @@
+import { createVar } from '@vanilla-extract/css'
+
+export const swatchColor = createVar()
+export const swatchSize = createVar()
+export const previewBackgroundColor = createVar()
+export const previewTextColor = createVar()
