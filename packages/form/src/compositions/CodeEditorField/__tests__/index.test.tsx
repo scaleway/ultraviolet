7857bbc280ab00c48a4e4baa705a6afba56f5f69
Julien Sulpis <jsulpis@scaleway.com>
2026-09-17 09:17:42 +0000

    perf(CodeEditor): lazy load all languages (#6909)
    * perf(CodeEditor): lazy load all languages
    
    * test: fix name
    
    * fix: tests
    
    * chore: remove @uiw/codemirror-extensions-langs

--- packages/form/src/compositions/CodeEditorField/__tests__/index.test.tsx
+++ packages/form/src/compositions/CodeEditorField/__tests__/index.test.tsx
@@ -1,15 +1,24 @@
+import { screen, waitFor } from '@testing-library/react'
 import { describe, expect, it } from 'vitest'
 import { CodeEditorField } from '..'
 import { renderWithForm } from '../../../__tests__/helpers'
 
-describe('optionSelectorField', () => {
-  it('should render correctly', () => {
+describe('codeEditorField', () => {
+  it('should render correctly', async () => {
     const { asFragment } = renderWithForm(<CodeEditorField extensions="js" name="test" />)
+
+    const textbox = screen.getByRole('textbox')
+    expect(textbox).toBeVisible()
+    await waitFor(() => expect(textbox).toHaveAttribute('data-language', 'javascript'))
+
     expect(asFragment()).toMatchSnapshot()
   })
 
-  it('should render correctly disabled', () => {
-    const { asFragment } = renderWithForm(<CodeEditorField disabled extensions="js" name="test" />)
-    expect(asFragment()).toMatchSnapshot()
+  it('should render correctly disabled', async () => {
+    renderWithForm(<CodeEditorField disabled extensions="js" name="test" />)
+
+    const textbox = screen.getByRole('textbox')
+    expect(textbox).toBeVisible()
+    expect(textbox).toHaveAttribute('aria-readonly', 'true')
   })
 })
